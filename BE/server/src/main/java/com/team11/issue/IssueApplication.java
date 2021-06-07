e19db2fa65673e55cbc9c23eac4c5617b3c143bd
malaheaven <yunhg95@gmail.com>
2021-06-07 20:03:26 +0900

    [#4] feat: :sparkles: spring project init

---
+++ BE/server/src/main/java/com/team11/issue/IssueApplication.java
@@ -0,0 +1,13 @@
+package com.team11.issue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IssueApplication.class, args);
+    }
+
+}
