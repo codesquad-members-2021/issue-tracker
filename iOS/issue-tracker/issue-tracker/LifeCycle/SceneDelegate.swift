f3fdf27e62b6d1ab9d8e158f07591a1ca2265048
Song Lee <songlish233@gmail.com>
2021-06-07 22:15:48 +0900

    chore: 프로젝트 파일 생성

---
+++ iOS/issue-tracker/issue-tracker/LifeCycle/SceneDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  SceneDelegate.swift
+//  issue-tracker
+//
+//  Created by Song on 2021/06/07.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+}
+
---
+++ iOS/issue-tracker/issue-tracker/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  issue-tracker
+//
+//  Created by Song on 2021/06/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
