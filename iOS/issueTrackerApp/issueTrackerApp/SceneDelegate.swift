61cbd60d5f16294992d84bdfd15d066842bd441a
zombietux <>
2021-06-07 17:43:49 +0900

    Implement: 초기 프로젝트 생성 (#none)

---
+++ iOS/issueTrackerApp/issueTrackerApp/SceneDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  SceneDelegate.swift
+//  issueTrackerApp
+//
+//  Created by zombietux on 2021/06/07.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+      
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
---
+++ iOS/issueTrackerApp/issueTrackerApp/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  issueTrackerApp
+//
+//  Created by zombietux on 2021/06/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
