ffa6744e274bbaae188ef8394943fe1f649d49bb
zeke <yangsin1031@naver.com>
2021-06-07 17:00:30 +0900

    chore: 🔧 프로젝트 생성

---
+++ iOS/issue-tracker/issue-tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  issue-tracker
+//
+//  Created by 양준혁 on 2021/06/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
