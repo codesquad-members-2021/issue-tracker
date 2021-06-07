bcf587aeef014329dec587b451dd19b6fdf0deee
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-06-08 00:25:11 +0900

    feat: CRA typescript 설치
    - ghojeong/issue-tracker/#7

---
+++ frontend/issue-cracker/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  extends: [
+    'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
+  ],
+  parserOptions: {
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
+    },
+  },
+  rules: {},
+  settings: {
+    react: {
+      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+  },
+};
---
+++ frontend/issue-cracker/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='App'>
+      Edit <code>src/App.tsx</code> Hi issue
+    </div>
+  );
+}
+
+export default App;
