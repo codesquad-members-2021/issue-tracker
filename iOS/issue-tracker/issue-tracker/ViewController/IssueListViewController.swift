06baa54095d97171f7e1bf517d21ce0f7d0b6229
Sonjh1306 <yale1306@gmail.com>
2021-06-08 17:41:28 +0900

    [#11] feat: IssueList Cell 생성
    IssueList TableView Custom Cell 생성(xib)

--- iOS/issue-tracker/issue-tracker/ViewController/IssueListViewController.swift
+++ iOS/issue-tracker/issue-tracker/ViewController/IssueListViewController.swift
@@ -6,3 +6,16 @@ class IssueListViewController: UIViewController {
         super.viewDidLoad()
     }
 }
+
+extension IssueListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: IssueTableViewCell.identifier) as? IssueTableViewCell else { return UITableViewCell()
+        }
+
+        return cell
+    }
+}
--- iOS/issue-tracker/issue-tracker/ViewController/LoginViewController.swift
+++ iOS/issue-tracker/issue-tracker/ViewController/LoginViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+
 class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
