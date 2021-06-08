20648de7503236c8bd7f30609ec7705b2e30826c
somedaycode <somedaycode@gmail.com>
2021-06-09 07:31:20 +0900

    Chore: #10 이슈 페이지에 Header 컴포넌트 추가

--- FE/issue-tracker/src/pages/Issues.tsx
+++ FE/issue-tracker/src/pages/Issues.tsx
@@ -1,5 +1,23 @@
+import styled from 'styled-components';
+
+import Header from '@components/common/Header';
+
 function Issues() {
-  return <div>여기는 이슈</div>;
+  return (
+    <IssueWrap>
+      <Header />
+    </IssueWrap>
+  );
 }
 
 export default Issues;
+
+const IssueWrap = styled.main`
+  display: flex;
+  margin: 0 auto;
+
+  // 반응형으로 만들어야하나?
+  // 기획서의 너비와 패딩으로 값을 주면 큰 모니터에서 생각보다 작음
+  padding: 0 80px;
+  width: 1440px;
+`;
