355175d76348f0b9fac882a324f770c4ef87741d
Lia <kjk0825@postech.ac.kr>
2021-06-08 17:06:55 +0900

    wip: ⚙️IssueCell UI(#8)

--- ios/IssueTracker/IssueTracker/IssueList/IssueCell.swift
+++ ios/IssueTracker/IssueTracker/IssueList/IssueCell.swift
@@ -9,6 +9,15 @@ import UIKit
 
 class IssueCell: UITableViewCell {
 
+    static let reuseIdentifier = "IssueCell"
+    static let nib = UINib(nibName: IssueCell.reuseIdentifier, bundle: nil)
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var milestoneStackView: UIStackView!
+    @IBOutlet weak var milestoneLabel: UILabel!
+    @IBOutlet weak var labelLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
     }
