ad6a58100daefba15fd81a1fd28bc1911a21aae1
lena <sue822330@gmail.com>
2021-06-08 17:33:18 +0900

    feat: ✨ add AuthManager (#15)

---
+++ iOS/issue-tracker/issue-tracker/AuthManager.swift
@@ -0,0 +1,46 @@
+//
+//  AuthManager.swift
+//  issue-tracker
+//
+//  Created by Ador on 2021/06/08.
+//
+
+import Foundation
+import AuthenticationServices
+
+class AuthManager {
+    private let cliendId = ""
+    private lazy var authURL = URL(string: "https://github.com/login/oauth/authorize?client_id=\(cliendId)&scope=user:email")!
+    private let callbackUrlScheme = "issueTracker"
+    
+    var networkManager: Networkable
+    
+    init(networkManager: Networkable) {
+        self.networkManager = networkManager
+    }
+
+    func authenticate() -> ASWebAuthenticationSession {
+        let authenticationSession = ASWebAuthenticationSession.init(url: authURL, callbackURLScheme: callbackUrlScheme, completionHandler: { (callbackURL: URL?, error: Error?) in
+            guard error == nil,
+                  let callbackURL = callbackURL,
+                  let queryItems = URLComponents(string: callbackURL.absoluteString)?.queryItems,
+                  let code = queryItems.first(where: { $0.name == "code" })?.value else {
+                print("An error occurred when attempting to sign in.")
+                return
+            }
+            print(code)
+            self.requestJWTToken(with: code)
+        })
+        return authenticationSession
+    }
+    
+    private func requestJWTToken(with code: String) {
+        let query = URLQueryItem(name: "code", value: code)
+        let endpoint = Endpoint(path: .login)
+        guard let url = endpoint.url(queryItems: [query]) else { return }
+        networkManager.request(url: url, decodableType: [String: String].self) { (token) in
+            UserDefaults.standard.set(token, forKey: "token")
+            // 메인 이슈 화면으로
+        }
+    }
+}
--- iOS/issue-tracker/issue-tracker/Model/Endpoint.swift
+++ iOS/issue-tracker/issue-tracker/Model/Endpoint.swift
@@ -7,25 +7,50 @@
 
 import Foundation
 
+//struct Endpoint {
+//    static let scheme: String = "https"
+//    static let host: String = ""
+//    static let port: Int = 8080
+//    //static let path: String = ""
+//    
+//    static func url(path: String, queryItems: [URLQueryItem] = []) -> URL? {
+//        var urlComponents = URLComponents()
+//        urlComponents.scheme = scheme
+//        urlComponents.host = host
+//        urlComponents.port = port
+//        urlComponents.path = path
+//        urlComponents.queryItems = queryItems
+//        return urlComponents.url
+//    }
+//    
+//    enum Path: String {
+//        case login = "/login"
+//        
+//        var pathString: String {
+//            return self.rawValue
+//        }
+//    }
+//}
+
 struct Endpoint {
-    static let scheme: String = "https"
-    static let host: String = ""
-    static let port: Int = 8080
-    static let path: String = ""
-    
-    static func url(path: String, queryItems: [URLQueryItem] = []) -> URL? {
+    let scheme: String = "https"
+    let host: String = ""
+    let port: Int = 8080
+    var path: Path
+
+    func url(queryItems: [URLQueryItem] = []) -> URL? {
         var urlComponents = URLComponents()
         urlComponents.scheme = scheme
         urlComponents.host = host
         urlComponents.port = port
-        urlComponents.path = path
+        urlComponents.path = path.pathString
         urlComponents.queryItems = queryItems
         return urlComponents.url
     }
-    
+
     enum Path: String {
         case login = "/login"
-        
+
         var pathString: String {
             return self.rawValue
         }
