d29d239ee0e6cc2c60df8094a37787cc43da3677
somedaycode <somedaycode@gmail.com>
2021-06-08 16:04:48 +0900

    Chore: #1 chakra-UI 테마 스타일 추가

--- FE/issue-tracker/src/App.tsx
+++ FE/issue-tracker/src/App.tsx
@@ -1,8 +1,7 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
-import { ChakraProvider } from '@chakra-ui/react';
 
-import theme from '@styles/theme';
+import customTheme from '@styles/theme';
 import Login from '@pages/Login';
 import Issues from '@pages/Issues';
 import NewIssue from '@pages/NewIssue';
@@ -12,32 +11,30 @@ import IssueDetail from '@pages/IssueDetail';
 
 function App() {
   return (
-    <ChakraProvider>
-      <ThemeProvider theme={theme}>
-        <BrowserRouter>
-          <Switch>
-            <Route path="/" exact>
-              <Login />
-            </Route>
-            <Route path="/issues">
-              <Issues />
-            </Route>
-            <Route path="/new-issue">
-              <NewIssue />
-            </Route>
-            <Route path="/issue-detail">
-              <IssueDetail />
-            </Route>
-            <Route path="/labels">
-              <Labels />
-            </Route>
-            <Route path="/milestones">
-              <MileStones />
-            </Route>
-          </Switch>
-        </BrowserRouter>
-      </ThemeProvider>
-    </ChakraProvider>
+    <ThemeProvider theme={customTheme}>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact>
+            <Login />
+          </Route>
+          <Route path="/issues">
+            <Issues />
+          </Route>
+          <Route path="/new-issue">
+            <NewIssue />
+          </Route>
+          <Route path="/issue-detail">
+            <IssueDetail />
+          </Route>
+          <Route path="/labels">
+            <Labels />
+          </Route>
+          <Route path="/milestones">
+            <MileStones />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
--- FE/issue-tracker/src/index.tsx
+++ FE/issue-tracker/src/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ChakraProvider } from '@chakra-ui/react';
+import customTheme from '@styles/theme';
+
 import App from './App';
 
 import '@styles/reset.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={customTheme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
