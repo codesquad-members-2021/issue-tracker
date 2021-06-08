5994a37f163b929c07094a7ef026ea7858da2f68
Jungwon Lee <leejungwon96@gmail.com>
2021-06-08 15:26:18 +0900

    feat: :sparkles: Header 뷰 완성

--- FE/issue-tracker/src/App.tsx
+++ FE/issue-tracker/src/App.tsx
@@ -2,6 +2,7 @@ import { RecoilRoot } from "recoil";
 import { Suspense } from "react";
 import Router from "@/Routes/Router";
 import GlobalStyles from "@/Styles/GlobalStyles";
+import Header from "@/Components/Header/Header";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Suspense fallback={<div>Loading...</div>}>
           <GlobalStyles />
           <Router />
+          <Header />
         </Suspense>
       </RecoilRoot>
     </div>
