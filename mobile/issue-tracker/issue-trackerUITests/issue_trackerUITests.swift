3e2cfba8b45c41471a518c28aa86701a4b5c5711
hoonha choi <sosqsz2@naver.com>
2021-06-08 16:49:53 +0900

    feat: 프로젝트 생성 및 설정 ghojeong/issue-tracker/#10

---
+++ mobile/issue-tracker/issue-trackerUITests/issue_trackerUITests.swift
@@ -0,0 +1,16 @@
+//
+//  issue_trackerUITests.swift
+//  issue-trackerUITests
+//
+//  Created by HOONHA CHOI on 2021/06/08.
+//
+
+import XCTest
+
+class IssuetrackerUITests: XCTestCase {
+
+    func testExample() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+}
