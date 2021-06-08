0e6ed9138a4afbfbe7827209a1f8edd96e4e264c
Ray suh <readingod@naver.com>
2021-06-08 15:10:43 +0900

    feat: (#1) 탭바 컨트롤러로 이동 구현

--- IssueTracker/IssueTracker/Presenter/Controller/LoginViewController.swift
+++ IssueTracker/IssueTracker/Presenter/Controller/LoginViewController.swift
@@ -1,11 +1,23 @@
 import UIKit
+import RxSwift
+import RxCocoa
+import NSObject_Rx
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var logInButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupLoginButton()
     }
 
-
+    private func setupLoginButton() {
+        logInButton.rx.tap
+            .subscribe(onNext: { [weak self] in
+                guard let issueVC = self?.storyboard?.instantiateViewController(withIdentifier: ViewControllerID.tabBar) else { return }
+                issueVC.modalPresentationStyle = .fullScreen
+                self?.present(issueVC, animated: true, completion: nil)
+            }).disposed(by: rx.disposeBag)
+    }
 }
 
