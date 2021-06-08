b1603591c418a7ff05ac57b0a242ce6a25019653
Lia <kjk0825@postech.ac.kr>
2021-06-08 14:43:52 +0900

    feat: ✨IssueTableViewCell file 생성(#8)

---
+++ ios/IssueTracker/IssueTracker/IssueList/IssueCell.swift
@@ -0,0 +1,20 @@
+//
+//  IssueCell.swift
+//  IssueTracker
+//
+//  Created by Lia on 2021/06/08.
+//
+
+import UIKit
+
+class IssueCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
