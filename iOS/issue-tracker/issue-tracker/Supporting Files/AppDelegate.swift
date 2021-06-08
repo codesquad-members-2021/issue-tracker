a9cd883b735a05ef374b6191889cc74a7c8ea97a
lena <sue822330@gmail.com>
2021-06-08 11:37:43 +0900

    chore: 🔧 clean up directory

---
+++ iOS/issue-tracker/issue-tracker/Supporting Files/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  issue-tracker
+//
+//  Created by 양준혁 on 2021/06/07.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
