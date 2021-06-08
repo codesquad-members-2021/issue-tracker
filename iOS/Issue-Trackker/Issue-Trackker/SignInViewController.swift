70085f5fd89566b6e17fcbecd3dd8c6152b0e141
shimyoungmin <dudals6965@gmail.com>
2021-06-08 17:22:01 +0900

    [#1]Feat: Add SignInViewController
    SignInViewController에 IBOutlet 연결

---
+++ iOS/Issue-Trackker/Issue-Trackker/SignInViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Issue-Trackker
+//
+//  Created by 심영민 on 2021/06/08.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    @IBOutlet weak var githubSignInButton: UIButton!
+    @IBOutlet weak var appleSignInButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
