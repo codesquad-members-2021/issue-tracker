2a4f939cb97aa526bdf03d94865f784f9bdb047e
Dami Kim <dami.k.dev@gmail.com>
2021-06-08 18:07:35 +0900

    WIP: OAuth 로그인 진행중 (#12)

--- frontend/src/components/pages/LoginPage.jsx
+++ frontend/src/components/pages/LoginPage.jsx
@@ -1,5 +1,16 @@
+import { Link } from "react-router-dom";
 const LoginPage = () => {
-	return <div>로그인페이지</div>;
+	return (
+		<>
+			<div>로그인페이지</div>
+
+			<button>
+				<a href="https://github.com/login/oauth/authorize?client_id=c39689919134be7915cf&redirect_uri=http://localhost:3000/login">
+					GitHub 계정으로 로그인
+				</a>
+			</button>
+		</>
+	);
 };
 
 export default LoginPage;
