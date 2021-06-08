bda7167f5e959d30ed9c7658e47617ececd1df9a
eamon3481 <68339352+eamon3481@users.noreply.github.com>
2021-06-09 01:40:25 +0900

    [#9] Feat: IssuesPage Router setting

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,11 +1,20 @@
 import { theme } from 'style/theme';
 import GlobalStyle from 'style/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import IssuesPage from 'pages/IssuesPage';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <BrowserRouter>
+        <Switch>
+          <Route path={['/', '/issues']}>
+            <IssuesPage />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
---
+++ fe/src/pages/IssuesPage.tsx
@@ -0,0 +1,5 @@
+const IssuesPage = () => {
+  return <div></div>;
+};
+
+export default IssuesPage;
