21d70efbe8be7d8195cea7c68ea82a1a0dcdcb5b
eve712 <62237639+eve712@users.noreply.github.com>
2021-06-10 02:50:36 +0900

    Add: #12 라벨 생성 컴포넌트 chakra 스타일 작성

---
+++ FE/issue-tracker/src/components/labels/newLabelStyle.ts
@@ -0,0 +1,28 @@
+const labelNameInput = {
+  placeholder: '레이블 이름',
+  size: 'md',
+  variant: 'filled',
+  marginBottom: '16px',
+};
+
+const labelDescInput = {
+  placeholder: '설명(선택)',
+  size: 'md',
+  variant: 'filled',
+  marginBottom: '16px',
+};
+
+const labelColorLeft = {
+  color: 'gray.600',
+  fontSize: 'xs',
+  border: 'none',
+};
+
+const labelCheckbox = {
+  backgroundColor: '#EDF2F7',
+  paddingRight: '16px',
+  spacing: '0.8rem',
+  colorScheme: 'cyan',
+};
+
+export { labelNameInput, labelDescInput, labelColorLeft, labelCheckbox };
