1042bd75d9c4d0152e206a17811cbd8f9c8605a0
somedaycode <somedaycode@gmail.com>
2021-06-09 10:24:03 +0900

    Chore: #10 이슈 목록 페이지 기본 레이아웃 컴포넌트 추가
    Actions, IssueTable.tsx

--- FE/issue-tracker/src/pages/Issues.tsx
+++ FE/issue-tracker/src/pages/Issues.tsx
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
 
 import Header from '@components/common/Header';
+import IssueContainer from '@components/issues/IssueContainer';
 
 function Issues() {
   return (
     <IssueWrap>
       <Header />
+      <IssueContainer />
     </IssueWrap>
   );
 }
@@ -13,8 +15,9 @@ function Issues() {
 export default Issues;
 
 const IssueWrap = styled.main`
-  display: flex;
   margin: 0 auto;
+  display: flex;
+  flex-direction: column;
 
   // 반응형으로 만들어야하나?
   // 기획서의 너비와 패딩으로 값을 주면 큰 모니터에서 생각보다 작음
