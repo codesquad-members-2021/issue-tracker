bae2c20bff747cdcb778836803137a8ac5f14744
hiro <kiryuinh32@gmail.com>
2021-06-10 02:17:21 +0900

    fix: LabelDummyData.labelResponses의 타입은 List -> Set으로 수정

--- be/issue-tracker-be/src/main/java/com/codesquad/issuetracker/label/LabelController.java
+++ be/issue-tracker-be/src/main/java/com/codesquad/issuetracker/label/LabelController.java
@@ -3,13 +3,13 @@ package com.codesquad.issuetracker.label;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.Set;
 
 @RestController
 public class LabelController {
 
     @GetMapping("/labels")
-    public List<LabelResponse> getLabels() {
+    public Set<LabelResponse> getLabels() {
         return LabelDummyData.labelResponses();
     }
 }
--- be/issue-tracker-be/src/main/java/com/codesquad/issuetracker/label/LabelDummyData.java
+++ be/issue-tracker-be/src/main/java/com/codesquad/issuetracker/label/LabelDummyData.java
@@ -1,20 +1,21 @@
 package com.codesquad.issuetracker.label;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class LabelDummyData {
     private LabelDummyData() {
     }
 
-    public static List<LabelResponse> labelResponses() {
-        return Arrays.asList(
+    public static Set<LabelResponse> labelResponses() {
+        return new HashSet<>(Arrays.asList(
                 LabelResponse.builder()
                     .id(1L)
                     .name("backend")
                     .description("라벨 설명")
                     .color("라벨 컬러")
                     .build()
-        );
+        ));
     }
 }
