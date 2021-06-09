0ba4500cf2cf02924f5d7f774e22cf578a861530
eve712 <62237639+eve712@users.noreply.github.com>
2021-06-09 22:19:34 +0900

    Add: Labels, Milestones 페이지에서 사용할 Actions 컴포넌트 추가

---
+++ FE/issue-tracker/src/components/common/Actions.tsx
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+import Tabs from '@components/common/Tabs';
+import { Button } from '@chakra-ui/react';
+
+interface Props {
+  page: string;
+}
+
+function Actions({ page }: Props) {
+  return (
+    <ActionsWrap>
+      <Tabs page={page} />
+      <Button
+        width="120px"
+        fontSize="xs"
+        background="bl_initial"
+        colorScheme="blue"
+      >
+        + 추가
+      </Button>
+    </ActionsWrap>
+  );
+}
+
+export default Actions;
+
+const ActionsWrap = styled.section`
+  /* theme에 추가하기 */
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+`;
