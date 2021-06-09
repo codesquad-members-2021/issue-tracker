c42d938b20675f143c5bd321973a65213b7a1838
junamee <skawn11114@gmail.com>
2021-06-09 16:50:46 +0900

    feat: (#14) 라우터설정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import IssueListPage from './components/issueListpage/IssueListPage';
-
+import LoginPage from './components/login/LoginPage'
+import Header from './components/header/Header'
+import { RecoilRoot } from 'recoil'
 function App() {
+  const isLogin = true
   return (
-      <div className="App">
-        <IssueListPage/>
-      
-      </div>
+
+    <div className="App">
+      <RecoilRoot>
+        <Router>
+          <Switch>
+          {isLogin && <Header/>}
+          </Switch>
+          <Switch>
+            <Route path='/' component={LoginPage} exact/>
+            //로그인이 되면, 되어있다면 화면 전환.
+            <Route path='/main' component={IssueListPage}/>
+          </Switch>
+        </Router>
+      </RecoilRoot>
+    </div>
   );
 }
 
--- frontend/src/components/issueListpage/IssueListPage.tsx
+++ frontend/src/components/issueListpage/IssueListPage.tsx
@@ -5,7 +5,6 @@ function IssueListPage(){
 
   return (
     <>
-      <Header/>
       <Body>
         <IssueList/>
       </Body>
