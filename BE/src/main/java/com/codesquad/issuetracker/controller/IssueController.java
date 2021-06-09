3dfb6062ca35ddab527289026d2bffc6fa8a4af6
woody <woojihye2339@gmail.com>
2021-06-09 09:25:04 +0900

    feat : IssueController 구현 및 API 매핑 #3
    > IssueController
      - Issue와 관련된 요청을 담당하는 컨트롤러
      - Issue에 대한 api와 매핑
    
    > 내부 로직 미작성
      - 현재 기능 구현은 되어있지 않은 상태
      - service와 repository 계층과의 의존성 없음

---
+++ BE/src/main/java/com/codesquad/issuetracker/controller/IssueController.java
@@ -0,0 +1,63 @@
+package com.codesquad.issuetracker.controller;
+
+import com.codesquad.issuetracker.ApiResponse;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/issue")
+public class IssueController {
+
+    @GetMapping
+    public ApiResponse getIssues(){
+        return ApiResponse.ok("Get Issue List");
+    }
+
+    @GetMapping("/{issueId}")
+    public ApiResponse getIssue(@PathVariable Long issueId){
+        return ApiResponse.ok("Get Single Issue Number " + issueId);
+    }
+
+    @PostMapping
+    public ApiResponse createIssue(){
+        return ApiResponse.ok("Create Issue");
+    }
+
+    @PutMapping("/{issueId}/title")
+    public ApiResponse editTitle(@PathVariable Long issueId, @RequestBody String title){
+        return ApiResponse.ok("Edit Title of the Issue");
+    }
+
+    @PutMapping("/{issueId}/assignee")
+    public ApiResponse editAssignee(@PathVariable Long issueId, @RequestBody ArrayList<String> assigneeList){
+        return ApiResponse.ok("Edit Assignee of the Issue");
+    }
+
+    @PutMapping("/{issueId}/content")
+    public ApiResponse editContent(@PathVariable Long issueId, @RequestBody String content){
+        return ApiResponse.ok("Edit Content of the Issue");
+    }
+
+    @PutMapping("/{issueId}/status")
+    public ApiResponse editStatus(@PathVariable Long issueId, @RequestBody boolean status){
+        return ApiResponse.ok("Edit Status of the Issue");
+    }
+
+    @PutMapping("/{issueId}/label")
+    public ApiResponse editLabel(@PathVariable Long issueId, @RequestBody ArrayList<String> labelList){
+        return ApiResponse.ok("Edit Labels of the Issue");
+    }
+
+
+    @PutMapping("/{issueId}/milestone")
+    public ApiResponse editMilestone(@PathVariable Long issueId, @RequestBody Long milestoneId){
+        return ApiResponse.ok("Edit Milestone of the Issue");
+    }
+
+    @DeleteMapping("/{issueId}")
+    public ApiResponse deleteIssue(@PathVariable Long issueId){
+        return ApiResponse.ok("Delete Issue Number " + issueId);
+    }
+
+}
