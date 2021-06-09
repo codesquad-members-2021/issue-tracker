940a5ed7b15aa03f7fd0de0b1933935656ddab1d
woody <woojihye2339@gmail.com>
2021-06-10 00:29:57 +0900

    feat : jasypt 암호화 설정
    > jasypt 설정 추가 및 적용
      - github client secret key를 jasypt을 통해 암호화
      - jasypt password는 환경 변수 설정

---
+++ BE/src/main/java/com/codesquad/issuetracker/config/JasyptConfig.java
@@ -0,0 +1,28 @@
+package com.codesquad.issuetracker.config;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EncryptablePropertySource;
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EncryptablePropertySource(name = "EncryptedProperties", value = "classpath:application-jasypt.properties")
+public class JasyptConfig {
+
+    @Value("${jasypt.encryptor.password}")
+    private String encryptKey;
+
+    @Bean("jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor(){
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(encryptKey);
+        config.setAlgorithm("PBEWithMD5AndDES");
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+
+}
