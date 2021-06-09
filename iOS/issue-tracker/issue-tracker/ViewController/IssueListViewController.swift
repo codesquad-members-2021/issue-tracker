6b0a5e0976cc186aae79dae26939fe73b3cba50f
Sonjh1306 <yale1306@gmail.com>
2021-06-09 15:28:09 +0900

    [#11] refactor: 커스텀 셀 사이즈 변경

--- iOS/issue-tracker/issue-tracker/ViewController/IssueListViewController.swift
+++ iOS/issue-tracker/issue-tracker/ViewController/IssueListViewController.swift
@@ -2,20 +2,23 @@ import UIKit
 
 class IssueListViewController: UIViewController {
 
+    @IBOutlet weak var issueTableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        issueTableView.register(IssueTableViewCell.nib, forCellReuseIdentifier: IssueTableViewCell.identifier)
     }
 }
 
-extension IssueListViewController: UITableViewDataSource {
+extension IssueListViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4
+        return 10
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: IssueTableViewCell.identifier) as? IssueTableViewCell else { return UITableViewCell()
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: IssueTableViewCell.identifier, for: indexPath) as? IssueTableViewCell else { return UITableViewCell()
         }
-
+        
         return cell
     }
 }
