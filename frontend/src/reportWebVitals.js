1039b213e83d5a3896b7427918e391f2eb652aec
Hannah Park <ha3158987@gmail.com>
2021-06-09 18:13:26 +0900

    add: OAuth 실습

--- frontend/src/reportWebVitals.js
+++ frontend/src/reportWebVitals.js
@@ -1,11 +1,11 @@
-const reportWebVitals = onPerfEntry => {
+const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
+      getCLS(onPerfEntry); //Cumulative Layout Shift (CLS)
+      getFID(onPerfEntry); //First Input Delay (FID)  웹페이지 반응성
+      getFCP(onPerfEntry); //First Contentful Paint (FCP) 화면이 그려질 때까지 걸리는 시간
+      getLCP(onPerfEntry); //Largest Contentful Paint (LCP) 화면에서 가장 큰 덩어리(중요도가 높은)를 로딩하는 데 걸리는 시간
+      getTTFB(onPerfEntry); //Time to First Byte (TTFB) 첫 번째 바이트를 가지고오는 데 걸린 시간
     });
   }
 };
