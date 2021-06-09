ecee87aa4ad5fb73fd94b240d23c6df118078740
jeonyeonkyu <ehb6915@naver.com>
2021-06-09 17:33:03 +0900

    Feat: 페이지 컴포넌트 초기세팅 #16

--- fe/client/src/App.tsx
+++ fe/client/src/App.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { Switch, Route } from 'react-router-dom';
+
+const LoginLazy = React.lazy(() => import('@/Pages/LoginPage'));
+const Page404Lazy = React.lazy(() => import('@/Pages/Page404'));
 
 function App() {
   return (
-    <div>
-    </div>
+    <>
+    <CssBaseline />
+    <Suspense fallback="loading...">
+      <Switch>
+        <Route exact path="/issueList">
+
+        </Route>
+
+        <Route exact path="/" component={LoginLazy} />
+
+        <Route>
+          <Page404Lazy />
+        </Route>
+      </Switch >
+    </Suspense>
+    </>
   );
 }
 
---
+++ fe/client/src/Pages/LoginPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div>
+        LoginPage
+    </div>
+  )
+}
+
+export default LoginPage;
