721b468e5bce5ae5a6189d334bfc2360adf1602c
zombietux <>
2021-06-09 12:56:11 +0900

    Implement: #3 로그인 인증유무를 판달할 코드 추가

--- iOS/issueTrackerApp/issueTrackerApp/Data/Repository/NetworkController.swift
+++ iOS/issueTrackerApp/issueTrackerApp/Data/Repository/NetworkController.swift
@@ -9,8 +9,30 @@ import Foundation
 
 class NetworkController {
     private let keychainController: KeychainController
+    private var requests: [URL: AnyObject] = [:]
     
     init(keychainController: KeychainController) {
         self.keychainController = keychainController
+    }
+    
+    var accessToken: String? {
+        return keychainController.readAccessToken()
+    }
+    
+    var isClientAuthenticated: Bool {
+        return accessToken != nil
+    }
+    
+    func authenticateWith(authorizationCode: String, state: String, completion: @escaping () -> Void) {
+        let accessTokenRequest = AccessTokenRequest(authorizationCode: authorizationCode, state: state)
+        let requestURL = accessTokenRequest.urlRequest.url!
+        requests[requestURL] = accessTokenRequest
+        accessTokenRequest.execute { (authorization) in
+            if let accessToken = authorization?.accessToken {
+                self.keychainController.store(accessToken: accessToken)
+            }
+            self.requests[requestURL] = nil
+            completion()
+        }
     }
 }
