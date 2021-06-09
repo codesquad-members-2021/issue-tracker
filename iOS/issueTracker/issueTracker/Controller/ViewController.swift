86b03ad9f203cc8e25e62c2585642b55f2364390
jhpark-steve <park9368@naver.com>
2021-06-09 15:28:44 +0900

    [#10] feat: :art: 메인 페이지 UI 작성

--- iOS/issueTracker/issueTracker/Controller/ViewController.swift
+++ iOS/issueTracker/issueTracker/Controller/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func qwer(_ sender: Any) {
+        self.performSegue(withIdentifier: "IssueList", sender: nil)
+    }
 }
 
