87ee6a8f7b8a1d1321c99db8ac10bcad7e5ee364
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-06-09 16:54:47 +0900

    fix: MileStoneResponse에 dueDate 추가
    날짜만 알면 되기 때문에 LocalDate 사용
    
    Closes: hiro032/issue-tracker/#17

--- be/issue-tracker-be/src/main/java/com/codesquad/issuetracker/milestone/MileStoneResponse.java
+++ be/issue-tracker-be/src/main/java/com/codesquad/issuetracker/milestone/MileStoneResponse.java
@@ -3,13 +3,15 @@ package com.codesquad.issuetracker.milestone;
 import lombok.Builder;
 import lombok.Data;
 
+import java.time.LocalDate;
+
 @Data
 @Builder
 public class MileStoneResponse {
     private Long id;
     private String name;
     private String description;
-    //TODO: 완료일 추가해야함.
+    private LocalDate dueDate;
     private boolean isClosed;
     private int openedIssueCount;
     private int closedIssueCount;
