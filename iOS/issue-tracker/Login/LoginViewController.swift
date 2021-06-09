9eabdf28900256d4c4178d2ce79d740766ab5dbf
ellyheetov <ellyheetov@gmail.com>
2021-06-09 14:59:24 +0900

    feat: apple login 버튼 추가 (#5)

--- iOS/issue-tracker/Login/LoginViewController.swift
+++ iOS/issue-tracker/Login/LoginViewController.swift
@@ -7,11 +7,35 @@
 
 import UIKit
 import Octokit
+import AuthenticationServices
 
 class LoginViewController: UIViewController {
 
+    var appleLogInButton : ASAuthorizationAppleIDButton = {
+        let button = ASAuthorizationAppleIDButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(handleLogInWithAppleID), for: .touchUpInside)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureButton()
+    }
+    
+    
+    @objc func handleLogInWithAppleID(){
+        print("test")
     }
 
+    private func configureButton(){
+        self.view.addSubview(appleLogInButton)
+        
+        NSLayoutConstraint.activate([
+            appleLogInButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            appleLogInButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            appleLogInButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -60),
+            appleLogInButton.heightAnchor.constraint(equalToConstant: 56)
+        ])
+    }
 }
