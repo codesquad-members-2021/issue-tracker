6ebd65dca351241463d335596ad49069076752c1
eve712 <62237639+eve712@users.noreply.github.com>
2021-06-10 03:37:58 +0900

    Refactor: input 요소들 분리해서 import
    * 마지막 Button을 children Props로 보냄

--- FE/issue-tracker/src/components/labels/NewLabel.tsx
+++ FE/issue-tracker/src/components/labels/NewLabel.tsx
@@ -1,56 +1,17 @@
 import styled from 'styled-components';
-import {
-  Input,
-  InputGroup,
-  InputLeftAddon,
-  InputRightAddon,
-  Checkbox,
-} from '@chakra-ui/react';
-import { ReactComponent as Refresh } from '@assets/refresh.svg';
-import Label from '@components/labels/Label';
-import {
-  labelNameInput,
-  labelDescInput,
-  labelColorLeft,
-  labelCheckbox,
-} from '@components/labels/newLabelStyle';
+import { Button } from '@chakra-ui/react';
+import LabelInputBox from '@components/labels/LabelInputBox';
+import { submitButton } from '@components/labels/newLabelStyle';
 
 function NewLabel() {
-  const colorCode = '#EFF0F6';
-
   return (
     <NewLabelWrap>
       <h2>새로운 레이블 추가</h2>
-
-      <NewLabelContent>
-        <LabelTagBox>
-          <Label name={'레이블 이름'} colorCode={colorCode} fontLight={false} />
-        </LabelTagBox>
-
-        <LabelInputBox>
-          <Input {...labelNameInput} />
-          <Input {...labelDescInput} />
-
-          <LabelColorInput>
-            <InputGroup size="md" width="240px" marginRight="16px">
-              <InputLeftAddon {...labelColorLeft} children="배경 색상" />
-              <Input value={colorCode} variant="filled" />
-              <InputRightAddon
-                children={<Refresh className="icon_refresh" />}
-                border="none"
-              />
-            </InputGroup>
-            <InputGroup size="md" width="352px" variant="filled">
-              <InputLeftAddon {...labelColorLeft} children="텍스트 색상" />
-              <Checkbox {...labelCheckbox} defaultIsChecked>
-                어두운색
-              </Checkbox>
-              <Checkbox {...labelCheckbox}>밝은색</Checkbox>
-              <InputRightAddon border="none" />
-            </InputGroup>
-          </LabelColorInput>
-        </LabelInputBox>
-      </NewLabelContent>
+      <LabelInputBox>
+        <ButtonBox>
+          <Button {...submitButton}>+ 완료</Button>
+        </ButtonBox>
+      </LabelInputBox>
     </NewLabelWrap>
   );
 }
@@ -69,21 +30,7 @@ const NewLabelWrap = styled.section`
   }
 `;
 
-const NewLabelContent = styled.div`
+const ButtonBox = styled.div`
   display: flex;
-`;
-const LabelTagBox = styled.div`
-  width: 330px;
-  ${({ theme }) => theme.flexCenter};
-`;
-const LabelInputBox = styled.div`
-  width: 100%;
-`;
-const LabelColorInput = styled.div`
-  display: flex;
-  .icon_refresh {
-    path {
-      stroke: ${({ theme }) => theme.colors.gr_label};
-    }
-  }
+  flex-direction: row-reverse;
 `;
