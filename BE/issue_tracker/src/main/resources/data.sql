b9668f34f9fa479afea22a5ccadfa5dd8dfe24cd
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-06-10 15:49:57 +0900

    chore : 기능 테스트를 위한 더미데이터 변경
    2번 issue의 is_open 속성 false로 변경

--- BE/issue_tracker/src/main/resources/data.sql
+++ BE/issue_tracker/src/main/resources/data.sql
@@ -11,7 +11,7 @@ values (1, "백엔드 1주차 마일스톤 내용", "2021-06-01", "2021-06-15", 
 -- issue
 insert into issue(id, created_time, is_open, issue_number, title, milestone_id, writer_id)
 values (1, Current_Timestamp, true, 1, "이슈 제목1", 1, 1),
-       (2, Current_Timestamp, true, 2, "이슈 제목2", 1, 2);
+       (2, Current_Timestamp, false, 2, "이슈 제목2", 1, 2);
 
 -- label
 insert into label(id, title, content, color)
