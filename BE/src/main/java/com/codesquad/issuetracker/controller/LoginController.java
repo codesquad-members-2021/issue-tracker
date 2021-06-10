b26d224493f13e3d952aef3b858b354a36237369
woody <woojihye2339@gmail.com>
2021-06-10 11:16:54 +0900

    feat : logger 설정 및 properties 코드 정리 #4
    > issuetracker 폴더 아래의 코드는 debug 로그로 설정
    
    > application.properties 코드 정리

--- BE/src/main/java/com/codesquad/issuetracker/controller/LoginController.java
+++ BE/src/main/java/com/codesquad/issuetracker/controller/LoginController.java
@@ -2,15 +2,18 @@ package com.codesquad.issuetracker.controller;
 
 import com.codesquad.issuetracker.ApiResponse;
 import com.codesquad.issuetracker.service.LoginService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
+
+    private Logger logger = LoggerFactory.getLogger(LoginController.class);
 
     private final String GITHUB_URL = "https://github.com/login/oauth/authorize";
     private final String CLIENT_ID = "7bd8b036c3471804563e";
@@ -28,7 +31,8 @@ public class LoginController {
 
     @GetMapping("/github")
     public ApiResponse githubLogin(@RequestParam String code) {
-        System.out.println(code);
+        logger.debug("code : {} ", code);
+
         loginService.githubLogin(code);
         return ApiResponse.ok("GitHub Login with code " + code);
     }
--- BE/src/main/java/com/codesquad/issuetracker/service/LoginService.java
+++ BE/src/main/java/com/codesquad/issuetracker/service/LoginService.java
@@ -3,6 +3,8 @@ package com.codesquad.issuetracker.service;
 import com.codesquad.issuetracker.AccessTokenResponse;
 import com.codesquad.issuetracker.domain.User;
 import com.codesquad.issuetracker.request.AccessTokenRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
 import org.springframework.http.RequestEntity;
 import org.springframework.stereotype.Service;
@@ -10,6 +12,8 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class LoginService {
+
+    private Logger logger = LoggerFactory.getLogger(LoginService.class);
 
     private static final String GITHUB_ACCESS_TOKEN_URL = "https://github.com/login/oauth/access_token";
     private static final String GITHUB_USER_URL = "https://api.github.com/user";
@@ -31,10 +35,13 @@ public class LoginService {
         RequestEntity<AccessTokenRequest> accessTokenRequestEntity = RequestEntity.post(GITHUB_ACCESS_TOKEN_URL) // 보낼 request를 만듦
                 .header("Accept", "application/json")// 받아올 리턴 값을 json형식으로 설정
                 .body(AccessTokenRequest.create(GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRETS, code));
+
         RestTemplate restTemplate = new RestTemplate(); // RestTemplate : 요청을 보내기 또는 응답을 받기
         AccessTokenResponse accessTokenResponse = restTemplate.exchange(accessTokenRequestEntity, AccessTokenResponse.class).getBody(); // 액세스토큰 받아오기
-        System.out.println(accessTokenResponse.getAccessToken());
-        RequestEntity<Void> githubUserInfoRequestEntity = RequestEntity.get(GITHUB_USER_URL)// body에 아무것도 보내지 않으므로 Void로 설정 
+
+        logger.debug("access token : {}", accessTokenResponse.getAccessToken());
+
+        RequestEntity<Void> githubUserInfoRequestEntity = RequestEntity.get(GITHUB_USER_URL)// body에 아무것도 보내지 않으므로 Void로 설정
                 .header("Accept", "application/json")
                 .header("Authorization", "token " + accessTokenResponse.getAccessToken())
                 .build();
