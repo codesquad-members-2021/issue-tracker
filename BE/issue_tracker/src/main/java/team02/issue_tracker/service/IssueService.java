dd621d0fe082eeced4669c9762a817b94f5b7dac
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-06-11 05:13:02 +0900

    feat : 추가적인 repository 의존성 주입, addIssue 메소드 추가
    IssueRequest를 Issue로 변환할 때 필요한 UserRepository, LabelRepository, MilestoneRepository 의존성 추가
    Label의 id값 리스트로 Label 리스트 생성하는 getLabels 메소드 추가
    assignee의 id값 리스트로 User 리스트 생성하는 getAssignees 메소드 추가

--- BE/issue_tracker/src/main/java/team02/issue_tracker/service/IssueService.java
+++ BE/issue_tracker/src/main/java/team02/issue_tracker/service/IssueService.java
@@ -1,24 +1,39 @@
 package team02.issue_tracker.service;
 
 import org.springframework.stereotype.Service;
-import team02.issue_tracker.domain.Issue;
+import org.springframework.transaction.annotation.Transactional;
+import team02.issue_tracker.domain.*;
+import team02.issue_tracker.dto.issue.IssueRequest;
 import team02.issue_tracker.dto.wrapping.DataResponse;
 import team02.issue_tracker.dto.wrapping.ListDataResponse;
 import team02.issue_tracker.dto.issue.DetailIssueResponse;
 import team02.issue_tracker.dto.issue.IssueResponse;
 import team02.issue_tracker.exception.IssueNotFoundException;
+import team02.issue_tracker.exception.LabelNotFoundException;
+import team02.issue_tracker.exception.MilestoneNotFoundException;
+import team02.issue_tracker.exception.UserNotFoundException;
 import team02.issue_tracker.repository.IssueRepository;
+import team02.issue_tracker.repository.LabelRepository;
+import team02.issue_tracker.repository.MilestoneRepository;
+import team02.issue_tracker.repository.UserRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class IssueService {
 
-    private IssueRepository issueRepository;
+    private final IssueRepository issueRepository;
+    private final UserRepository userRepository;
+    private final LabelRepository labelRepository;
+    private final MilestoneRepository milestoneRepository;
 
-    public IssueService(IssueRepository issueRepository) {
+    public IssueService(IssueRepository issueRepository, UserRepository userRepository, LabelRepository labelRepository, MilestoneRepository milestoneRepository) {
         this.issueRepository = issueRepository;
+        this.userRepository = userRepository;
+        this.labelRepository = labelRepository;
+        this.milestoneRepository = milestoneRepository;
     }
 
     public ListDataResponse getAllIssueResponses() {
@@ -33,5 +48,38 @@ public class IssueService {
         Issue issue = issueRepository.findById(issueId).orElseThrow(IssueNotFoundException::new);
         DetailIssueResponse issueResponse = new DetailIssueResponse(issue);
         return new DataResponse(issueResponse);
+    }
+
+    @Transactional
+    public void addIssue(IssueRequest issueRequest) {
+        User writer = userRepository.findById(issueRequest.getUserId()).orElseThrow(UserNotFoundException::new);
+        List<Label> labels = getLabels(issueRequest.getLabelIds());
+        Milestone milestone = milestoneRepository.findById(issueRequest.getMilestoneId()).orElseThrow(MilestoneNotFoundException::new);
+        List<User> assignees = getAssignees(issueRequest.getAssigneeIds());
+
+        Issue issue = issueRequest.toIssue(writer, labels, milestone, assignees);
+        issueRepository.save(issue);
+    }
+
+    private List<Label> getLabels(List<Long> labelIds) {
+        List<Label> labels = new ArrayList<>();
+        labelIds.stream()
+                .map(labelId -> {
+                    Label label = labelRepository.findById(labelId).orElseThrow(LabelNotFoundException::new);
+                    labels.add(label);
+                    return label;
+                });
+        return labels;
+    }
+
+    private List<User> getAssignees(List<Long> assigneeIds) {
+        List<User> assignees = new ArrayList<>();
+        assigneeIds.stream()
+                .map(assigneeId -> {
+                    User assignee = userRepository.findById(assigneeId).orElseThrow(UserNotFoundException::new);
+                    assignees.add(assignee);
+                    return assignee;
+                });
+        return assignees;
     }
 }
