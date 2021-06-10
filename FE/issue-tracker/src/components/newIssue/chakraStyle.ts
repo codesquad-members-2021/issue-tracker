afe1b38a49b2ff32a6851e7e037a69430ec9d756
somedaycode <somedaycode@gmail.com>
2021-06-10 19:13:22 +0900

    Chore: 이슈 생성 페이지를 위한 ChakraStyle 파일 추가

---
+++ FE/issue-tracker/src/components/newIssue/chakraStyle.ts
@@ -0,0 +1,46 @@
+const titleInput = {
+  marginBottom: '16px',
+  padding: '16px',
+  height: '56px',
+  variant: 'filled',
+  background: 'gr_inputBackground',
+  placeholder: '제목',
+  borderRadius: '14px',
+};
+
+const contentsInput = {
+  size: 'md',
+  height: '291px',
+  padding: '16px 24px',
+  background: 'gr_inputBackground',
+  placeholder: '코멘트를 입력하세요',
+  variant: 'filled',
+};
+
+const menuBtnStyle = {
+  padding: '34px 32px',
+  display: 'flex',
+  width: '100%',
+  height: '96px',
+  background: 'gr_offWhite',
+  variant: 'unstyled',
+  justifyContent: 'space-between',
+};
+
+const menuItemStyle = {
+  display: 'flex',
+  justifyContent: 'space-between',
+};
+
+const checkBoxStyle = {
+  colorScheme: 'cyan',
+  borderColor: 'gr_label',
+};
+
+export {
+  titleInput,
+  contentsInput,
+  checkBoxStyle,
+  menuItemStyle,
+  menuBtnStyle,
+};
