c7832d3b9ac73d8253ca183d979a7a5d1e6d8f32
bibi <bibi6666667@kakao.com>
2021-06-10 17:24:16 +0900

    feat : jwt토큰 검증에 필요한 클래스 생성  #4

---
+++ BE/src/main/java/com/codesquad/issuetracker/config/JwtConfig.java
@@ -0,0 +1,23 @@
+package com.codesquad.issuetracker.config;
+
+import com.codesquad.issuetracker.jwt.BearerAuthInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class JwtConfig implements WebMvcConfigurer {
+
+    private final BearerAuthInterceptor bearerAuthInterceptor;
+
+    public JwtConfig(BearerAuthInterceptor bearerAuthInterceptor) {
+        this.bearerAuthInterceptor = bearerAuthInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(bearerAuthInterceptor)
+                .addPathPatterns("/issue/**");
+    }
+}
---
+++ BE/src/main/java/com/codesquad/issuetracker/jwt/AuthorizationExtractor.java
@@ -0,0 +1,25 @@
+package com.codesquad.issuetracker.jwt;
+
+import jdk.internal.joptsimple.internal.Strings;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+
+@Component
+public class AuthorizationExtractor {
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String ACCESS_TOKEN_TYPE = AuthorizationExtractor.class.getSimpleName();
+
+    // HTTP 요청으로부터 JWT토큰을 추출함
+    public String extract(HttpServletRequest request, String type) {
+        Enumeration<String> headers = request.getHeaders(AUTHORIZATION);
+        while (headers.hasMoreElements()) {
+            String value = headers.nextElement();
+            if (value.toLowerCase().startsWith(type.toLowerCase())) {
+                return value.substring(type.length()).trim();
+            }
+        }
+        return Strings.EMPTY;
+    }
+}
