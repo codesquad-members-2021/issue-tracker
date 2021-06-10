5f32aca5712b8d1e14130d58b4d5af936e8d747c
Dami Kim <dami.k.dev@gmail.com>
2021-06-10 18:16:35 +0900

    feat: 버튼에 Material UI 적용(#29)

--- frontend/src/components/Issues/Issues.jsx
+++ frontend/src/components/Issues/Issues.jsx
@@ -1,17 +1,18 @@
 import styled from "styled-components";
 import IssueList from "./IssueList/IssueList";
-
+import Menu from "./Menu/Menu";
 const Issues = () => {
 	return (
 		<StyledIssues>
+			<Menu></Menu>
 			<IssueList />
 		</StyledIssues>
 	);
 };
 
-export default Issues;
-
 const StyledIssues = styled.div`
 	display: flex;
 	width: 100%;
 `;
+
+export default Issues;
--- frontend/src/styles/StyledButtons.jsx
+++ frontend/src/styles/StyledButtons.jsx
@@ -1,24 +1,48 @@
 import { useContext } from "react";
 // import Button from "@material-ui/core/Button";
 import styled, { ThemeContext } from "styled-components";
-
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import Button from "@material-ui/core/Button";
+import theme from "styles/theme";
 export const LoginButton = ({ children }) => {
-	const { grayScale, border_radius, fontSizes } = useContext(ThemeContext);
+	// const { grayScale, border_radius, fontSizes } = useContext(ThemeContext);
 
 	return (
-		<Button
-			variant="contained"
-			_fontSize={fontSizes.lg}
-			_bgColor={grayScale.title_active}
-			_color={grayScale.off_white}
-			_border_radius={border_radius.xl}
-		>
-			{children}
-		</Button>
+		<></>
+		// <StyledLoginButton
+		// 	variant="contained"
+		// 	_fontSize={fontSizes.lg}
+		// 	_bgColor={grayScale.title_active}
+		// 	_color={grayScale.off_white}
+		// 	_border_radius={border_radius.xl}
+		// >
+		// 	{children}
+		// </StyledLoginButton>
 	);
 };
+export const TabButton = styled(Button)`
+	width: 160px;
+	height: 40px;
+	font-size: 13px;
+	font-weight: bold;
+	color: ${theme.grayScale.label};
+	border: 1px solid ${theme.grayScale.label};
+`;
+export const AddButton = styled(Button)`
+	width: 160px;
+	height: 40px;
+	font-size: 13px;
+	font-weight: bold;
+	background-color: ${theme.colors.blue};
+	color: ${theme.grayScale.off_white};
+	border: 1px solid ${theme.grayScale.label};
+	border-radius: 11px;
+	&:hover {
+		background-color: ${theme.colors.dark_blue};
+	}
+`;
 
-const Button = styled.button`
+const StyledLoginButton = styled.button`
 	width: 340px;
 	height: 64px;
 	font-size: ${props => props._fontSize};
