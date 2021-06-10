02050cc0d6a2c2512279a474371f5207b993b4ca
Lia <kjk0825@postech.ac.kr>
2021-06-10 15:31:29 +0900

    feat: ✨swipe delete 시 alert 후 삭제 기능 추가(#34)

--- ios/IssueTracker/IssueTracker/IssueList/ViewController/IssueListViewController.swift
+++ ios/IssueTracker/IssueTracker/IssueList/ViewController/IssueListViewController.swift
@@ -24,6 +24,7 @@ class IssueListViewController: UIViewController {
         super.viewDidLoad()
         setViewModel(with: IssueListMock.data)
         setting()
+        setObserver()
     }
     
 }
@@ -34,13 +35,34 @@ extension IssueListViewController: UITableViewDelegate {
     func setViewModel(with issues: [Issue]) {
         viewModel = IssueViewModel(issues: issues)
         dataSource = IssueDataSource(viewModel: viewModel)
-        tableViewDelegate = IssueTableDelegate(viewModel: viewModel)
+        tableViewDelegate = IssueTableDelegate()
     }
     
     private func setting() {
         issueTableView.dataSource = dataSource
         issueTableView.delegate = tableViewDelegate
         issueTableView.register(UINib(nibName: IssueCell.reuseIdentifier, bundle: nil), forCellReuseIdentifier: IssueCell.reuseIdentifier)
+    }
+    
+}
+
+
+extension IssueListViewController {
+    
+    private func setObserver() {
+        NotificationCenter.default.addObserver(self, selector: #selector(alertForDelete), name: .deleteIssue, object: nil)
+    }
+    
+    @objc func alertForDelete(_ notification: Notification) {
+        guard let index = notification.userInfo?["index"] as? IndexPath else { return }
+        
+        let alert = UIAlertController(title: "정말로 이 이슈를 삭제하시겠습니까?", message: "", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "취소", style: .default, handler: nil))
+        alert.addAction(UIAlertAction(title: "삭제", style: .destructive) { action in
+            self.viewModel.deleteIssue(at: index.row)
+            self.issueTableView.deleteRows(at: [index], with: UITableView.RowAnimation.automatic)
+        })
+        self.present(alert, animated: true, completion: nil)
     }
     
 }
--- ios/IssueTracker/IssueTracker/IssueList/ViewController/IssueTableDelegate.swift
+++ ios/IssueTracker/IssueTracker/IssueList/ViewController/IssueTableDelegate.swift
@@ -7,36 +7,23 @@
 
 import UIKit
 
-class IssueTableDelegate: NSObject {
-    
-    private var viewModel: IssueViewModel
-    
-    init(viewModel: IssueViewModel) {
-        self.viewModel = viewModel
-        super.init()
-    }
-    
-}
-
-
-extension IssueTableDelegate: UITableViewDelegate {
+class IssueTableDelegate: NSObject, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return UITableView.automaticDimension
     }
 
     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-
-            let share = UIContextualAction(style: .normal, title: "Close") { action, view, completion in
-                completion(true)
-            }
-
-            let delete = UIContextualAction(style: .destructive, title: "Delete") { [weak self] action, view, completion in
-                self?.viewModel.deleteIssue(at: indexPath.row)
-                tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.automatic)
-                completion(true)
-            }
-
+        
+        let share = UIContextualAction(style: .normal, title: "Close") { action, view, completion in
+            completion(true)
+        }
+        
+        let delete = UIContextualAction(style: .destructive, title: "Delete") { action, view, completion in
+            NotificationCenter.default.post(name: .deleteIssue, object: nil, userInfo: ["index": indexPath])
+            completion(true)
+        }
+        
         let configuration = UISwipeActionsConfiguration(actions: [delete, share])
         configuration.performsFirstActionWithFullSwipe = false
         return configuration
