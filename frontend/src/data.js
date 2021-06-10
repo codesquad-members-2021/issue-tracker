49f28716759f1fcafa58e06b0dd82bff1db89bb3
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-06-11 01:10:32 +0900

    feat: 타임스탬프 유틸함수 추가 :

---
+++ frontend/src/data.js
@@ -0,0 +1,18 @@
+export const issues = [
+	{
+		title: "issue-list 만들기",
+		id: 1,
+		labelId: 2,
+		milestoneId: 4,
+		author: "goody",
+		createdAt: "2021-06-10",
+	},
+	{
+		title: "mainPage에 menu 만들기",
+		id: 2,
+		labelId: 2,
+		milestoneId: 4,
+		author: "daisy",
+		createdAt: "2021-06-11",
+	},
+];
