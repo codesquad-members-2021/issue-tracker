034aaee330af89a302a2643c62ac0962ec601b75
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-06-10 11:59:33 +0900

    chore : data.sql에 더미 데이터 추가

--- BE/issue_tracker/src/main/resources/data.sql
+++ BE/issue_tracker/src/main/resources/data.sql
@@ -1,13 +1,31 @@
 -- user
-insert into `user`(id, email, oauth_resource, password, profile_image, username) values
-(1, "yeon@google.com", "Google", "1234", "프로필 이미지 url", "김나연");
+insert into `user`(id, email, oauth_resource, password, profile_image, username)
+values (1, "yeon@google.com", "Google", "1234", "프로필 이미지 url", "yeon");
+insert into `user`(id, email, oauth_resource, password, profile_image, username)
+values (2, "shion@github.com", "GitHub", "1234", "프로필 이미지 url", "shion");
 
 -- milestone
-insert into milestone(id, content, created_date, due_date, is_open, title) values
-(1, "백엔드 1주차 마일스톤 내용", "2021-06-01", "2021-06-15", true, "BE 1주차");
+insert into milestone(id, content, created_date, due_date, is_open, title)
+values (1, "백엔드 1주차 마일스톤 내용", "2021-06-01", "2021-06-15", true, "BE 1주차");
 
 -- issue
-insert into issue(id, created_time, is_open, issue_number, title, milestone_id, writer_id) values
-(1, "2021-06-10 21:00", true, 1, "이슈 제목1", 1, 1);
+insert into issue(id, created_time, is_open, issue_number, title, milestone_id, writer_id)
+values (1, Current_Timestamp, true, 1, "이슈 제목1", 1, 1),
+       (2, Current_Timestamp, true, 2, "이슈 제목2", 1, 2);
 
+-- label
+insert into label(id, title, content, color)
+values (1, "BE", "백엔드 라벨", "#3DDCFF");
+insert into label(id, title, content, color)
+values (2, "FE", "프론트엔드 라벨", "#3DDCFF");
 
+-- issue_label
+insert into issue_label(issue_id, label_id)
+values (1, 1),
+       (1, 2),
+       (2, 1);
+
+-- issue_assignee
+insert into issue_assignee(issue_id, user_id)
+values (1, 1),
+       (1, 2);
