0d099913b96d8ee37df9b761d5655cac092551de
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2021-06-10 17:54:11 +0900

    refactor:(#27) 파일명 변경

---
+++ frontend/src/components/atom/LabelBadge.tsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+interface LabelBadgeProps {
+  color: string;
+  desc?: string;
+}
+function LabelBadge({ color, desc }: LabelBadgeProps) {
+  return <LabelBadgeBlock color={color}>{desc}</LabelBadgeBlock>;
+}
+
+const LabelBadgeBlock = styled.div<LabelBadgeProps>`
+  margin-left: 10px;
+  padding: 0 5px;
+  height: 25px;
+  width: auto;
+  max-width: 100px;
+  text-align: center;
+  border-radius: 30px;
+  background-color: ${(props) => props.color};
+  color: ${({ theme }) => theme.color.white};
+`;
+
+export default LabelBadge;
