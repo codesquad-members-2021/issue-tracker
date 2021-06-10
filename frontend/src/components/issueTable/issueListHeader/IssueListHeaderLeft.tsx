95e3e38c941045e4e754715ba2c5dfd69045b308
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2021-06-10 23:28:19 +0900

    refactor:(#27) issue header컴포넌트 분리 중

---
+++ frontend/src/components/issueTable/issueListHeader/IssueListHeaderLeft.tsx
@@ -0,0 +1,40 @@
+import React, { ReactElement } from 'react';
+import styled from 'styled-components';
+import CheckBox from '../../atom/CheckBox';
+import AdjustRoundedIcon from '@material-ui/icons/AdjustRounded';
+import CheckRoundedIcon from '@material-ui/icons/CheckRounded';
+import { useSetRecoilState } from 'recoil';
+import { issueTypeState } from '../../../store/issueInfoStore';
+
+interface Props {}
+
+export default function IssueListHeaderLeft({}: Props): ReactElement {
+  const setIssueType = useSetRecoilState(issueTypeState);
+
+  const handleOpenClick = () => setIssueType('open');
+  const handleCloseClick = () => setIssueType('close');
+  return (
+    <IssueListHeaderLeftBlock>
+      <CheckBox />
+      <div className='issue-header__filter-tab'>
+        <div onClick={handleOpenClick}>
+          <AdjustRoundedIcon /> <span>열린 이슈 ()</span>
+        </div>
+        <div onClick={handleCloseClick}>
+          <CheckRoundedIcon /> <span>닫힌 이슈 ()</span>
+        </div>
+      </div>
+    </IssueListHeaderLeftBlock>
+  );
+}
+
+const IssueListHeaderLeftBlock = styled.div`
+  .issue-header__filter-tab {
+    display: flex;
+    div {
+      display: flex;
+      align-items: center;
+      margin-right: 10px;
+    }
+  }
+`;
---
+++ frontend/src/components/issueTable/issueListHeader/IssueListHeaderRight.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from 'react';
+import styled from 'styled-components';
+interface Props {}
+
+export default function IssueListHeaderRight({}: Props): ReactElement {
+  return <IssueListHeaderRightBlock></IssueListHeaderRightBlock>;
+}
+
+const IssueListHeaderRightBlock = styled.div``;
