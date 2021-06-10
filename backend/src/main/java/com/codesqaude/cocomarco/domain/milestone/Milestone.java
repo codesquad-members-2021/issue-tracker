e03c32c6df80fc6f9e842dfcde214097083d4f7b
95degree <95degree@naver.com>
2021-06-10 10:49:01 +0900

    feat : Milestone 엔티티에 Getter,AllArgsConstructor,NoArgsConstructor 추가

--- backend/src/main/java/com/codesqaude/cocomarco/domain/milestone/Milestone.java
+++ backend/src/main/java/com/codesqaude/cocomarco/domain/milestone/Milestone.java
@@ -1,6 +1,9 @@
 package com.codesqaude.cocomarco.domain.milestone;
 
 import com.codesqaude.cocomarco.domain.issue.Issue;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -8,6 +11,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Milestone {
 
     @Id
