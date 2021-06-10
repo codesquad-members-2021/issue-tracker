d9ffa162f2ba62d0b0891a3d264122758ac72b32
Ray suh <readingod@naver.com>
2021-06-10 15:48:00 +0900

    feath: (#6) HexString을 UIColor로 변경하는 메서드 생성

--- IssueTracker/IssueTracker/Presenter/Controller/IssueListViewController.swift
+++ IssueTracker/IssueTracker/Presenter/Controller/IssueListViewController.swift
@@ -13,8 +13,12 @@ class IssueListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupIssueFilterButton()
+        setupDelegate()
         bind()
     }
+}
+
+private extension IssueListViewController {
     
     private func setupIssueFilterButton() {
         issueFilterButton.rx.tap
@@ -24,12 +28,28 @@ class IssueListViewController: UIViewController {
             }).disposed(by: rx.disposeBag)
     }
     
+    private func setupDelegate() {
+        issueCollectionView.rx.setDelegate(self).disposed(by: rx.disposeBag)
+    }
+}
+
+private extension IssueListViewController {
+    
     private func bind() {
         viewModel.getIssueList()
         
         viewModel.issueList
             .drive(issueCollectionView.rx.items(cellIdentifier: IssueCell.identifier, cellType: IssueCell.self)) { _, issue, cell in
-                cell.configure(issue.title, issue.comment, milestone: issue.milestone, label: issue.labels.first!.name)
+                cell.configure(issue.title, issue.comment, milestone: issue.milestone, labels: issue.labels)
             }.disposed(by: rx.disposeBag)
+    }
+}
+
+extension IssueListViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = issueCollectionView.frame.width * 0.8
+        let height = issueCollectionView.frame.height * 0.3
+        return CGSize(width: width, height: height)
     }
 }
--- IssueTracker/IssueTracker/Presenter/View/IssueCell.swift
+++ IssueTracker/IssueTracker/Presenter/View/IssueCell.swift
@@ -3,16 +3,19 @@ import UIKit
 class IssueCell: UICollectionViewCell {
     
     static let identifier = "IssueCell"
-    
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var commnetLabel: UILabel!
     @IBOutlet weak var milestoneLabel: UILabel!
-    @IBOutlet weak var labelLabel: UILabel!
+    @IBOutlet weak var labelStackView: UIStackView!
     
-    func configure(_ title:String, _ comment:String, milestone:String, label:String) {
+    func configure(_ title:String, _ comment:String, milestone:String, labels:[Label]) {
         self.titleLabel.text = title
         self.commnetLabel.text = comment
         self.milestoneLabel.text = milestone
-        self.labelLabel.text = label
+        for index in 0..<labels.count {
+            let label = LabelView()
+            label.configure(labels[index].name, labels[index].colorCode)
+            labelStackView.addArrangedSubview(label)
+        }
     }
 }
