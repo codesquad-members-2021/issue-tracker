7b5c7c89e1b6c1442c558b9012e5b19ae973aae2
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-06-11 01:13:31 +0900

    chore : 테스트를 위한 더미 데이터 수정

--- BE/issue_tracker/src/main/resources/data.sql
+++ BE/issue_tracker/src/main/resources/data.sql
@@ -9,9 +9,9 @@ insert into milestone(id, content, created_date, due_date, is_open, title)
 values (1, "백엔드 1주차 마일스톤 내용", "2021-06-01", "2021-06-15", true, "BE 1주차");
 
 -- issue
-insert into issue(id, created_time, is_open, issue_number, title, milestone_id, writer_id)
+insert into issue(id, created_time, is_open, issue_number, title, writer_id, milestone_id)
 values (1, CURRENT_TIMESTAMP, true, 1, "이슈 제목1", 1, 1),
-       (2, CURRENT_TIMESTAMP, false, 2, "이슈 제목2", 1, 2);
+       (2, CURRENT_TIMESTAMP, false, 2, "이슈 제목2", 2, null);
 
 -- label
 insert into label(id, title, content, color)
@@ -22,8 +22,7 @@ values (2, "FE", "프론트엔드 라벨", "#3DDCFF");
 -- issue_label
 insert into issue_label(issue_id, label_id)
 values (1, 1),
-       (1, 2),
-       (2, 1);
+       (1, 2);
 
 -- issue_assignee
 insert into issue_assignee(issue_id, user_id)
