455f362bbb6559973a7ce35406170ca541edcff1
95degree <95degree@naver.com>
2021-06-10 10:49:45 +0900

    feat : MilestoneRepository 생성

---
+++ backend/src/main/java/com/codesqaude/cocomarco/domain/milestone/MilestoneRepository.java
@@ -0,0 +1,8 @@
+package com.codesqaude.cocomarco.domain.milestone;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MilestoneRepository extends JpaRepository<Milestone,Long> {
+}
