adabd4c4f2f310b7f8c0b5be3b7c9d251b51d18a
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-06-11 01:13:00 +0900

    feat : 이슈에 해당하는 마일스톤이 없는 경우 null을 Optional로 감싸서 값을 넣도록 구현

--- BE/issue_tracker/src/main/java/team02/issue_tracker/dto/issue/DetailIssueResponse.java
+++ BE/issue_tracker/src/main/java/team02/issue_tracker/dto/issue/DetailIssueResponse.java
@@ -2,23 +2,32 @@ package team02.issue_tracker.dto.issue;
 
 import lombok.Getter;
 import team02.issue_tracker.domain.Issue;
+import team02.issue_tracker.domain.Milestone;
 import team02.issue_tracker.dto.CommentResponse;
 import team02.issue_tracker.dto.MilestoneDetailResponse;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Getter
 public class DetailIssueResponse extends AbstractIssueResponse {
 
-    private MilestoneDetailResponse milestone;
+    private Optional<MilestoneDetailResponse> milestone;
     private List<CommentResponse> comments;
 
     public DetailIssueResponse(Issue issue) {
         super(issue);
-        this.milestone = new MilestoneDetailResponse(issue.getMilestone());
+        this.milestone = toMilestoneDetailResponse(issue.getMilestone());
         this.comments = issue.getComments().stream()
                 .map(CommentResponse::new)
                 .collect(Collectors.toList());
+    }
+
+    private Optional<MilestoneDetailResponse> toMilestoneDetailResponse(Milestone milestone) {
+        if (milestone == null) {
+            return Optional.empty();
+        }
+        return Optional.of(new MilestoneDetailResponse(milestone));
     }
 }
