afae3229f248db498859db11a5f54ee387fe7285
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-06-10 16:52:42 +0900

    fix: 파일 컨트롤러 수정
    멀티파트를 기본 contents 타입으로 인식하도록 변경(다른 이유도 있지만 스웨거에서 예쁘게 보임). 빈 파일은 400에러 떨굴 수 있도록 MultipartFileNotFoundException 추가

---
+++ be/issue-tracker-be/src/main/java/com/codesquad/issuetracker/file/MultipartFileNotFoundException.java
@@ -0,0 +1,13 @@
+package com.codesquad.issuetracker.file;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class MultipartFileNotFoundException extends RuntimeException {
+    private static final String DEFAULT_MESSAGE = "전송된 파일이 없습니다.";
+
+    public MultipartFileNotFoundException() {
+        super(DEFAULT_MESSAGE);
+    }
+}
