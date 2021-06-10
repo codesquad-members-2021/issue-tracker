d6276fb3e0a71faca36cea71a4725c1044bb8576
somedaycode <somedaycode@gmail.com>
2021-06-10 19:12:49 +0900

    Chore: #18 SelectAssignee 컴포넌트 UI 복제하여 기본적인 구조를 완성

---
+++ FE/issue-tracker/src/components/newIssue/SelectMilestone.tsx
@@ -0,0 +1,80 @@
+import styled from 'styled-components';
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Button,
+  Checkbox,
+} from '@chakra-ui/react';
+
+import { Avatar } from '@chakra-ui/avatar';
+
+import { ReactComponent as PlusIcon } from '@assets/plus.svg';
+import MenuTitle from '@components/common/MenuTitle';
+import { checkBoxStyle, menuItemStyle, menuBtnStyle } from './chakraStyle';
+
+function SelectMilestone() {
+  return (
+    <Wrap>
+      <Menu>
+        <MenuButton
+          {...menuBtnStyle}
+          as={Button}
+          rightIcon={<PlusIcon />}
+          textAlign="left"
+          _focus={{ border: 0 }}
+        >
+          담당자
+        </MenuButton>
+        <MenuList>
+          <MenuTitle>담당자 추가</MenuTitle>
+          <MenuItem {...menuItemStyle}>
+            <ItemWrap>
+              <Avatar className="avatar" size="sm" src="./janmang.jpeg" />
+              <Text>Oni</Text>
+            </ItemWrap>
+            <Checkbox {...checkBoxStyle} />
+          </MenuItem>
+          <MenuItem {...menuItemStyle}>
+            <ItemWrap>
+              <Avatar className="avatar" size="sm" src="./janmang.jpeg" />
+              <Text>Oni</Text>
+            </ItemWrap>
+            <Checkbox {...checkBoxStyle} />
+          </MenuItem>
+        </MenuList>
+      </Menu>
+      <AddList>
+        <li>
+          <Avatar className="avatar" size="sm" src="./janmang.jpeg" />
+          <Text>Oni</Text>
+        </li>
+      </AddList>
+      <AddList>
+        <li>
+          <Avatar className="avatar" size="sm" src="./janmang.jpeg" />
+          <Text>Oni</Text>
+        </li>
+      </AddList>
+    </Wrap>
+  );
+}
+
+export default SelectMilestone;
+
+const Wrap = styled.div``;
+
+const ItemWrap = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Text = styled.span`
+  margin-left: 4px;
+  color: ${({ theme }) => theme.colors.gr_label};
+`;
+
+const AddList = styled.ul`
+  padding: 8px 32px;
+`;
