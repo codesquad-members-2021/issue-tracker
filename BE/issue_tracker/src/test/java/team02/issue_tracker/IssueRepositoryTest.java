4af1fc97dc32567bb1fe05d1d50941dd989206dc
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-06-10 15:56:10 +0900

    test : 마일스톤이 갖고 있는 이슈들의 개수 반환 기능 테스트 추가

--- BE/issue_tracker/src/test/java/team02/issue_tracker/IssueRepositoryTest.java
+++ BE/issue_tracker/src/test/java/team02/issue_tracker/IssueRepositoryTest.java
@@ -5,12 +5,15 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 import team02.issue_tracker.domain.Issue;
+import team02.issue_tracker.exception.IssueNotFoundException;
 import team02.issue_tracker.repository.IssueRepository;
 
 import java.util.List;
 
 @SpringBootTest
+@Transactional
 public class IssueRepositoryTest {
 
     @Autowired
@@ -20,6 +23,17 @@ public class IssueRepositoryTest {
     @DisplayName("issueRepository에서 전체 issue가 잘 반환되는지 확인한다.")
     void findAll() {
         List<Issue> issues = issueRepository.findAll();
-        Assertions.assertThat(issues.size()).isEqualTo(1);
+        Assertions.assertThat(issues.size()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("마일스톤에서 이슈의 개수가 올바르게 반환되는지 확인한다.")
+    void test() {
+        Issue issue = issueRepository.findById(1L).orElseThrow(IssueNotFoundException::new);
+        int issueCount = issue.getMilestone().getTotalIssueCount();
+        int openIssueCount = issue.getMilestone().getOpenIssueCount();
+
+        Assertions.assertThat(issueCount).isEqualTo(2);
+        Assertions.assertThat(openIssueCount).isEqualTo(1);
     }
 }
