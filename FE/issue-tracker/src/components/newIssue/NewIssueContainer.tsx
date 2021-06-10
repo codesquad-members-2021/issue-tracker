29328797471438c44e80ee3c080a1741764e0e48
somedaycode <somedaycode@gmail.com>
2021-06-10 19:09:31 +0900

    Chore: #18 콘텐츠(Text, selection)과 버튼을 포함하는 컨테이너 추가

---
+++ FE/issue-tracker/src/components/newIssue/NewIssueContainer.tsx
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+
+import { Avatar } from '@chakra-ui/avatar';
+
+import TextBox from './TextBox';
+import SelectBox from './SelectBox';
+
+function NewIssueContainer() {
+  return (
+    <Wrap>
+      <ContentsBox>
+        <Avatar className="avatar" size="md" src="./janmang.jpeg" />
+        <TextBox />
+        <SelectBox />
+      </ContentsBox>
+      <ButtonBox></ButtonBox>
+    </Wrap>
+  );
+}
+
+export default NewIssueContainer;
+
+const Wrap = styled.section`
+  width: 100%;
+  height: 100%;
+  height: 569px;
+`;
+
+const ContentsBox = styled.div`
+  display: flex;
+  margin: 16px 0;
+  padding: 32px 0;
+  width: 100%;
+  border-top: 1px solid ${({ theme }) => theme.colors.gr_line};
+  border-bottom: 1px solid ${({ theme }) => theme.colors.gr_line};
+`;
+
+const ButtonBox = styled.div`
+  display: flex;
+  width: 100%;
+  height: 80px;
+  justify-content: flex-end;
+`;
