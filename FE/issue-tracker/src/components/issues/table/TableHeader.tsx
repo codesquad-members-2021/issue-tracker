5f18a4ee44e1a9d5335debe01bc0ef05a123df3e
somedaycode <somedaycode@gmail.com>
2021-06-10 15:04:03 +0900

    Chore: #10 테이블 헤더 메뉴에 드롭다운 추가

--- FE/issue-tracker/src/components/issues/table/TableHeader.tsx
+++ FE/issue-tracker/src/components/issues/table/TableHeader.tsx
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
 
+import { ReactComponent as DropDownIcon } from '@assets/dropDown.svg';
+import { Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/menu';
+import { Button } from '@chakra-ui/button';
+import { menuBtnStyle } from './style';
+
 function TableHeader() {
   return (
     <TableHeaderWrap>
@@ -12,10 +17,102 @@ function TableHeader() {
       </HeaderLeft>
       <HeaderRight>
         <FilterLists>
-          <li>담당자</li>
-          <li>레이블</li>
-          <li>마일스톤</li>
-          <li>작성자</li>
+          <Menu>
+            <MenuButton
+              className="menu-title"
+              {...menuBtnStyle}
+              as={Button}
+              rightIcon={<DropDownIcon />}
+            >
+              담당자
+            </MenuButton>
+            <MenuList>
+              <div>
+                <span>담당자 필터</span>
+              </div>
+              <MenuItem>
+                <span>담당자가 없는 이슈</span>
+                <input type="checkbox" name="openIssue" />
+              </MenuItem>
+              <MenuItem>
+                <span>프로필 Q</span>
+                <input type="checkbox" name="openIssue" />
+              </MenuItem>
+            </MenuList>
+          </Menu>
+          <Menu>
+            <MenuButton
+              className="menu-title"
+              {...menuBtnStyle}
+              as={Button}
+              rightIcon={<DropDownIcon />}
+            >
+              레이블
+            </MenuButton>
+            <MenuList>
+              <div>
+                <span>레이블 필터</span>
+              </div>
+              <MenuItem>
+                <span>레이블이 없는 이슈</span>
+                <input type="checkbox" name="openIssue" />
+              </MenuItem>
+              <MenuItem>
+                color Bug
+                <input type="checkbox" name="openIssue" />
+              </MenuItem>
+              <MenuItem>
+                color documentation
+                <input type="checkbox" name="openIssue" />
+              </MenuItem>
+            </MenuList>
+          </Menu>
+          <Menu>
+            <MenuButton
+              className="menu-title"
+              {...menuBtnStyle}
+              as={Button}
+              rightIcon={<DropDownIcon />}
+            >
+              마일스톤
+            </MenuButton>
+            <MenuList>
+              <div>
+                <span>마일스톤 필터</span>
+              </div>
+              <MenuItem>
+                마일스톤 없는 필터
+                <input type="checkbox" name="openIssue" />
+              </MenuItem>
+              <MenuItem>
+                마스터즈 코스
+                <input type="checkbox" name="openIssue" />
+              </MenuItem>
+            </MenuList>
+          </Menu>
+          <Menu>
+            <MenuButton
+              className="menu-title"
+              {...menuBtnStyle}
+              as={Button}
+              rightIcon={<DropDownIcon />}
+            >
+              작성자
+            </MenuButton>
+            <MenuList>
+              <div>
+                <span>작성자 필터</span>
+              </div>
+              <MenuItem>
+                프로필사진 Q
+                <input type="checkbox" name="openIssue" />
+              </MenuItem>
+              <MenuItem>
+                프로필사진 Eve
+                <input type="checkbox" name="openIssue" />
+              </MenuItem>
+            </MenuList>
+          </Menu>
         </FilterLists>
       </HeaderRight>
     </TableHeaderWrap>
@@ -52,15 +149,13 @@ const HeaderRight = styled.div`
   display: flex;
 `;
 
-const FilterLists = styled.ul`
+const FilterLists = styled.div`
   display: flex;
 
-  li {
+  .menu-title {
+    width: 100px;
     display: flex;
-    align-items: center;
-    margin-left: 24px;
-    width: 65px;
-    height: 32px;
+    justify-content: space-between;
   }
 `;
 
