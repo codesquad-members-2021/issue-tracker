4111a11a67f6f35749bda439cd132d750c005cb2
woody <woojihye2339@gmail.com>
2021-06-10 15:29:40 +0900

    feat : User 클래스 수정 #4
    > GeneratedValue 어노테이션
      - id 값 자동 생성을 위해 추가

--- BE/src/main/java/com/codesquad/issuetracker/domain/User.java
+++ BE/src/main/java/com/codesquad/issuetracker/domain/User.java
@@ -1,31 +1,55 @@
 package com.codesquad.issuetracker.domain;
 
+import com.codesquad.issuetracker.domain.oauth.GitHubUser;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    
+
     @JsonProperty("user_id")
     private String userId;
 
-    private User(Long id, String name, String userId) {
-        this.id = id;
+    private User(String name, String userId) {
         this.name = name;
         this.userId = userId;
+    }
+
+    private User(Long id, String name, String userId) {
+        this(name, userId);
+        this.id = id;
     }
 
     public User() {
 
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
     public User create(Long id, String name, String userId) {
         return new User(id, name, userId);
+    }
+
+    public static User githubUserToUser(GitHubUser githubUser) {
+        return new User(githubUser.getName(), githubUser.getLogin());
     }
 }
