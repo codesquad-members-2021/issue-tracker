b45aef94e96e5d7300f129d1e936e3b562fd012c
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-06-10 16:01:07 +0900

    feat : getDetailIssueResponse 메소드 추가

--- BE/issue_tracker/src/main/java/team02/issue_tracker/service/IssueService.java
+++ BE/issue_tracker/src/main/java/team02/issue_tracker/service/IssueService.java
@@ -1,8 +1,12 @@
 package team02.issue_tracker.service;
 
 import org.springframework.stereotype.Service;
-import team02.issue_tracker.dto.DataResponse;
+import team02.issue_tracker.domain.Issue;
+import team02.issue_tracker.dto.wrapping_dto.DataResponse;
+import team02.issue_tracker.dto.wrapping_dto.ListDataResponse;
+import team02.issue_tracker.dto.DetailIssueResponse;
 import team02.issue_tracker.dto.IssueResponse;
+import team02.issue_tracker.exception.IssueNotFoundException;
 import team02.issue_tracker.repository.IssueRepository;
 
 import java.util.List;
@@ -17,11 +21,17 @@ public class IssueService {
         this.issueRepository = issueRepository;
     }
 
-    public DataResponse getAllIssueResponses() {
+    public ListDataResponse getAllIssueResponses() {
         List<IssueResponse> issueResponses = issueRepository.findAll().stream()
                 .map(IssueResponse::new)
                 .collect(Collectors.toList());
 
-        return new DataResponse(issueResponses);
+        return new ListDataResponse(issueResponses);
+    }
+
+    public DataResponse getDetailIssueResponse(Long issueId) {
+        Issue issue = issueRepository.findById(issueId).orElseThrow(IssueNotFoundException::new);
+        DetailIssueResponse issueResponse = new DetailIssueResponse(issue);
+        return new DataResponse(issueResponse);
     }
 }
