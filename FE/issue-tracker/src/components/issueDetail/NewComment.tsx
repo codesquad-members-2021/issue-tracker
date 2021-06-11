09cc63e432d1f931ccf4e9888d8d8b22c10620b4
eve712 <62237639+eve712@users.noreply.github.com>
2021-06-11 16:32:36 +0900

    Style: #25 커멘트 작성하는 컴포넌트 추가

---
+++ FE/issue-tracker/src/components/issueDetail/NewComment.tsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+import { Avatar } from '@chakra-ui/react';
+import TextBox from '@components/newIssue/TextBox';
+
+function NewComment() {
+  return (
+    <NewCommentContainer>
+      <AvatarBox>
+        <Avatar src="./janmang.jpeg" />
+      </AvatarBox>
+      <TextBox />
+    </NewCommentContainer>
+  );
+}
+
+export default NewComment;
+
+const NewCommentContainer = styled.div`
+  display: flex;
+`;
+
+const AvatarBox = styled.div``;
