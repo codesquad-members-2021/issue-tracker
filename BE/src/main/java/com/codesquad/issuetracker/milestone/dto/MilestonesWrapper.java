ca1842a683506833e42274de3178c7fce2c2f395
pbg0205 <pbg0205@naver.com>
2021-06-11 12:25:03 +0900

    refactor : Add AllArgsConstructor annotation(#14)

--- BE/src/main/java/com/codesquad/issuetracker/milestone/dto/MilestonesWrapper.java
+++ BE/src/main/java/com/codesquad/issuetracker/milestone/dto/MilestonesWrapper.java
@@ -1,17 +1,16 @@
 package com.codesquad.issuetracker.milestone.dto;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class MilestonesWrapper {
 
     private List<MilestoneResponseDto> milestoneResponseDtoList;
-
-    private MilestonesWrapper(List<MilestoneResponseDto> milestoneResponseDtoList) {
-        this.milestoneResponseDtoList = milestoneResponseDtoList;
-    }
 
     public static MilestonesWrapper create(List<MilestoneResponseDto> milestoneResponseDtoList) {
         return new MilestonesWrapper(milestoneResponseDtoList);
