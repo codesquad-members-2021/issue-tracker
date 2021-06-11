644fe5f5fe0fa2b7171a172cfa11adfb3688958a
K <PizzaCola.K@gmail.com>
2021-06-11 16:33:46 +0900

    refactor: Change Milestone Wrapper field name
    - milestone과 milestones로 수정

--- BE/src/main/java/com/codesquad/issuetracker/milestone/dto/MilestoneWrapper.java
+++ BE/src/main/java/com/codesquad/issuetracker/milestone/dto/MilestoneWrapper.java
@@ -1,15 +1,14 @@
 package com.codesquad.issuetracker.milestone.dto;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class MilestoneWrapper {
 
-    private MilestoneResponseDto milestoneResponseDto;
-
-    private MilestoneWrapper(MilestoneResponseDto milestoneResponseDto) {
-        this.milestoneResponseDto = milestoneResponseDto;
-    }
+    private final MilestoneResponseDto milestone;
 
     public static MilestoneWrapper create(MilestoneResponseDto milestoneResponseDto) {
         return new MilestoneWrapper(milestoneResponseDto);
--- BE/src/main/java/com/codesquad/issuetracker/milestone/dto/MilestonesWrapper.java
+++ BE/src/main/java/com/codesquad/issuetracker/milestone/dto/MilestonesWrapper.java
@@ -3,14 +3,15 @@ package com.codesquad.issuetracker.milestone.dto;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
 @Getter
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class MilestonesWrapper {
 
-    private List<MilestoneResponseDto> milestoneResponseDtoList;
+    private final List<MilestoneResponseDto> milestones;
 
     public static MilestonesWrapper create(List<MilestoneResponseDto> milestoneResponseDtoList) {
         return new MilestonesWrapper(milestoneResponseDtoList);
