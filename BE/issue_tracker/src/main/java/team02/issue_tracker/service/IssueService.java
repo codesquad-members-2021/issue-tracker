3604de04566f2f119d80528b565479d266e66dd2
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-06-12 07:24:52 +0900

    feat : 새로운 Issue 생성 기능 구현
    IssueRequest DTO를 통해 Comment, Issue, IssueLabel, IssueAssignee를 생성하고 DB에 저장

--- BE/issue_tracker/src/main/java/team02/issue_tracker/service/IssueService.java
+++ BE/issue_tracker/src/main/java/team02/issue_tracker/service/IssueService.java
@@ -1,7 +1,6 @@
 package team02.issue_tracker.service;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import team02.issue_tracker.domain.*;
 import team02.issue_tracker.dto.issue.IssueRequest;
 import team02.issue_tracker.dto.wrapping.DataResponse;
@@ -12,10 +11,7 @@ import team02.issue_tracker.exception.IssueNotFoundException;
 import team02.issue_tracker.exception.LabelNotFoundException;
 import team02.issue_tracker.exception.MilestoneNotFoundException;
 import team02.issue_tracker.exception.UserNotFoundException;
-import team02.issue_tracker.repository.IssueRepository;
-import team02.issue_tracker.repository.LabelRepository;
-import team02.issue_tracker.repository.MilestoneRepository;
-import team02.issue_tracker.repository.UserRepository;
+import team02.issue_tracker.repository.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,12 +24,19 @@ public class IssueService {
     private final UserRepository userRepository;
     private final LabelRepository labelRepository;
     private final MilestoneRepository milestoneRepository;
+    private final IssueLabelRepository issueLabelRepository;
+    private final IssueAssigneeRepository issueAssigneeRepository;
+    private final CommentRepository commentRepository;
 
-    public IssueService(IssueRepository issueRepository, UserRepository userRepository, LabelRepository labelRepository, MilestoneRepository milestoneRepository) {
+    public IssueService(IssueRepository issueRepository, UserRepository userRepository, LabelRepository labelRepository,
+                        MilestoneRepository milestoneRepository, IssueLabelRepository issueLabelRepository, IssueAssigneeRepository issueAssigneeRepository, CommentRepository commentRepository) {
         this.issueRepository = issueRepository;
         this.userRepository = userRepository;
         this.labelRepository = labelRepository;
         this.milestoneRepository = milestoneRepository;
+        this.issueLabelRepository = issueLabelRepository;
+        this.issueAssigneeRepository = issueAssigneeRepository;
+        this.commentRepository = commentRepository;
     }
 
     public ListDataResponse getAllIssueResponses() {
@@ -50,36 +53,51 @@ public class IssueService {
         return new DataResponse(issueResponse);
     }
 
-    @Transactional
     public void addIssue(IssueRequest issueRequest) {
         User writer = userRepository.findById(issueRequest.getUserId()).orElseThrow(UserNotFoundException::new);
-        List<Label> labels = getLabels(issueRequest.getLabelIds());
         Milestone milestone = milestoneRepository.findById(issueRequest.getMilestoneId()).orElseThrow(MilestoneNotFoundException::new);
-        List<User> assignees = getAssignees(issueRequest.getAssigneeIds());
 
-        Issue issue = issueRequest.toIssue(writer, labels, milestone, assignees);
-        issueRepository.save(issue);
+        Issue issue = saveIssue(issueRequest, writer, milestone);
+        saveComment(issueRequest, writer, issue);
+
+        List<IssueLabel> issueLabels = getIssueLabels(issue, issueRequest.getLabelIds());
+        issueLabelRepository.saveAll(issueLabels);
+
+        List<IssueAssignee> issueAssignees = getIssueAssignees(issue, issueRequest.getAssigneeIds());
+        issueAssigneeRepository.saveAll(issueAssignees);
     }
 
-    private List<Label> getLabels(List<Long> labelIds) {
-        List<Label> labels = new ArrayList<>();
+    private Issue saveIssue(IssueRequest issueRequest, User writer, Milestone milestone) {
+        Issue issue = issueRequest.toIssue(writer);
+        issue.addMilestone(milestone);
+        return issueRepository.save(issue);
+    }
+
+    private void saveComment(IssueRequest issueRequest, User writer, Issue issue) {
+        Comment comment = issueRequest.toComment(writer);
+        comment.addIssue(issue);
+        commentRepository.save(comment);
+    }
+
+    private List<IssueLabel> getIssueLabels(Issue issue, List<Long> labelIds) {
+        List<IssueLabel> issueLabels = new ArrayList<>();
         labelIds.stream()
-                .map(labelId -> {
+                .forEach(labelId -> {
                     Label label = labelRepository.findById(labelId).orElseThrow(LabelNotFoundException::new);
-                    labels.add(label);
-                    return label;
+                    issueLabels.add(new IssueLabel(issue, label));
                 });
-        return labels;
+
+        return issueLabels;
     }
 
-    private List<User> getAssignees(List<Long> assigneeIds) {
-        List<User> assignees = new ArrayList<>();
+    private List<IssueAssignee> getIssueAssignees(Issue issue, List<Long> assigneeIds) {
+        List<IssueAssignee> issueAssignees = new ArrayList<>();
         assigneeIds.stream()
-                .map(assigneeId -> {
+                .forEach(assigneeId -> {
                     User assignee = userRepository.findById(assigneeId).orElseThrow(UserNotFoundException::new);
-                    assignees.add(assignee);
-                    return assignee;
+                    issueAssignees.add(new IssueAssignee(issue, assignee));
                 });
-        return assignees;
+
+        return issueAssignees;
     }
 }
