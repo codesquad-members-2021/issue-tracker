6ec6a5d7ec3fe1229ccf1888dabeb8417b47a0b1
zeke <yangsin1031@naver.com>
2021-06-11 17:50:57 +0900

    feat: ✨ AddIssueButton 구현 (#27)

---
+++ iOS/issue-tracker/issue-tracker/View/IssueList/AddIssueButton.swift
@@ -0,0 +1,38 @@
+//
+//  AddIssueButton.swift
+//  issue-tracker
+//
+//  Created by 양준혁 on 2021/06/11.
+//
+
+import UIKit
+
+class AddIssueButton: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setButton()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func draw(_ rect: CGRect) {
+        let path = UIBezierPath()
+        
+        path.move(to: CGPoint(x: self.bounds.width * 0.25, y: self.bounds.height * 0.5))
+        path.addLine(to: CGPoint(x: self.bounds.width * 0.75, y: self.bounds.height * 0.5))
+        path.move(to: CGPoint(x: self.bounds.width * 0.5, y: self.bounds.height * 0.25))
+        path.addLine(to: CGPoint(x: self.bounds.width * 0.5, y: self.bounds.height * 0.75))
+        UIColor.white.set()
+        path.stroke()
+    }
+    
+    func setButton() {
+        clipsToBounds = true
+        layer.cornerRadius = self.bounds.size.width * 0.5
+        backgroundColor = .systemBlue
+        
+    }
+}
