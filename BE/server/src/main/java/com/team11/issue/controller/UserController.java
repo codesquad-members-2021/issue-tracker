52d1b95bde0f6aca21cbfa2052ba29199a869911
malaheaven <yunhg95@gmail.com>
2021-06-11 16:18:06 +0900

    [#30] feat: :sparkles: jwtToken 발행하기 구현

--- BE/server/src/main/java/com/team11/issue/controller/UserController.java
+++ BE/server/src/main/java/com/team11/issue/controller/UserController.java
@@ -1,14 +1,12 @@
 package com.team11.issue.controller;
 
+import com.team11.issue.dto.ResponseDTO;
 import com.team11.issue.dto.user.LoginRequestDTO;
 import com.team11.issue.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/user")
@@ -26,8 +24,14 @@ public class UserController {
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody LoginRequestDTO loginRequestDTO) {
         logger.info("로그인 요청");
-        userService.login(loginRequestDTO);
-        return ResponseEntity.ok().body("LoginResposeDto 변경 예정");
+        return ResponseEntity.ok().body(userService.login(loginRequestDTO));
+    }
+
+    @PutMapping("/logout")
+    public ResponseEntity logout(@RequestHeader String authorization) {
+        logger.info("로그아웃 요청");
+        userService.logout(authorization);
+        return ResponseEntity.ok().body(new ResponseDTO("logout"));
     }
 
 
--- BE/server/src/main/java/com/team11/issue/service/UserService.java
+++ BE/server/src/main/java/com/team11/issue/service/UserService.java
@@ -1,18 +1,24 @@
 package com.team11.issue.service;
 
+import com.team11.issue.domain.User;
 import com.team11.issue.dto.oauth.UserInfoDTO;
 import com.team11.issue.dto.user.LoginRequestDTO;
+import com.team11.issue.dto.user.LoginResponseDTO;
 import com.team11.issue.exception.AccessTokenNotFoundException;
 import com.team11.issue.oauth.GitHubOauth;
+import com.team11.issue.repository.UserRepository;
+import com.team11.issue.util.JwtUtil;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
     private final GitHubOauth gitHubOauth;
+    private final UserRepository userRepository;
 
-    public UserService(GitHubOauth gitHubOauth) {
+    public UserService(GitHubOauth gitHubOauth, UserRepository userRepository) {
         this.gitHubOauth = gitHubOauth;
+        this.userRepository = userRepository;
     }
 
     private String getAccessToken(LoginRequestDTO loginRequestDTO) {
@@ -23,9 +29,55 @@ public class UserService {
         return gitHubOauth.getUserInfoFromGitHub(accessToken);
     }
 
-    public void login(LoginRequestDTO loginRequestDTO) {
+    private User findByName(String name) {
+        /*TODO: Exception 처리*/
+        return userRepository.findByName(name).orElseThrow(() -> new RuntimeException());
+    }
+
+    private String createJwtToken(String userName) {
+        return JwtUtil.createToken(userName);
+    }
+
+    private String getJwtToken(String authorization) {
+        String jwtToken = null;
+        if (authorization.startsWith("Bearer "))
+            return jwtToken = authorization.substring(7);
+        return jwtToken;
+    }
+
+    private boolean verifyUser(String userName) {
+        return userRepository.findByName(userName).isPresent();
+    }
+
+    public LoginResponseDTO login(LoginRequestDTO loginRequestDTO) {
         String accessToken = getAccessToken(loginRequestDTO);
-        UserInfoDTO userInfo = getUserInfoFromGitHub(accessToken);
-        System.out.println(userInfo.toString());
+        UserInfoDTO loginUserInfo = getUserInfoFromGitHub(accessToken);
+
+        if (verifyUser(loginUserInfo.getName())) {
+            User user = findByName(loginUserInfo.getName());
+            user.updateUser(accessToken);
+            userRepository.save(user);
+            String jwtToken = createJwtToken(user.getName());
+            return new LoginResponseDTO(user, jwtToken);
+        }
+
+        User saveUser = new User(loginUserInfo, accessToken);
+        userRepository.save(saveUser);
+
+        User user = findByName(loginUserInfo.getName());
+        String jwtToken = createJwtToken(user.getName());
+
+        return new LoginResponseDTO(user, jwtToken);
+
+    }
+
+    public void logout(String authorization) {
+        String jwtToken = getJwtToken(authorization);
+        String userName = JwtUtil.getUserIdFromJwtToken(jwtToken);
+
+        User user = findByName(userName);
+        user.removeAccessToken();
+
+        userRepository.save(user);
     }
 }
