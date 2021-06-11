d3d932304b2ed479322e781217ad7d8473ec09c9
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2021-06-11 16:32:23 +0900

    refactor:(#27) useToggle if문으로 빨리 리턴 및 any타입 삭제

--- frontend/src/hooks/useToggle.tsx
+++ frontend/src/hooks/useToggle.tsx
@@ -23,19 +23,20 @@ function useToggle({ toggle, modal }: CtrlModal): toggleReturnType {
 
   const handleClick = (e: MouseEvent): void => {
     const target = e.target as HTMLDivElement;
-    const toggleTarget = toggle.map((el: any) => el.current);
-    const ModalTarget = modal.current;
-    if (toggleTarget[0]) {
-      for (const element of toggleTarget) {
-        if (element.contains(target)) {
-          setOpen((open) => !open);
-          return;
-        }
+
+    if (!toggle.length) return;
+
+    for (const element of toggle) {
+      if (element.current?.contains(target)) {
+        setOpen((open) => !open);
+        return;
       }
-      if (ModalTarget?.contains(target)) setOpen(true);
-      else setOpen(false);
     }
+
+    if (modal.current?.contains(target)) setOpen(true);
+    else setOpen(false);
   };
+
   return { open, setOpen };
 }
 export default useToggle;
