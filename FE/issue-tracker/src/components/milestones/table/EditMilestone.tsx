c771efc5a37038588bc048c4c831eb63ae4be48f
eve712 <62237639+eve712@users.noreply.github.com>
2021-06-11 18:09:49 +0900

    Refactor: #13 Props last를 isLastItemStyle로 네이밍 수정

--- FE/issue-tracker/src/components/milestones/table/EditMilestone.tsx
+++ FE/issue-tracker/src/components/milestones/table/EditMilestone.tsx
@@ -18,25 +18,5 @@ function EditMilestone() {
 export default EditMilestone;
 
 const EditMilestoneWrap = styled.section`
-  width: 100%;
-  border: 1px solid ${({ theme }) => theme.colors.gr_line};
-  border-top: none;
-  padding: 32px;
-  background-color: ${({ theme }) => theme.colors.gr_offWhite};
-  h2 {
-    font-size: ${({ theme }) => theme.fontSizes['2xl']};
-    margin-bottom: 16px;
-  }
-  .icon_edit {
-    margin-right: 5px;
-    path {
-      stroke: ${({ theme }) => theme.colors.gr_offWhite};
-    }
-  }
-  .icon_cancel {
-    margin-right: 5px;
-    path {
-      stroke: ${({ theme }) => theme.colors.bl_initial};
-    }
-  }
+  ${({ theme }) => theme.editLabelMilestoneWrap};
 `;
