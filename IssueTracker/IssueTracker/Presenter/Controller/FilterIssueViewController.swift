6d41ba4d6845affc1dc1c6b24f8c6a714f3e6ee5
Ray suh <readingod@naver.com>
2021-06-11 16:59:31 +0900

    feat: (#6) filter select/deselect 구현

--- IssueTracker/IssueTracker/Presenter/Controller/FilterIssueViewController.swift
+++ IssueTracker/IssueTracker/Presenter/Controller/FilterIssueViewController.swift
@@ -5,13 +5,13 @@ import NSObject_Rx
 import RxDataSources
 
 class FilterIssueViewController: UIViewController {
-
+    
     @IBOutlet weak var cancelButton: UIButton!
     @IBOutlet weak var filterTableView: UITableView!
     
     private let dataSource = RxTableViewSectionedReloadDataSource<SectionOfFilterList>(configureCell: { dataSource, tableView, indexPath, item in
-        let cell = tableView.dequeueReusableCell(withIdentifier: CellID.filter, for: indexPath)
-        cell.textLabel?.text = item.mainInfo
+        let cell = tableView.dequeueReusableCell(withIdentifier: FilterCell.identifier, for: indexPath) as! FilterCell
+        cell.configure(item.mainInfo)
         return cell
     })
     
@@ -19,8 +19,17 @@ class FilterIssueViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupMainView()
+        bind()
+    }
+}
+
+private extension FilterIssueViewController {
+    
+    private func setupMainView() {
         setupIssueCancelButton()
         setupDataSource()
+        setupFilterTableView()
     }
     
     private func setupIssueCancelButton() {
@@ -34,8 +43,42 @@ class FilterIssueViewController: UIViewController {
         dataSource.titleForHeaderInSection = { dataSource, indexPath in
             return dataSource.sectionModels[indexPath].header
         }
+    }
+    
+    private func setupFilterTableView() {
+        filterTableView.register(FilterCell.self, forCellReuseIdentifier: FilterCell.identifier)
+        filterTableView.rx.setDelegate(self).disposed(by: rx.disposeBag)
+    }
+}
+
+private extension FilterIssueViewController {
+    
+    private func bind() {
+        bindFilterTableView()
+    }
+    
+    private func bindFilterTableView() {
         viewModel.filterList
             .drive(filterTableView.rx.items(dataSource: dataSource))
             .disposed(by: rx.disposeBag)
+        
+        filterTableView.rx.itemSelected
+            .subscribe(onNext: { [weak self] indexPath in
+                let cell = self?.filterTableView.cellForRow(at: indexPath) as! FilterCell
+                cell.isSelected(true)
+            }).disposed(by: rx.disposeBag)
+        
+        filterTableView.rx.itemDeselected
+            .subscribe(onNext: { [weak self] indexPath in
+                let cell = self?.filterTableView.cellForRow(at: indexPath) as! FilterCell
+                cell.isSelected(false)
+            }).disposed(by: rx.disposeBag)
+    }
+}
+
+extension FilterIssueViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return filterTableView.frame.height/15
     }
 }
