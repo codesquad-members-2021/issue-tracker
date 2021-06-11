6023de97d2b1d22407748561573977556ecd5bc2
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2021-06-11 16:27:22 +0900

    refactor:(#27) app에서 suspense optionTable,issueTable에 각각 적용

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,22 +3,20 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import LoginPage from 'components/login/LoginPage';
 import Header from './components/header/Header';
-import { RecoilRoot } from 'recoil';
+
 import MainPage from './page/MainPage';
 function App() {
   const isLogin = true;
   return (
     <div className='App'>
-      <Suspense fallback={<h1>loading...</h1>}>
-        <Router>
-          <Switch>{isLogin && <Header />}</Switch>
-          <Switch>
-            <Route path='/' component={LoginPage} exact />
-            //로그인이 되면, 되어있다면 화면 전환.
-            <Route path='/main' component={MainPage} />
-          </Switch>
-        </Router>
-      </Suspense>
+      <Router>
+        <Switch>{isLogin && <Header />}</Switch>
+        <Switch>
+          <Route path='/' component={LoginPage} exact />
+          //로그인이 되면, 되어있다면 화면 전환.
+          <Route path='/main' component={MainPage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- frontend/src/page/MainPage.tsx
+++ frontend/src/page/MainPage.tsx
@@ -1,15 +1,20 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, Suspense } from 'react';
 import styled from 'styled-components';
 import IssueTable from 'components/issueTable/IssueTable';
 import OptionTable from 'components/optionTable/OptionTable';
+import PrimaryButton from 'components/atom/PrimaryButton';
 
 interface Props {}
 
 export default function MainPage({}: Props): ReactElement {
   return (
     <MainPageBlock>
-      <OptionTable />
-      <IssueTable />
+      <Suspense fallback='loading...'>
+        <OptionTable />
+      </Suspense>
+      <Suspense fallback='loading...'>
+        <IssueTable />
+      </Suspense>
     </MainPageBlock>
   );
 }
