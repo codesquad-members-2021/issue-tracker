ec8761eb70a2658b2be79a0495503230a3d70aed
eve712 <62237639+eve712@users.noreply.github.com>
2021-06-11 18:08:49 +0900

    Refactor: #12 EditLabelWrap 스타일 컴포넌트의 css를 theme에 저장해서 사용

--- FE/issue-tracker/src/components/labels/table/EditLabel.tsx
+++ FE/issue-tracker/src/components/labels/table/EditLabel.tsx
@@ -16,25 +16,5 @@ function EditLabel() {
 export default EditLabel;
 
 const EditLabelWrap = styled.section`
-  width: 100%;
-  border: 1px solid ${({ theme }) => theme.colors.gr_line};
-  border-top: none;
-  padding: 32px;
-  background-color: ${({ theme }) => theme.colors.gr_offWhite};
-  h2 {
-    font-size: ${({ theme }) => theme.fontSizes['2xl']};
-    margin-bottom: 16px;
-  }
-  .icon_edit {
-    margin-right: 5px;
-    path {
-      stroke: ${({ theme }) => theme.colors.gr_offWhite};
-    }
-  }
-  .icon_cancel {
-    margin-right: 5px;
-    path {
-      stroke: ${({ theme }) => theme.colors.bl_initial};
-    }
-  }
+  ${({ theme }) => theme.editLabelMilestoneWrap};
 `;
