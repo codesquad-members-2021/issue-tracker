84afe299cfd8994b4afa8f15e530d28a1f7a58be
pbg0205 <pbg0205@naver.com>
2021-06-11 12:12:29 +0900

    feat : Correct details in MilestoneRequestDt(#14)
    - Add final keyword in field
    
    - Correct constructor to RequiredArgsConstructor annotation

--- BE/src/main/java/com/codesquad/issuetracker/milestone/dto/MilestoneRequestDto.java
+++ BE/src/main/java/com/codesquad/issuetracker/milestone/dto/MilestoneRequestDto.java
@@ -2,31 +2,21 @@ package com.codesquad.issuetracker.milestone.dto;
 
 import com.codesquad.issuetracker.milestone.domain.Milestone;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
-import org.springframework.format.annotation.DateTimeFormat;
 
-import java.beans.ConstructorProperties;
 import java.time.LocalDate;
 
 @Getter
-@NoArgsConstructor
+@RequiredArgsConstructor
 @ToString
 public class MilestoneRequestDto {
 
-    private String title;
+    private final String title;
 
-    private String description;
+    private final String description;
 
-    private LocalDate dueDate;
-
-    @ConstructorProperties({"title", "description", "dueDate"})
-    public MilestoneRequestDto(String title, String description,
-                               @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate dueDate) {
-        this.title = title;
-        this.description = description;
-        this.dueDate = dueDate;
-    }
+    private final LocalDate dueDate;
 
     public Milestone toEntity() {
         return Milestone.create(title, description, dueDate);
