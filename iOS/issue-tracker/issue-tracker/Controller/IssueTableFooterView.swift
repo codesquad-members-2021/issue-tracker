5c87910df7de778e2cd320c95afe1b185db4ecc5
zeke <yangsin1031@naver.com>
2021-06-11 17:50:27 +0900

    feat: ✨  IssueTableFooterView 구현 (#27)

---
+++ iOS/issue-tracker/issue-tracker/Controller/IssueTableFooterView.swift
@@ -0,0 +1,41 @@
+//
+//  IssueTableFooterView.swift
+//  issue-tracker
+//
+//  Created by 양준혁 on 2021/06/10.
+//
+
+import UIKit
+import SnapKit
+
+class IssueTableFooterView: UIView {
+
+    var label: UILabel = {
+        var label = UILabel()
+        label.text = "아래로 당기면 검색바가 보여요!👀"
+        label.textColor = .lightGray
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = #colorLiteral(red: 0.9489405751, green: 0.9490727782, blue: 0.9685038924, alpha: 1)
+        addSubview(label)
+        setAutolayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        backgroundColor = #colorLiteral(red: 0.9489405751, green: 0.9490727782, blue: 0.9685038924, alpha: 1)
+        addSubview(label)
+        setAutolayout()
+    }
+    
+    func setAutolayout() {
+        label.snp.makeConstraints { label in
+            label.centerX.equalToSuperview()
+            label.top.equalTo(39)
+        }
+    }
+    
+}
