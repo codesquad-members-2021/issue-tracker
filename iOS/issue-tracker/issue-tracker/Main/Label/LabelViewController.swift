dfebc9493e18d67d3667ea2402bf43fd65e437e0
Song Lee <songlish233@gmail.com>
2021-06-11 18:11:07 +0900

    feat: [#44] 모든 탭 타이틀 추가

--- iOS/issue-tracker/issue-tracker/Main/Label/LabelViewController.swift
+++ iOS/issue-tracker/issue-tracker/Main/Label/LabelViewController.swift
@@ -14,6 +14,7 @@ class LabelViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.orange
+        title = "레이블"
     }
     
 }
