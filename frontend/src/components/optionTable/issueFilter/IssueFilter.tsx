2d3a7ce8311b08373c49ebdfc8c3dbae24b05031
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2021-06-11 15:37:05 +0900

    refactor:(#27) IssueFilter 컴포넌트에서 Input 분리

--- frontend/src/components/optionTable/issueFilter/IssueFilter.tsx
+++ frontend/src/components/optionTable/issueFilter/IssueFilter.tsx
@@ -3,7 +3,7 @@ import React, { ReactElement, useRef } from 'react';
 import styled from 'styled-components';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import IssueFilterModal from './IssueFilterModal';
-import SearchIcon from '@material-ui/icons/Search';
+import SearchInput from './SearchInput';
 interface Props {}
 
 export default function IssueFilter({}: Props): ReactElement {
@@ -16,14 +16,9 @@ export default function IssueFilter({}: Props): ReactElement {
       <IssueFilterBlock>
         <div className='filter__btn' ref={filterButton}>
           <div>필터</div>
-          <div>
-            <ExpandMoreIcon />
-          </div>
+          <ExpandMoreIcon />
         </div>
-        {/* <SearchInput>
-          <SearchIcon />
-          <Input value='is:issue is:open'></Input>
-        </SearchInput> */}
+        <SearchInput />
       </IssueFilterBlock>
       {open && <IssueFilterModal modalRef={filterModal} />}
     </>
@@ -31,6 +26,7 @@ export default function IssueFilter({}: Props): ReactElement {
 }
 
 const IssueFilterBlock = styled.div`
+  display: flex;
   .filter__btn {
     display: flex;
     align-items: center;
@@ -41,27 +37,5 @@ const IssueFilterBlock = styled.div`
     background: ${({ theme }) => theme.color.bgGrey};
     border-radius: 11px 0px 0px 11px;
     border-right: 1px solid ${({ theme }) => theme.color.lineGrey};
-  }
-`;
-
-const SearchInput = styled.div`
-  padding: 0px 10px;
-  display: flex;
-  min-width: 372px;
-  width: fit-content;
-  height: 40px;
-  justify-content: center;
-  align-items: center;
-  background: ${({ theme }) => theme.color.inputBg};
-  border-radius: 0px 11px 11px 0px;
-`;
-const Input = styled.input`
-  min-width: 300px;
-  width: 450px;
-  height: 40px;
-  border: none;
-  background-color: ${({ theme }) => theme.color.transparent};
-  &:focus {
-    outline: none;
   }
 `;
