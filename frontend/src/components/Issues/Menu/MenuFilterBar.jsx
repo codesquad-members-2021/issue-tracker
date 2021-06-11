9bec3ba1acbb504d6339916d710032fd84c6b48a
Dami Kim <dami.k.dev@gmail.com>
2021-06-11 12:27:57 +0900

    WIP: FilterModal Material UI custom 적용 중(#29)

--- frontend/src/components/Issues/Menu/MenuFilterBar.jsx
+++ frontend/src/components/Issues/Menu/MenuFilterBar.jsx
@@ -2,17 +2,43 @@ import styled from "styled-components";
 import { ReactComponent as SearchIcon } from "images/search.svg";
 import DropDownButton from "components/common/DropDownButton";
 import theme from "styles/theme";
+import FilterModal from "components/common/FilterModal";
+import { useState, useEffect } from "react";
 const MenuFilterBar = () => {
+	const [isFilterClicked, setIsFilterClicked] = useState(false); //isFilterClicked말고 flag에 쓸만한 이름 추천좀..
+	const handleClick = () => {
+		isFilterClicked === false
+			? setIsFilterClicked(true)
+			: setIsFilterClicked(false);
+	};
+	//토글 하면됨
+
+	useEffect(() => {
+		document.body.addEventListener("click", closePopup);
+		return function cleanup() {
+			window.removeEventListener("click", closePopup);
+		};
+	}, []);
+
+	const closePopup = e => {
+		const target = e.target;
+		// console.log(target);
+		!target.closest(".filter-modal") && setIsFilterClicked(false);
+	};
+
 	return (
-		<MenuFilterLayout>
-			<DropDownButton text={"필터"} />
-			<FilterInputContainer>
-				<FilterInput>
-					<SearchIcon />
-					<FilterInputText>is:issue is:open</FilterInputText>
-				</FilterInput>
-			</FilterInputContainer>
-		</MenuFilterLayout>
+		<>
+			<MenuFilterLayout>
+				<DropDownButton text={"필터"} clickEvent={handleClick} />
+				<FilterInputContainer>
+					<FilterInput>
+						<SearchIcon stroke={theme.grayScale.placeholder} />
+						<FilterInputText>is:issue is:open</FilterInputText>
+					</FilterInput>
+				</FilterInputContainer>
+			</MenuFilterLayout>
+			{<FilterModal />}
+		</>
 	);
 };
 
@@ -20,6 +46,7 @@ const MenuFilterLayout = styled.div`
 	display: flex;
 	width: 601px;
 	height: 40px;
+	position: relative;
 `;
 const FilterInputContainer = styled.div`
 	width: 100%;
