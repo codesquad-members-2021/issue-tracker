de79d82150b567bc99fc5ca63666a3107e84fc7a
zeke <yangsin1031@naver.com>
2021-06-11 17:51:43 +0900

    feat: ✨ CancelBarButton 구현 (#27)

---
+++ iOS/issue-tracker/issue-tracker/View/IssueList/CancelButton.swift
@@ -0,0 +1,20 @@
+//
+//  CancelButton.swift
+//  issue-tracker
+//
+//  Created by 양준혁 on 2021/06/11.
+//
+
+import UIKit
+
+class CancelButton: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setTitle("취소", for: .normal)
+        setTitleColor(.systemBlue, for: .normal)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
