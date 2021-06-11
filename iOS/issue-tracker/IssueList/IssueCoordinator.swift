68756740630af8489f64319434f24142f190b069
ellyheetov <ellyheetov@gmail.com>
2021-06-11 15:59:15 +0900

    refactor: Storyboard 이동 및 storyboard name 열거형 추가

--- iOS/issue-tracker/IssueList/IssueCoordinator.swift
+++ iOS/issue-tracker/IssueList/IssueCoordinator.swift
@@ -12,7 +12,7 @@ class IssueCoordinator: NSObject, Coordinator {
     var navigationController = UINavigationController()
     
     override init() {
-        let issueListVC = UIStoryboard(name: "IssueList", bundle: nil).instantiateViewController(withIdentifier: IssueListViewController.reuseIdentifier) as! IssueListViewController
+        let issueListVC = UIStoryboard(name: StoryBoardName.IssueList.description, bundle: nil).instantiateViewController(withIdentifier: IssueListViewController.reuseIdentifier) as! IssueListViewController
         
         self.navigationController = UINavigationController(rootViewController: issueListVC)
         navigationController.tabBarItem = UITabBarItem(title: "이슈", image: UIImage.init(systemName: "exclamationmark.circle"), tag: 0)
@@ -21,7 +21,7 @@ class IssueCoordinator: NSObject, Coordinator {
     }
     
     func pushEditView() {
-        let issueEditVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: IssueEditViewController.reuseIdentifier) as! IssueEditViewController
+        let issueEditVC = UIStoryboard(name: StoryBoardName.IssueEdit.description, bundle: nil).instantiateViewController(withIdentifier: IssueEditViewController.reuseIdentifier) as! IssueEditViewController
         
         navigationController.pushViewController(issueEditVC, animated: true)
     }
