318331078bdb6b0b2f7142a3b6f508bb2eea586d
pbg0205 <pbg0205@naver.com>
2021-06-11 14:57:45 +0900

    feat : Add UserRepository(#19)

---
+++ BE/src/main/java/com/codesquad/issuetracker/user/infra/UserRepository.java
@@ -0,0 +1,12 @@
+package com.codesquad.issuetracker.user.infra;
+
+import com.codesquad.issuetracker.user.domain.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface UserRepository extends CrudRepository<User, UUID> {
+    @Override
+    List<User> findAll();
+}
