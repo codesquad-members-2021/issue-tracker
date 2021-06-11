4145b891344c619c41f8f85e97d290ca279755d0
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2021-06-11 12:33:13 +0900

    feat:(#34) 모달탭 select 아이템 템플릿 생성

---
+++ frontend/src/components/common/LabelSelectItem.tsx
@@ -0,0 +1,10 @@
+import React, { ReactElement } from 'react';
+import styled from 'styled-components';
+
+interface Props {}
+
+export default function LabelSelectItem({}: Props): ReactElement {
+  return <LabelSelectItemBlock></LabelSelectItemBlock>;
+}
+
+const LabelSelectItemBlock = styled.div``;
