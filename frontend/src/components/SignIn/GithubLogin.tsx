8427e25dbdd2f91656f4a221ad4dd4ee3c85318c
Jaeuk-YOO <ryou9305@naver.com>
2021-06-13 05:01:21 +0900

    fix: 깃헙 로그인 관련 테스트 로직 작성

--- frontend/src/components/SignIn/GithubLogin.tsx
+++ frontend/src/components/SignIn/GithubLogin.tsx
@@ -3,28 +3,37 @@ import styled from 'styled-components';
 
 function GithubLogin() {
   const [info, setInfo] = useState<any>();
-  
+  const [userInfo, setUserInfo] = useState<any>();
+
   useEffect(() => {
-    const fetchResult = async () => {
-      const result = await fetch('http://ec2-13-124-158-166.ap-northeast-2.compute.amazonaws.com/api/users/github');
-      setInfo(await result.json());
+    if (window.location.search.split("?code=")[1]) {
+      const fetchCode = async () => {
+        const result = await fetch('http://ec2-13-124-158-166.ap-northeast-2.compute.amazonaws.com/api/users/github/callback?code='+window.location.search.split("?code=")[1]);
+        
+        // userinfo는 테스트용입니다.
+        const checkedUserInfo = await result.json();
+        setUserInfo(checkedUserInfo);
+        console.log(checkedUserInfo)
+        alert(`안녕하세요 ${checkedUserInfo.name}`)
+      }
+      fetchCode();
+    } else {
+      const fetchResult = async () => {
+        const result = await fetch('http://ec2-13-124-158-166.ap-northeast-2.compute.amazonaws.com/api/users/github');
+        setInfo(await result.json());
+      }
+      fetchResult();
     }
-    fetchResult();
   }, []);
   
   if (!info) return <></>;
   
   return (
-    
     <a href={`https://github.com/login/oauth/authorize?scope=${info.scope}&client_id=${info.clientId}`}>
       <GithubLoginButton>
         github으로 로그인하기
       </GithubLoginButton> 
     </a>
-    
-    // <a href="http://ec2-13-124-158-166.ap-northeast-2.compute.amazonaws.com/api/users/github/callback" target={"_blank"}>
-    //   github으로 로그인하기
-    // </a>
   )
 }
 
