1400fe30b55f87b1118c31cefc2d0617ff0b654a
malaheaven <yunhg95@gmail.com>
2021-06-13 02:39:41 +0900

    [#22] feat: :sparkles: swagger 적용

---
+++ BE/server/src/main/java/com/team11/issue/config/SwaggerConfig.java
@@ -0,0 +1,44 @@
+package com.team11.issue.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket newsApi() {
+        Set<String> responseContentType = new HashSet<>();
+        responseContentType.add("application/json;charset=UTF-8");
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                /*.host("")*/
+                .groupName("issue-tracker")
+                .produces(responseContentType)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.ant("/api/**"))
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("issue-tracker API")
+                .description("issue-tracker API")
+                .license("Apache License Version 2.0")
+                .version("2.0")
+                .build();
+    }
+}
--- BE/server/src/main/java/com/team11/issue/controller/UserController.java
+++ BE/server/src/main/java/com/team11/issue/controller/UserController.java
@@ -2,12 +2,17 @@ package com.team11.issue.controller;
 
 import com.team11.issue.dto.ResponseDTO;
 import com.team11.issue.dto.user.LoginRequestDTO;
+import com.team11.issue.dto.user.LoginResponseDTO;
 import com.team11.issue.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Api(tags = {"Oauth API"}, description = "로그인, 로그아웃 사용자 인증과 관련된 API")
 @RestController
 @RequestMapping("api/user")
 public class UserController {
@@ -22,13 +27,15 @@ public class UserController {
 
 
     @PostMapping("/login")
-    public ResponseEntity login(@RequestBody LoginRequestDTO loginRequestDTO) {
+    @ApiOperation(value = "로그인", notes = "code를 가지고 로그인을 합니다.")
+    public ResponseEntity<LoginResponseDTO> login(@ApiParam("github code") @RequestBody LoginRequestDTO loginRequestDTO) {
         logger.info("로그인 요청");
         return ResponseEntity.ok().body(userService.login(loginRequestDTO));
     }
 
     @PutMapping("/logout")
-    public ResponseEntity logout(@RequestHeader String authorization) {
+    @ApiOperation(value = "로그아웃", notes = "jwt token을 가지고 로그아웃을 합니다.")
+    public ResponseEntity<ResponseDTO> logout(@ApiParam("Bearer Jwt token") @RequestHeader String authorization) {
         logger.info("로그아웃 요청");
         userService.logout(authorization);
         return ResponseEntity.ok().body(new ResponseDTO("logout"));
