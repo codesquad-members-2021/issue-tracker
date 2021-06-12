f8c476c6ad76817580c01c554ca58f6a109e489e
junzero741 <junzero741@gmail.com>
2021-06-12 23:34:46 +0900

    chore: 코드 정리

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import GlobalStyle from "./styles/GlobalStyle";
-
 import { StylesProvider } from "@material-ui/core/styles";
 
 ReactDOM.render(
