08a7c8ed1ab0f8ee179ac797b57919033591b388
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-06-13 21:45:10 +0900

    fix: 리스폰스 json 프로퍼티명 수정

--- be/issue-tracker-be/src/main/java/com/codesquad/issuetracker/label/LabelResponseWrapper.java
+++ be/issue-tracker-be/src/main/java/com/codesquad/issuetracker/label/LabelResponseWrapper.java
@@ -1,8 +1,11 @@
 package com.codesquad.issuetracker.label;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data(staticConstructor = "from")
 public class LabelResponseWrapper {
+
+    @JsonProperty("label")
     private final LabelResponse labelResponse;
 }
