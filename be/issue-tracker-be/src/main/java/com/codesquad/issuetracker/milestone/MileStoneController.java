0b36281f554d186452ac6bb34b1fa72fb924e6fe
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-06-13 21:40:51 +0900

    feat: 마일스톤 post, put 추가
    Closes: hiro032/issue-tracker#62

--- be/issue-tracker-be/src/main/java/com/codesquad/issuetracker/milestone/MileStoneController.java
+++ be/issue-tracker-be/src/main/java/com/codesquad/issuetracker/milestone/MileStoneController.java
@@ -1,7 +1,9 @@
 package com.codesquad.issuetracker.milestone;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 public class MileStoneController {
@@ -9,6 +11,16 @@ public class MileStoneController {
     @GetMapping("/milestones")
     public MileStoneResponses readAll() {
         return MileStoneDummyData.mileStonesResponse();
+    }
+
+    @PostMapping("/milestones")
+    public MileStoneResponseWrapper create(@RequestBody @Valid MileStoneRequest mileStoneRequest) {
+        return MileStoneResponseWrapper.from(MileStoneDummyData.openMileStoneResponse());
+    }
+
+    @PutMapping("/milestones/{milestoneId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@PathVariable long milestoneId, @RequestBody @Valid MileStoneRequest mileStoneRequest) {
     }
 
     @GetMapping("/milestones-count")
