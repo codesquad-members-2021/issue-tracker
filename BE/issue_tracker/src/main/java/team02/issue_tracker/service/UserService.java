b1308d57dc263c478a1ca31023f6413833943d07
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-06-14 02:18:44 +0900

    feat : modifyIssueAssignees와 deleteIssueAssignees 메소드 추가

--- BE/issue_tracker/src/main/java/team02/issue_tracker/service/UserService.java
+++ BE/issue_tracker/src/main/java/team02/issue_tracker/service/UserService.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Service;
 import team02.issue_tracker.domain.Issue;
 import team02.issue_tracker.domain.IssueAssignee;
 import team02.issue_tracker.domain.User;
-import team02.issue_tracker.dto.issue.IssueRequest;
+import team02.issue_tracker.dto.issue.IssueAssigneeIdsRequest;
 import team02.issue_tracker.exception.UserNotFoundException;
 import team02.issue_tracker.repository.IssueAssigneeRepository;
 import team02.issue_tracker.repository.UserRepository;
@@ -22,7 +22,7 @@ public class UserService {
         this.userRepository = userRepository;
         this.issueAssigneeRepository = issueAssigneeRepository;
     }
-    
+
     public User enroll(User user) {
         return userRepository.save(user);
     }
@@ -36,14 +36,24 @@ public class UserService {
         return userRepository.findById(id).orElseThrow(UserNotFoundException::new);
     }
 
-    public List<IssueAssignee> makeIssueAssignees(Issue issue, IssueRequest issueRequest) {
+    public List<IssueAssignee> makeIssueAssignees(Issue issue, List<Long> assigneeIds) {
         List<IssueAssignee> issueAssignees = new ArrayList<>();
-        issueRequest.getAssigneeIds().stream()
+        assigneeIds.stream()
                 .forEach(assigneeId -> {
                     User assignee = userRepository.findById(assigneeId).orElseThrow(UserNotFoundException::new);
                     issueAssignees.add(new IssueAssignee(issue, assignee));
                 });
 
         return issueAssigneeRepository.saveAll(issueAssignees);
+    }
+
+    public List<IssueAssignee> modifyIssueAssignees(Issue issue, IssueAssigneeIdsRequest issueAssigneeIdsRequest) {
+        deleteIssueAssignees(issue);
+        return makeIssueAssignees(issue, issueAssigneeIdsRequest.getAssigneeIds());
+    }
+
+    private void deleteIssueAssignees(Issue issue) {
+        List<IssueAssignee> issueAssignees = issueAssigneeRepository.findByIssueId(issue.getId());
+        issueAssigneeRepository.deleteAll(issueAssignees);
     }
 }
