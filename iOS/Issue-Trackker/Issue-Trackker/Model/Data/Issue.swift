86860ce8ae376c506c51056d59397a76499aa121
shimyoungmin <dudals6965@gmail.com>
2021-06-13 20:42:54 +0900

    [#13]Feat: Add DataModel
    Issue, Label, Milestone, Assignment 데이터 모델 추가

---
+++ iOS/Issue-Trackker/Issue-Trackker/Model/Data/Issue.swift
@@ -0,0 +1,23 @@
+//
+//  Issue.swift
+//  Issue-Trackker
+//
+//  Created by 심영민 on 2021/06/13.
+//
+
+import Foundation
+
+struct Issue: Decodable {
+    var id: Int
+    var title: String
+    var writer: String
+    var writeTime: String
+    var milestone: String
+    var assignments: [Assignment]
+    var labels: [Label]
+}
+
+
+
+
+
---
+++ iOS/Issue-Trackker/Issue-Trackker/Model/Data/Milestone.swift
@@ -0,0 +1,18 @@
+//
+//  Milestone.swift
+//  Issue-Trackker
+//
+//  Created by 심영민 on 2021/06/13.
+//
+
+import Foundation
+
+struct Milestone: Decodable {
+    var id: Int
+    var title: String
+    var detail: String
+    var deadLine: String
+    var complete: Int
+    var openIssueCount: Int
+    var closeIssueCount: Int
+}
