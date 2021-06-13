84cb97ef43abff03f831ccca0c9f8ff1efbb8c38
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-06-13 23:00:20 +0900

    feat : closeIssues 메소드 추가

--- BE/issue_tracker/src/main/java/team02/issue_tracker/controller/IssueController.java
+++ BE/issue_tracker/src/main/java/team02/issue_tracker/controller/IssueController.java
@@ -2,6 +2,7 @@ package team02.issue_tracker.controller;
 
 import org.springframework.web.bind.annotation.*;
 import team02.issue_tracker.dto.issue.DetailIssueResponse;
+import team02.issue_tracker.dto.issue.IssueIdsRequest;
 import team02.issue_tracker.dto.issue.IssueRequest;
 import team02.issue_tracker.dto.issue.IssueResponse;
 import team02.issue_tracker.dto.wrapping.ApiResult;
@@ -23,18 +24,24 @@ public class IssueController {
 
     @GetMapping
     public ApiResult<List<IssueResponse>> showAllIssues() {
-        return issueService.getAllIssueResponses();
+        return ApiResult.success(issueService.getAllIssueResponses());
     }
 
     @GetMapping("/{issueId}")
     public ApiResult<DetailIssueResponse> showDetailIssue(@PathVariable Long issueId) {
-        return issueService.getDetailIssueResponse(issueId);
+        return ApiResult.success(issueService.getDetailIssueResponse(issueId));
     }
 
     @LoginRequired
     @PostMapping
     public ApiResult<String> createIssue(@RequestBody IssueRequest issueRequest, @UserId Long userId) {
-        return issueService.addIssue(issueRequest, userId);
+        issueService.addIssue(issueRequest, userId);
+        return ApiResult.ok();
+    }
 
+    @PostMapping("/close")
+    public ApiResult<String> closeIssues(@RequestBody IssueIdsRequest issueRequest) {
+        issueService.closeIssues(issueRequest);
+        return ApiResult.ok();
     }
 }
