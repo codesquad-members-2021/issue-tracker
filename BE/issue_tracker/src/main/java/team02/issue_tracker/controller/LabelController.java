4f638239539f5eedcf0fb223f4c9a0894e757f46
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-06-14 21:43:15 +0900

    feat : LabelController에서 레이블의 개수를 보여주는 showLabelCount 메소드 추가

--- BE/issue_tracker/src/main/java/team02/issue_tracker/controller/LabelController.java
+++ BE/issue_tracker/src/main/java/team02/issue_tracker/controller/LabelController.java
@@ -3,6 +3,7 @@ package team02.issue_tracker.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import team02.issue_tracker.dto.LabelCountResponse;
 import team02.issue_tracker.dto.LabelResponse;
 import team02.issue_tracker.dto.wrapping.ApiResult;
 import team02.issue_tracker.service.LabelService;
@@ -22,5 +23,10 @@ public class LabelController {
     @GetMapping
     public ApiResult<List<LabelResponse>> showAllLabels() {
         return ApiResult.success(labelService.getAllLabelResponses());
+    }
+
+    @GetMapping("/count")
+    public ApiResult<LabelCountResponse> showLabelCount() {
+        return ApiResult.success(labelService.getLabelCount());
     }
 }
