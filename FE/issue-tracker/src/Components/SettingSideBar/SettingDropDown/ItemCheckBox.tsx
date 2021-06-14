dc9e40aa23fa186369441e7377516fefb2e073b3
swing-park <ongsim123@gmail.com>
2021-06-14 15:56:00 +0900

    chore: :wrench: 경로 수정 및 레거시 파일 삭제

--- FE/issue-tracker/src/Components/SettingSideBar/SettingDropDown/ItemCheckBox.tsx
+++
@@ -1,7 +0,0 @@
-import { SettingSideBar as S } from "@/Components/SettingSideBar/SettingSideBarStyles";
-
-const ItemCheckBox = () => {
-  return <S.ItemCheckBox color="primary" />;
-};
-
-export default ItemCheckBox;
