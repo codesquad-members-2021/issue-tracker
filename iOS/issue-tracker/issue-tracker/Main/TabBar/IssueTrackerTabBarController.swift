f618172949613ef34410b415c43089e0ad749ab0
JinseoPark <rarebook92@naver.com>
2021-06-14 18:45:40 +0900

    fix : [#35] 프로필 사진 불러와서 보여주기 수정

--- iOS/issue-tracker/issue-tracker/Main/TabBar/IssueTrackerTabBarController.swift
+++ iOS/issue-tracker/issue-tracker/Main/TabBar/IssueTrackerTabBarController.swift
@@ -8,10 +8,10 @@
 import UIKit
 
 class IssueTrackerTabBarController: UITabBarController {
-
+    
     private var loginInfo: LoginInfo?
     private let imageLoadManager = ImageLoadManager()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tabBar.tintColor = Colors.mainGrape
@@ -19,6 +19,10 @@ class IssueTrackerTabBarController: UITabBarController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
         updateUserImage()
     }
     
@@ -27,20 +31,34 @@ class IssueTrackerTabBarController: UITabBarController {
     }
     
     private func updateUserImage() {
-        guard let imageURL = self.loginInfo?.avatarURL,
-              let viewControllers = self.viewControllers,
-              let myAccountViewController = viewControllers.compactMap({ $0 as? MyAccountViewController }).first else { return }
         
+        guard let imageURL = self.loginInfo?.avatarURL else { return }
         imageLoadManager.load(from: imageURL) { cachePath in
-            guard let userImage = UIImage(contentsOfFile: cachePath),
-                  let iconSize = myAccountViewController.tabBarItem.image?.size else { return }
-            
-            let resizedUserImage = userImage.resizedImage(size: iconSize)
-            
+            guard let userImage = UIImage(contentsOfFile: cachePath) else {return}
             DispatchQueue.main.async {
-                myAccountViewController.tabBarItem.image = resizedUserImage
+                self.addSubviewToLastTabItem(userImage)
             }
         }
     }
+}
+
+extension UITabBarController {
     
+    func addSubviewToLastTabItem(_ image: UIImage) {
+        
+        if let lastTabBarButton = self.tabBar.subviews.last, let tabItemImageView = lastTabBarButton.subviews.first {
+            if let accountTabBarItem = self.tabBar.items?.last {
+                accountTabBarItem.selectedImage = nil
+                accountTabBarItem.image = nil
+            }
+            let imgView = UIImageView()
+            imgView.frame = tabItemImageView.frame
+            imgView.layer.cornerRadius = tabItemImageView.frame.height/2
+            imgView.layer.masksToBounds = true
+            imgView.contentMode = .scaleAspectFill
+            imgView.clipsToBounds = true
+            imgView.image = image
+            self.tabBar.subviews.last?.addSubview(imgView)
+        }
+    }
 }
