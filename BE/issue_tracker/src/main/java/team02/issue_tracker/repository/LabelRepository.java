186e685230b8eb8fc0cf490da535af3706fed057
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-06-14 21:01:15 +0900

    feat : LabelRepository에 findAll 메소드 추가
    isDeleted=false 인 것만 반환하도록 구현

--- BE/issue_tracker/src/main/java/team02/issue_tracker/repository/LabelRepository.java
+++ BE/issue_tracker/src/main/java/team02/issue_tracker/repository/LabelRepository.java
@@ -1,13 +1,18 @@
 package team02.issue_tracker.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import team02.issue_tracker.domain.Label;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface LabelRepository extends JpaRepository<Label, Long> {
 
     Optional<Label> findById(Long id);
+
+    @Query("select l from Label l where l.isDeleted = false")
+    List<Label> findAll();
 }
