e52014f966d028ec213bd756d30470af3eaa83f2
Jaeuk-YOO <ryou9305@naver.com>
2021-06-14 16:58:45 +0900

    add: OAuth 콜백관련 클라이언트 라우팅 처리

---
+++ frontend/src/pages/OAuthCallbackPage.tsx
@@ -0,0 +1,25 @@
+import { useEffect } from 'react';
+
+const OAuthCallbackPage = () => {
+  useEffect(() => {
+    const fetchCode = async () => {
+      const result = await fetch('http://ec2-13-124-158-166.ap-northeast-2.compute.amazonaws.com/api/users/github/callback?code='+window.location.search.split("?code=")[1]);
+      
+      window.history.replaceState('', '', '/signin');
+      
+      // userinfo는 테스트용입니다.
+      const checkedUserInfo = await result.json();
+      console.log(checkedUserInfo);
+      
+      localStorage.setItem("issue-tracker-user", JSON.stringify(checkedUserInfo));
+      alert(`안녕하세요 ${checkedUserInfo.name}`);
+      window.location.href = '/';
+    }
+    
+    fetchCode();
+  }, [])
+
+  return <></>;
+}
+
+export default OAuthCallbackPage
