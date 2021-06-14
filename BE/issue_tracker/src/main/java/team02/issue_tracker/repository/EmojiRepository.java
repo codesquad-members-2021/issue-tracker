81dcd524cf22fb0812af41ac6d1b64f3b5b73f0c
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-06-14 17:14:49 +0900

    feat : EmojiRepository 클래스 추가

---
+++ BE/issue_tracker/src/main/java/team02/issue_tracker/repository/EmojiRepository.java
@@ -0,0 +1,14 @@
+package team02.issue_tracker.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import team02.issue_tracker.domain.Emoji;
+
+import java.util.Optional;
+
+@Repository
+public interface EmojiRepository extends JpaRepository<Emoji, Long> {
+
+    @Override
+    Optional<Emoji> findById(Long id);
+}
