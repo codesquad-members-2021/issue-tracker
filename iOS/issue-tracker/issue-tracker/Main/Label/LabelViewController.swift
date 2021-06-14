6b9b4454a6d742f79a4990bb0e88d3ef16018346
Song Lee <songlish233@gmail.com>
2021-06-14 16:10:26 +0900

    feat: [#44] 레이블 추가 버튼 생성

--- iOS/issue-tracker/issue-tracker/Main/Label/LabelViewController.swift
+++ iOS/issue-tracker/issue-tracker/Main/Label/LabelViewController.swift
@@ -9,12 +9,34 @@ import UIKit
 
 class LabelViewController: UIViewController {
     
+    private lazy var addLabelButton: UIButton = {
+        let button = UIButton()
+        let plusImage = UIImage(systemName: "plus")
+        button.setImage(plusImage, for: .normal)
+        button.tintColor = Colors.mainGrape
+        button.setTitle("추가", for: .normal)
+        button.setTitleColor(Colors.mainGrape, for: .normal)
+        button.semanticContentAttribute = .forceRightToLeft
+        button.imageEdgeInsets = UIEdgeInsets(top: 0, left: 4, bottom: 0, right: 0)
+        button.addTarget(self, action: #selector(addLabelTouched), for: .touchUpInside)
+        return button
+    }()
+    
     private var loginInfo: LoginInfo?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.white
         title = "레이블"
+        addNavigationButton()
+    }
+    
+    private func addNavigationButton() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: addLabelButton)
+    }
+    
+    @objc private func addLabelTouched(_ sender: UIButton) {
+        
     }
     
 }
