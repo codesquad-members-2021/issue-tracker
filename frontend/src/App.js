6a7d0b28f8228aca64d5a082cf601dd251755e1b
junamee <skawn11114@gmail.com>
2021-06-14 23:39:27 +0900

    feat: (#49) routing 화면전환 시 로딩처리 -suspense, -lazy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import LoginPage from 'page/loginPage/LoginPage';
 import Header from './components/header/Header';
-import MainPage from './page/mainPage/MainPage';
-import CreateIssuePage from 'page/createIssuePage/CreateIssuePage';
+// import LoginPage from 'page/loginPage/LoginPage';
+// import MainPage from './page/mainPage/MainPage';
+// import CreateIssuePage from 'page/createIssuePage/CreateIssuePage';
 
 function App() {
+  
+  const LoginPage = lazy(()=>import('./page/loginPage/LoginPage'))
+  const MainPage = lazy(()=>import('./page/mainPage/MainPage'))
+  const CreateIssuePage = lazy(()=>import('./page/createIssuePage/CreateIssuePage'))
   const isLogin = true;
   return (
     <div className='App'>
+
       <Router>
-        {isLogin && <Header />}
-        <Switch>
-          <Route path='/' component={LoginPage} exact />
-          <Route path='/main' component={MainPage} />
-          <Route path='/create' component={CreateIssuePage} />
-        </Switch>
+        <Suspense fallback={<h1>Loading...🎢</h1>}>
+          {isLogin && <Header />}
+          <Switch>
+            <Route path='/' component={LoginPage} exact />
+            <Route path='/main' component={MainPage} />
+            <Route path='/create' component={CreateIssuePage} />
+          </Switch>
+        </Suspense>
       </Router>
+
     </div>
   );
 }
