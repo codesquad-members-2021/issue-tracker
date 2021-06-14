2a49ec53e0a044ed113779c3127e103e321a34e7
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-06-14 17:36:33 +0900

    feat : modifyComment 메소드 추가
    코멘트 수정 기능

--- BE/issue_tracker/src/main/java/team02/issue_tracker/controller/CommentController.java
+++ BE/issue_tracker/src/main/java/team02/issue_tracker/controller/CommentController.java
@@ -2,11 +2,12 @@ package team02.issue_tracker.controller;
 
 import org.springframework.web.bind.annotation.*;
 import team02.issue_tracker.dto.CommentEmojiRequest;
+import team02.issue_tracker.dto.CommentRequest;
 import team02.issue_tracker.dto.wrapping.ApiResult;
 import team02.issue_tracker.service.CommentService;
 
 @RestController
-@RequestMapping("/api/comments")
+@RequestMapping("/api/comments/{commentId}")
 public class CommentController {
 
     private final CommentService commentService;
@@ -15,10 +16,15 @@ public class CommentController {
         this.commentService = commentService;
     }
 
-    @PostMapping("/{commentId}/emojis")
+    @PostMapping("/emojis")
     public ApiResult<String> addEmojiOnComment(@PathVariable Long commentId, @RequestBody CommentEmojiRequest commentEmojiRequest) {
         commentService.addEmoji(commentId, commentEmojiRequest);
         return ApiResult.ok();
     }
 
+    @PatchMapping
+    public ApiResult<String> modifyComment(@PathVariable Long commentId, @RequestBody CommentRequest commentRequest) {
+        commentService.modifyComment(commentId, commentRequest);
+        return ApiResult.ok();
+    }
 }
