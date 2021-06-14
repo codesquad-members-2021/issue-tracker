986350410aa7cdf12427d45441d714022d014f2b
bibi <bibi6666667@kakao.com>
2021-06-14 16:05:16 +0900

    feat : Issue클래스에 기본생성자, getter, toString() 생성 #3

--- BE/src/main/java/com/codesquad/issuetracker/domain/Issue.java
+++ BE/src/main/java/com/codesquad/issuetracker/domain/Issue.java
@@ -1,10 +1,14 @@
 package com.codesquad.issuetracker.domain;
 
-import org.springframework.data.annotation.Id;
-
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
+@Entity
 public class Issue {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -12,9 +16,12 @@ public class Issue {
     private String title;
     private String content;
     private boolean status;
-    private final LocalDateTime createdAt;
+    private LocalDateTime createdAt;
     private Long milestoneId;
-    private final Long userId;
+    private Long userId;
+
+    public Issue() {
+    }
 
     private Issue(Long id, String title, String content, boolean status, LocalDateTime createdAt, Long milestoneId, Long userId) {
         this.id = id;
@@ -28,5 +35,46 @@ public class Issue {
 
     public Issue create(Long id, String title, String content, boolean status, LocalDateTime createdAt, Long milestoneId, Long userId) {
         return new Issue(id, title, content, status, createdAt, milestoneId, userId);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public Long getMilestoneId() {
+        return milestoneId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    @Override
+    public String toString() {
+        return "Issue{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", status=" + status +
+                ", createdAt=" + createdAt +
+                ", milestoneId=" + milestoneId +
+                ", userId=" + userId +
+                '}';
     }
 }
