471e8f5240ad211fe04aea81fc45ec3290bb2db5
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-06-14 16:46:18 +0900

    feat : saveAll, deleteCommentEmojis 메소드 추가

--- BE/issue_tracker/src/main/java/team02/issue_tracker/service/CommentService.java
+++ BE/issue_tracker/src/main/java/team02/issue_tracker/service/CommentService.java
@@ -2,18 +2,24 @@ package team02.issue_tracker.service;
 
 import org.springframework.stereotype.Service;
 import team02.issue_tracker.domain.Comment;
+import team02.issue_tracker.domain.CommentEmoji;
 import team02.issue_tracker.domain.Issue;
 import team02.issue_tracker.domain.User;
 import team02.issue_tracker.dto.issue.IssueRequest;
+import team02.issue_tracker.repository.CommentEmojiRepository;
 import team02.issue_tracker.repository.CommentRepository;
+
+import java.util.List;
 
 @Service
 public class CommentService {
 
     private final CommentRepository commentRepository;
+    private final CommentEmojiRepository commentEmojiRepository;
 
-    public CommentService(CommentRepository commentRepository) {
+    public CommentService(CommentRepository commentRepository, CommentEmojiRepository commentEmojiRepository) {
         this.commentRepository = commentRepository;
+        this.commentEmojiRepository = commentEmojiRepository;
     }
 
     public Comment makeComment(IssueRequest issueRequest, User writer, Issue issue) {
@@ -24,5 +30,14 @@ public class CommentService {
 
     public Comment save(Comment comment) {
         return commentRepository.save(comment);
+    }
+
+    public void saveAll(List<Comment> comments) {
+        commentRepository.saveAll(comments);
+    }
+
+    public void deleteCommentEmojis(Long commentId) {
+        List<CommentEmoji> commentEmojis = commentEmojiRepository.findByCommentId(commentId);
+        commentEmojiRepository.deleteAll(commentEmojis);
     }
 }
