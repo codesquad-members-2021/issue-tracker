17d7a3f7561938bb8c0c0a5e1d7eac62504ff952
Dami Kim <dami.k.dev@gmail.com>
2021-06-14 23:28:18 +0900

    refactor: TabButton 재사용성 높이는 방향으로 리팩토링(#29)

--- frontend/src/components/common/ButtonGroup.jsx
+++ frontend/src/components/common/ButtonGroup.jsx
@@ -1,24 +1,38 @@
-import { ButtonGroupLeftBtn, ButtonGroupRightBtn } from "styles/StyledButtons";
+import {
+	ButtonGroupLeftBtn,
+	ButtonGroupRightBtn,
+	TabButton,
+} from "styles/StyledButtons";
 import styled from "styled-components";
 import { ReactComponent as LabelIcon } from "images/tag.svg";
 import { ReactComponent as MileStoneIcon } from "images/milestone.svg";
 import theme from "styles/theme";
+
 const ButtonGroup = ({
 	milestoneCount,
 	milestoneClickEvent,
 	labelCount,
 	labelClickEvent,
+	buttonText,
 }) => {
 	return (
 		<>
-			<ButtonGroupLeftBtn onClick={milestoneClickEvent}>
+			<TabButton
+				onClick={milestoneClickEvent}
+				_width={({ theme }) => theme.buttonWidths.base}
+				_radius={"left"}
+			>
 				<MileStoneIcon fill={theme.grayScale.label} />
 				<ButtonText>마일스톤 ({milestoneCount})</ButtonText>
-			</ButtonGroupLeftBtn>
-			<ButtonGroupRightBtn onClick={labelClickEvent}>
+			</TabButton>
+			<TabButton
+				onClick={labelClickEvent}
+				_width={({ theme }) => theme.buttonWidths.base}
+				_radius={"right"}
+			>
 				<LabelIcon stroke={theme.grayScale.label} />
 				<ButtonText>레이블 ({labelCount})</ButtonText>
-			</ButtonGroupRightBtn>
+			</TabButton>
 		</>
 	);
 };
