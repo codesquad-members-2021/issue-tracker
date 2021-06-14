53a006a4c2e4e62665441747460e2c9e0d080cc0
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-06-14 22:29:50 +0900

    feat : LabelController에 ModifyLabel 기능 추가
    LabelRequest에 따라 레이블을 수정하는 기능

--- BE/issue_tracker/src/main/java/team02/issue_tracker/controller/LabelController.java
+++ BE/issue_tracker/src/main/java/team02/issue_tracker/controller/LabelController.java
@@ -1,9 +1,8 @@
 package team02.issue_tracker.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import team02.issue_tracker.dto.LabelCountResponse;
+import team02.issue_tracker.dto.LabelRequest;
 import team02.issue_tracker.dto.LabelResponse;
 import team02.issue_tracker.dto.wrapping.ApiResult;
 import team02.issue_tracker.service.LabelService;
@@ -28,5 +27,11 @@ public class LabelController {
     @GetMapping("/count")
     public ApiResult<LabelCountResponse> showLabelCount() {
         return ApiResult.success(labelService.getLabelCount());
+    }
+
+    @PutMapping("/{labelId}")
+    public ApiResult<String> modifyLabel(@PathVariable Long labelId, @RequestBody LabelRequest labelRequest) {
+        labelService.modifyLabel(labelId, labelRequest);
+        return ApiResult.ok();
     }
 }
