c1b858282fbe2f96f01b659ae742a50011993a56
Jaeuk-YOO <ryou9305@naver.com>
2021-06-14 17:37:18 +0900

    fix: Responsive 관련 수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
+
 import MainPage from 'pages/MainPage';
 import SigninPage from 'pages/SigninPage';
 import OAuthCallbackPage from 'pages/OAuthCallbackPage';
-import { BrowserRouter, Route } from 'react-router-dom';
+import IssueListPage from 'pages/IssueListPage';
+
 // import CountMyRecoil from './components/Count/CountMyRecoil';
 
 function App() {
@@ -18,6 +21,7 @@ function App() {
         <Route exact path="/" component={MainPage} />
         <Route path="/signin" component={SigninPage} />
         <Route path="/auth/github/callback" component={OAuthCallbackPage} />
+        <Route path="/issues" component={IssueListPage} />
       </BrowserRouter>
     </>
   );
