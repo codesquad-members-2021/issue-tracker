1cb7b194663e35c3e3c14ec7a9e43bb4b6cacd2b
bibi <bibi6666667@kakao.com>
2021-06-14 16:06:36 +0900

    chore : 코드 재정렬 #3

--- BE/src/main/java/com/codesquad/issuetracker/exception/TokenEmptyException.java
+++ BE/src/main/java/com/codesquad/issuetracker/exception/TokenEmptyException.java
@@ -1,6 +1,6 @@
 package com.codesquad.issuetracker.exception;
 
-public class TokenEmptyException extends RuntimeException{
+public class TokenEmptyException extends RuntimeException {
 
     private static final String TOKEN_EMPTY = "토큰이 존재하지 않습니다";
     public TokenEmptyException() {
