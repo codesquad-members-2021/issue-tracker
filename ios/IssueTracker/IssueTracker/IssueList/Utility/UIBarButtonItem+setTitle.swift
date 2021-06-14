8360a6a9a9655c6c344fab3f8d1b828002516dd9
Jisoo Kim <ghis22130@naver.com>
2021-06-14 18:56:05 +0900

    fix: 🐞 Merge Conflict 해결

---
+++ ios/IssueTracker/IssueTracker/IssueList/Utility/UIBarButtonItem+setTitle.swift
@@ -0,0 +1,20 @@
+//
+//  UIBarButtonItem+setTitle.swift
+//  IssueTracker
+//
+//  Created by 지북 on 2021/06/14.
+//
+
+import UIKit
+
+extension UIBarButtonItem {
+    func setTitle(_ title: String?, for: UIControl.State) {
+        let button = self.customView as? UIButton ?? UIButton()
+        button.setTitle(title, for: .normal)
+    }
+    
+    func setIsHidden(_ hidden: Bool, animated: Bool) {
+        let button = self.customView as? UIButton ?? UIButton()
+        button.setIsHidden(hidden, animated: animated)
+    }
+}
