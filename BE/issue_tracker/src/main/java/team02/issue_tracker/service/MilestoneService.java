f2f87cd2ca49ea91d51ecff89935bee2b36c80ca
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-06-14 16:48:18 +0900

    feat : 마일스톤 상태 검사 로직 추가

--- BE/issue_tracker/src/main/java/team02/issue_tracker/service/MilestoneService.java
+++ BE/issue_tracker/src/main/java/team02/issue_tracker/service/MilestoneService.java
@@ -2,7 +2,7 @@ package team02.issue_tracker.service;
 
 import org.springframework.stereotype.Service;
 import team02.issue_tracker.domain.Milestone;
-import team02.issue_tracker.dto.issue.IssueRequest;
+import team02.issue_tracker.exception.IllegalStatusException;
 import team02.issue_tracker.exception.MilestoneNotFoundException;
 import team02.issue_tracker.repository.MilestoneRepository;
 
@@ -18,7 +18,11 @@ public class MilestoneService {
     }
 
     public Milestone findOne(Long id) {
-        return milestoneRepository.findById(id).orElseThrow(MilestoneNotFoundException::new);
+        Milestone milestone = milestoneRepository.findById(id).orElseThrow(MilestoneNotFoundException::new);
+        if(milestone.isDeleted()) {
+            throw new IllegalStatusException("삭제된 마일스톤입니다.");
+        }
+        return milestone;
     }
 
     public Milestone getMilestone(Long milestoneId) {
