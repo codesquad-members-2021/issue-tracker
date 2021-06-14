4ac7faf1a08684054812fdd097c3e2c237b62738
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-06-14 16:45:07 +0900

    feat : getTotalIssueCount, getOpenIssueCount 메소드 수정
    delete된 issue는 포함하지 않도록 변경

--- BE/issue_tracker/src/main/java/team02/issue_tracker/domain/Milestone.java
+++ BE/issue_tracker/src/main/java/team02/issue_tracker/domain/Milestone.java
@@ -28,11 +28,14 @@ public class Milestone {
     private List<Issue> issues = new ArrayList<>();
 
     public int getTotalIssueCount() {
-        return issues.size();
+        return (int) issues.stream()
+                .filter(issue -> !issue.isDeleted())
+                .count();
     }
 
     public int getOpenIssueCount() {
         return (int) issues.stream()
+                .filter(issue -> !issue.isDeleted())
                 .filter(Issue::isOpen)
                 .count();
     }
