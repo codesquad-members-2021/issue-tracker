c644bc981801675ea0134e13bb12cbd47e68fc6e
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2021-06-14 17:15:25 +1200

    Chore: 메인 클래스

---
+++ be/src/main/java/com/issuetracker/IssueTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.issuetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueTrackerApplication.class, args);
+	}
+
+}
