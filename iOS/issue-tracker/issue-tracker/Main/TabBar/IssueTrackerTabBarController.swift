281a09ec1cbfac808f9b07b8552ee871223fc2fb
JinseoPark <rarebook92@naver.com>
2021-06-15 12:07:04 +0900

    feat :[#35] 프로필 사진 이미지 리사이징

--- iOS/issue-tracker/issue-tracker/Main/TabBar/IssueTrackerTabBarController.swift
+++ iOS/issue-tracker/issue-tracker/Main/TabBar/IssueTrackerTabBarController.swift
@@ -14,7 +14,7 @@ class IssueTrackerTabBarController: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tabBar.tintColor = Colors.mainGrape        
+        tabBar.tintColor = Colors.mainGrape
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -36,21 +36,25 @@ class IssueTrackerTabBarController: UITabBarController {
         imageLoadManager.load(from: imageURL) { cachePath in
             guard let userImage = UIImage(contentsOfFile: cachePath) else {return}
             DispatchQueue.main.async {
-                self.addSubviewToLastTabItem(userImage)
+                self.addSubviewToMyAccountTabItem(userImage)
             }
         }
     }
 }
 
 extension UITabBarController {
-    func addSubviewToLastTabItem(_ image: UIImage) {
+    func addSubviewToMyAccountTabItem(_ image: UIImage) {
         //tabBar.tabBar.items?.last = MyAccountViewController
         guard let myAccountTabBarItem = self.tabBar.items?.last else {return}
         myAccountTabBarItem.image = nil
+                
+        
+//        myAccountTabBarItem.image = image.resizedImage(size: CGSize(width: 28, height: 28))?.withRenderingMode(.alwaysOriginal)
+//        myAccountTabBarItem.selectedImage = myAccountTabBarItem.image
+//
         
         let imgView = UIImageView()
-        
-        imgView.frame = CGRect(x: 34.3333, y: 7.33333, width: 25.3333, height: 24.3333) //하드코딩값. 실제 myAccountTabBarItem.image.size를 정확하게 가져올 방법을 못찾음.
+        imgView.frame = CGRect(x: 34.3333, y: 7.66667, width: 24.3333, height: 23.6667) //하드코딩값. 실제 myAccountTabBarItem.image.size를 정확하게 가져올 방법을 못찾음.
         imgView.layer.cornerRadius = 10
         imgView.layer.masksToBounds = true
         imgView.contentMode = .scaleAspectFill
