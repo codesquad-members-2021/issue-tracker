ed76b7ab91bb6d31bb552a7033305ecb60645e30
Hoyoung Son <xzxking17@gmail.com>
2021-06-15 16:58:45 +0900

    [#57] feat: :sparkles: Recoil 적용

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -2,36 +2,42 @@ import { ThemeProvider } from 'styled-components';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Container } from '@material-ui/core';
 import { StylesProvider } from '@material-ui/core/styles';
+import { RecoilRoot } from 'recoil';
 
 import GlobalStyle from './util/styles/GlobalStyle';
 import theme from './util/styles/theme';
 
 import Header from './components/Header';
 import { RegisterPage, LoginPage, IssuePage, IssueWritePage, LabelListPage } from './pages';
+import BackgroundFluid from './components/Common/BackgroundFluid';
 
 const App = () => {
   return (
-    <StylesProvider injectFirst>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Container maxWidth="lg">
-          <Router>
-            <Switch>
-              <Route path={'/'} exact component={LoginPage} />
-              <Route path={'/register'} exact component={RegisterPage} />
-              <>
-                <Header />
+    <RecoilRoot>
+      <StylesProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <BackgroundFluid>
+            <Container maxWidth="lg">
+              <Router>
                 <Switch>
-                  <Route path={'/issues'} exact component={IssuePage} />
-                  <Route path={'/issues/write'} exact component={IssueWritePage} />
-                  <Route path={'/labels'} exact component={LabelListPage} />
+                  <Route path={'/'} exact component={LoginPage} />
+                  <Route path={'/register'} exact component={RegisterPage} />
+                  <>
+                    <Header />
+                    <Switch>
+                      <Route path={'/issues'} exact component={IssuePage} />
+                      <Route path={'/issues/write'} exact component={IssueWritePage} />
+                      <Route path={'/labels'} exact component={LabelListPage} />
+                    </Switch>
+                  </>
                 </Switch>
-              </>
-            </Switch>
-          </Router>
-        </Container>
-      </ThemeProvider>
-    </StylesProvider>
+              </Router>
+            </Container>
+          </BackgroundFluid>
+        </ThemeProvider>
+      </StylesProvider>
+    </RecoilRoot>
   );
 };
 
