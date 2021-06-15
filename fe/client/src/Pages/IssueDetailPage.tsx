176e3962ff05bb04f5236984938cf918d5aba601
jeonyeonkyu <ehb6915@naver.com>
2021-06-15 23:08:16 +0900

    Feat: 이슈만들기 페이지 UI 짧게 구현  #85

---
+++ fe/client/src/Pages/IssueDetailPage.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import HeadContent from '@components/issueDetail/HeadContent';
+import Tabs from '@components/createIssue/Tabs';
+import styled from 'styled-components';
+import CommentItem from '@components/issueDetail/CommentItem';
+import IconButton from '@components/common/IconButton';
+
+const IssueDetailPage = () => {
+  return (
+    <>
+      <HeadContent />
+      <FlexWrapper>
+        <CommentListWrapper>
+          <CommentItem />
+          <IconButton variant="contained" color="primary"
+            icon='plus' height="40px" margin='16px 0 0 0' background="#007AFF"
+            style={{ float: 'right' }}  >
+            코멘트 작성
+          </IconButton>
+        </CommentListWrapper>
+        <SideWrapper>
+          <Tabs />
+          <IconButton icon="trash" color="secondary">
+            이슈삭제
+          </IconButton>
+        </SideWrapper>
+      </FlexWrapper>
+    </>
+  )
+}
+
+const FlexWrapper = styled.div`
+  display: flex;
+`;
+
+const SideWrapper = styled.div`
+  display:table;
+  text-align: end;
+`;
+
+const CommentListWrapper = styled.div`
+  width: 100%;
+`;
+
+export default IssueDetailPage;
---
+++ fe/client/src/components/issueDetail/HeadContent.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import TitleInput from '@components/common/TitleInput';
+import IconButton from '@components/common/IconButton';
+import styled from 'styled-components';
+import CloseOrOpenLabel from '@components/common/CloseOrOpenLabel';
+
+const HeadContent = () => {
+  return (
+    <>
+      <HeadTitleWrapper>
+        <TitleInput />
+        <IconButton icon="close" variant="outlined" color="primary" minwidth="130px" margin='0 0 0 92px'>
+          <ButtonText>편집 취소</ButtonText>
+        </IconButton>
+        <IconButton icon="edit" variant="outlined" color="primary" minwidth="130px" margin='0 0 0 10px'>
+          <ButtonText>편집 취소</ButtonText>
+        </IconButton>
+      </HeadTitleWrapper>
+      <HeadDescWrapper>
+        <CloseOrOpenLabel isOpen />
+        <DescSpan>이 이슈가 1분전에 닫혓습니다</DescSpan>
+      </HeadDescWrapper>
+      <Hr />
+    </>
+  )
+}
+const HeadTitleWrapper = styled.div`
+  display: flex;
+  height: 56px;
+  margin-bottom: 16px;
+`;
+
+const HeadDescWrapper = styled.div`
+  margin-bottom: 36px;
+`;
+
+const ButtonText = styled.span`
+  font-size:12px;
+`;
+
+const DescSpan = styled.span`
+  font-size: 18px;
+  color:#4E4B66;
+`;
+const Hr = styled.hr`
+  margin:0 0 32px 0;
+  border-top:1px solid #D9DBE9;
+`;
+
+export default HeadContent;
