2739ddf7132c5d5a9e33301ad335bc1d661c9b33
Raccoon <nobllizdc@gmail.com>
2021-06-15 20:46:22 +0900

    fix: Token 유무에 따라 첫페이지 redirect되도록 수정 ghojeong/issue-tracker/#50

--- frontend/issue-cracker/src/App.tsx
+++ frontend/issue-cracker/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 import styled, { ThemeProvider } from 'styled-components';
 import theme from '../src/components/styles/theme';
 import { GlobalStyle } from './components/styles/GlobalStyle';
@@ -10,17 +10,26 @@ import IssueList from './components/layout/IssueList';
 import IssueAdd from './components/layout/IssueAdd';
 
 function App(): JSX.Element {
+  const token = localStorage.getItem('token');
+
   return (
     <BrowserRouter>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <AppStyle>
+          <Route exact path="/">
+            {token ? (
+              <Redirect to="/main/issue-list" />
+            ) : (
+              <Redirect to="/login" />
+            )}
+          </Route>
           <Route exact path="/login" component={LogIn} />
           <Route exact path="/callback" component={Callback} />
           <Route path="/main" component={Header} />
           <Route exact path="/main/issue-list" component={IssueList} />
-          <Route exact path="/main/add-issue" component={IssueAdd} />
-          {/* <Route exact path="/iss ue-add" component={Header} /> */}
+          <Route exact path="/main/issue-add" component={IssueAdd} />
+          {/* <Route exact path="/issue-add" component={Header} /> */}
           {/* <Route exact path="/main/add" component={AddList} /> */}
         </AppStyle>
       </ThemeProvider>
--- frontend/issue-cracker/src/index.tsx
+++ frontend/issue-cracker/src/index.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { StylesProvider } from '@material-ui/core/styles';
+import { RecoilRoot } from 'recoil';
 
 ReactDOM.render(
   <React.StrictMode>
-    <StylesProvider injectFirst>
-      <App />
-    </StylesProvider>
+    <RecoilRoot>
+      <StylesProvider injectFirst>
+        <App />
+      </StylesProvider>
+    </RecoilRoot>
   </React.StrictMode>,
   document.getElementById('root')
 );
