248db9a5a48b449472ab68131b8052fc4b50086a
PizzaCola-K <PizzaCola.K@gmail.com>
2021-06-15 17:00:13 +0900

    fix: Allow /api/login if Authorization is null
    - /api/login은 Authorization 헤더 없어도 허용

--- BE/src/main/java/com/codesquad/issuetracker/config/WebConfig.java
+++ BE/src/main/java/com/codesquad/issuetracker/config/WebConfig.java
@@ -25,6 +25,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authInterceptor)
-                .addPathPatterns("/api/**");
+                .addPathPatterns("/api/**")
+                .excludePathPatterns("/api/login/**");
     }
 }
