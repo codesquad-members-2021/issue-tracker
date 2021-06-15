bb35162d512e7edef5d35af4aad26be078e99b4c
bibi <bibi6666667@kakao.com>
2021-06-15 17:44:26 +0900

    feat : LabelService 클래스 추가 #3

--- BE/src/main/java/com/codesquad/issuetracker/service/LabelService.java
+++ BE/src/main/java/com/codesquad/issuetracker/service/LabelService.java
@@ -2,16 +2,11 @@ package com.codesquad.issuetracker.service;
 
 import com.codesquad.issuetracker.domain.Label;
 import com.codesquad.issuetracker.repository.LabelRepository;
-import com.codesquad.issuetracker.request.LabelRequest;
-import com.codesquad.issuetracker.response.LabelResponse;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Set;
 
 @Service
-@Transactional(readOnly = true)
 public class LabelService {
 
     private final LabelRepository labelRepository;
@@ -20,22 +15,11 @@ public class LabelService {
         this.labelRepository = labelRepository;
     }
 
-    @Transactional
-    public Label create(Label label) {
-        return labelRepository.save(label);
-    }
-
-
-    public List<LabelResponse> getLabels() {
-        return labelRepository.findAll().stream()
-                .map(label -> LabelResponse.create(label))
-                .collect(Collectors.toList());
-    }
-
-    @Transactional
-    public void edit(Long labelId, LabelRequest labelRequest){
-        Label label = labelRepository.getById(labelId);
-        label.update(labelRequest);
-        labelRepository.save(label);
+    public Set<Label> getLabelsForIssue(Long issueId) {
+        Set<Label> labels = labelRepository.findByIssueId(issueId);
+        for(Label label : labels) {
+            System.out.println(label);
+        }
+        return labels;
     }
 }
