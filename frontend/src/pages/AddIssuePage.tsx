16ab9dafa07a5ba6e3a1f83d6c202f144cb9fd9a
Hannah Park <ha3158987@gmail.com>
2021-06-15 16:52:39 +0900

    add: Editor 구현 시작

--- frontend/src/pages/AddIssuePage.tsx
+++ frontend/src/pages/AddIssuePage.tsx
@@ -1,13 +1,72 @@
 import styled from 'styled-components';
+import { useEffect, useState } from 'react'
 import ResponsiveLayout from '../components/common/ResponsiveLayout';
 
 const AddIssuePage = () => {
+  const [lastPressedKey, setLastPressedKey] = useState<any>();
+  const [accumulateLetters, setAccumulateLetters] = useState<any>([]);
+  const [textBlock, setTextBlock] = useState<any>({});
+
+
+  useEffect(() => {
+    console.log("textBlock", textBlock);
+  })
+
+  const inspectLastPressedKey = (curKey) => {
+    if (curKey === " " && lastPressedKey === "-") {
+      console.log("잘눌림");
+    }
+  }
+
+  const createNewTextBlock = () => {
+    const textObj = {
+      tag: '',
+      contents: ''
+    }
+    textObj.contents = accumulateLetters.join('');
+    if (textObj.contents[1] === "Shift" && textObj.contents[0] === '#') {
+      textObj.tag = `<h1>${textObj.contents}</h1>`;
+    }
+    setTextBlock(textObj);
+  }
+
+  const onKeyDownEvent = (e) => {
+    console.log("event", e.key, e.key === "Shift");
+    const pressedKey = e.key;
+
+    if (pressedKey !== "Shift" && pressedKey !== "Meta" && pressedKey !== "Enter" && pressedKey !== "Backspace") {
+      setAccumulateLetters([...accumulateLetters, pressedKey]);
+      if(lastPressedKey) {
+        inspectLastPressedKey(pressedKey);
+        //unordered list를 반환해줘야 함.
+      }
+      setLastPressedKey(pressedKey);
+    }
+
+    if (pressedKey === "Enter") {
+      createNewTextBlock();
+    }
+
+    if (pressedKey === "Backspace") {
+
+    }
+  }
+
   return (
     <AddIssueLayout>
       <InputLayer>
         <AddIssueTitle>새로운 이슈 작성</AddIssueTitle>
         <TitleInput type={"text"} placeholder={"제목"}></TitleInput>
-        <CommentInput type={"text"} placeholder={"코멘트를 입력하세요"}></CommentInput>
+        <CommentLayer>
+          <CommentInput
+            type={"text"}
+            placeholder={"코멘트를 입력하세요"}
+            onKeyDown={(e) => onKeyDownEvent(e)}
+          />
+          <CommentOutput>
+            {textBlock.contents}
+          </CommentOutput>
+        </CommentLayer>
       </InputLayer>
     </AddIssueLayout>
   )
@@ -24,7 +83,6 @@ const InputLayer = styled(ResponsiveLayout)`
   display: flex;
   flex-direction: column;
   padding: 0 80px;
-  /* border-radius: 1.4rem; */
 `
 const TitleInput = styled.input`
 
@@ -36,12 +94,28 @@ const TitleInput = styled.input`
   border: none;
   padding: 0 2.4rem;
 `
+
+const CommentLayer = styled.div`
+  display: flex;
+`
+
 const CommentInput = styled.input`
-  width: 88rem;
+  width: 50%;
   height: 34.3rem;
   border-radius: 1.4rem;
   background-color: #EFF0F6;
   border: none;
   padding: 0 2.4rem;
 `
+
+const CommentOutput = styled.div`
+  width: 50%;
+  height: 34.3rem;
+  border-radius: 1.4rem;
+  background-color: #EFF0F6;
+  border: none;
+  padding: 0 2.4rem;
+  margin-left: 2rem;
+`
+
 export default AddIssuePage;
