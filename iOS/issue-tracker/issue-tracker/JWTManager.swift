89430d29f7922d8c461573a5d4ffddeeeea38233
BMO <b38238496@gmail.com>
2021-06-15 17:20:38 +0900

    [#38] feat: JWTManager 구현
    Security 사용하여 get, set 구현
    테스트 코드 작성

---
+++ iOS/issue-tracker/issue-tracker/JWTManager.swift
@@ -0,0 +1,42 @@
+
+import Foundation
+import Security
+
+struct JWTManager {
+    func get() -> String? {
+        let keyChainQuery: NSDictionary = [
+            kSecClass: kSecClassCertificate,
+            kSecReturnData: kCFBooleanTrue,
+            kSecMatchLimit: kSecMatchLimitOne
+        ]
+        
+        var dataTypeRef: AnyObject?
+        let status = SecItemCopyMatching(keyChainQuery, &dataTypeRef)
+        
+        if status == errSecSuccess {
+            guard let retrievedData = dataTypeRef as? Data else {
+                return nil
+            }
+            let value = String(data: retrievedData, encoding: String.Encoding.utf8)
+            return value
+        } else {
+            return nil
+        }
+    }
+    
+    func set(jwt: String) -> Bool {
+        if jwt == "" {
+            return false
+        }
+        
+        let keyChainQuery: NSDictionary = [
+            kSecClass: kSecClassCertificate,
+            kSecValueData: jwt.data(using: .utf8, allowLossyConversion: false)
+        ]
+        
+        SecItemDelete(keyChainQuery)
+        
+        let status: OSStatus = SecItemAdd(keyChainQuery, nil)
+        return status == noErr
+    }
+}
---
+++ iOS/issue-tracker/issue-trackerTests/JWTManagerTest.swift
@@ -0,0 +1,57 @@
+//
+//  JWTManagerTest.swift
+//  issue-trackerTests
+//
+//  Created by user on 2021/06/15.
+//
+
+import XCTest
+
+class JWTManagerTest: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func test_jwt_set_success() {
+        let jwtManager = JWTManager()
+        let status = jwtManager.set(jwt: "1234")
+        
+        XCTAssertEqual(status, true)
+    }
+    
+    func test_jwt_set_failure() {
+        let jwtManager = JWTManager()
+        let status = jwtManager.set(jwt: "")
+        
+        XCTAssertEqual(status, false)
+    }
+    
+    func test_jwt_get_success() {
+        let jwtManager = JWTManager()
+        let status = jwtManager.set(jwt: "1234")
+        let get = jwtManager.get()
+        
+        XCTAssertEqual(get, "1234")
+    }
+    
+    func test_jwt_get_failure() {
+        let jwtManager = JWTManager()
+        let status = jwtManager.set(jwt: "1234")
+        let get = jwtManager.get()
+        
+        XCTAssertNotEqual(get, "234")
+    }
+    
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
