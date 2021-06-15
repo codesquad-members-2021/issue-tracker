9e600f0fbbbe444c8328947ba20ea38751795623
PizzaCola-K <PizzaCola.K@gmail.com>
2021-06-15 16:38:00 +0900

    feat: Add Interceptor
    - Interceptor를 추가하였습니다.
    - CORS Preflight를 위해서 Options는 Interceptor에서 허용

--- BE/src/main/java/com/codesquad/issuetracker/auth/interceptor/AuthInterceptor.java
+++ BE/src/main/java/com/codesquad/issuetracker/auth/interceptor/AuthInterceptor.java
@@ -1,8 +1,6 @@
 package com.codesquad.issuetracker.auth.interceptor;
 
-import com.auth0.jwt.interfaces.DecodedJWT;
 import com.codesquad.issuetracker.auth.component.JwtUtils;
-import com.codesquad.issuetracker.user.domain.User;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -20,6 +18,10 @@ public class AuthInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            return true;
+        }
+
         String token = getJwt(request);
         request.setAttribute("author", jwtUtils.getUserFromJwt(token));
         return true;
