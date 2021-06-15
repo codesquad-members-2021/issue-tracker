9832bb7ae1b0919ad05bf4e6290b7b00e5b6c38d
eve712 <62237639+eve712@users.noreply.github.com>
2021-06-15 18:25:08 +0900

    Add: #38 로그인 관련 URL 변수 추가

--- FE/issue-tracker/src/const/var.ts
+++ FE/issue-tracker/src/const/var.ts
@@ -1,1 +1,8 @@
 export const test = 'url';
+
+const clientID = process.env.REACT_APP_CLIENT_ID;
+const baseURL = 'http://localhost:3000/api';
+export const LOGIN_URL = `https://github.com/login/oauth/authorize?client_id=${clientID}&redirect_uri=${baseURL}/login/callback&scope=user`;
+
+export const TOKEN_URL =
+  'https://f88e009a-3e2b-4862-838e-1f2cde9b95ed.mock.pstmn.io/api/login/auth?client=web&code=';
