a5a2c2bfd922535cbb6508be776b33bd5bce3130
zeke <yangsin1031@naver.com>
2021-06-16 17:22:01 +0900

    chore: 🔧 AddViewController 이름변경

--- iOS/issue-tracker/issue-tracker/Controller/MilestoneViewController.swift
+++ iOS/issue-tracker/issue-tracker/Controller/MilestoneViewController.swift
@@ -33,7 +33,7 @@ class MilestoneViewController: UIViewController {
     }
     
     @objc func addMilestone() {
-        let nav = UINavigationController(rootViewController: AddViewController())
+        let nav = UINavigationController(rootViewController: AddMilestoneViewController())
         present(nav, animated: true, completion: nil)
     }
 }
