93c4d1355a846cacb12cd86012f4663c86dea2cf
somedaycode <somedaycode@gmail.com>
2021-06-16 21:07:59 +0900

    Feat: #49 fetchOnMouseEnter 함수 로직 임시로 저장

--- FE/issue-tracker/src/components/issues/tableHeader/MilestoneFilter.tsx
+++ FE/issue-tracker/src/components/issues/tableHeader/MilestoneFilter.tsx
@@ -7,34 +7,43 @@ import {
   Checkbox,
 } from '@chakra-ui/react';
 
-import { ReactComponent as DropDownIcon } from '@assets/dropDown.svg';
-import MenuTitle from '@components/common/MenuTitle';
 import { checkBoxStyle, menuItemStyle } from '@styles/chakraStyle';
+import { ReactComponent as DropDownIcon } from '@assets/dropDown.svg';
+
+import MenuTitle from '@components/common/MenuTitle';
 import { menuBtnStyle } from './style';
 
 function MilestoneFilter() {
+  const query = 'milestones';
+  const fetchOnMouseEnter = (
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>,
+    query: string
+  ) => {};
+
   return (
-    <Menu>
-      <MenuButton
-        className="menu-title"
-        {...menuBtnStyle}
-        as={Button}
-        rightIcon={<DropDownIcon />}
-      >
-        마일스톤
-      </MenuButton>
-      <MenuList>
-        <MenuTitle>마일스톤 필터</MenuTitle>
-        <MenuItem {...menuItemStyle}>
-          마일스톤 없는 필터
-          <Checkbox {...checkBoxStyle} />
-        </MenuItem>
-        <MenuItem {...menuItemStyle}>
-          마스터즈 코스
-          <Checkbox {...checkBoxStyle} />
-        </MenuItem>
-      </MenuList>
-    </Menu>
+    <div onMouseEnter={(e) => fetchOnMouseEnter(e, query)}>
+      <Menu>
+        <MenuButton
+          className="menu-title"
+          {...menuBtnStyle}
+          as={Button}
+          rightIcon={<DropDownIcon />}
+        >
+          마일스톤
+        </MenuButton>
+        <MenuList>
+          <MenuTitle>마일스톤 필터</MenuTitle>
+          <MenuItem {...menuItemStyle}>
+            마일스톤 없는 필터
+            <Checkbox {...checkBoxStyle} />
+          </MenuItem>
+          <MenuItem {...menuItemStyle}>
+            마스터즈 코스
+            <Checkbox {...checkBoxStyle} />
+          </MenuItem>
+        </MenuList>
+      </Menu>
+    </div>
   );
 }
 
