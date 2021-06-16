55250e54daa807e11bd5099ada141865533c06db
somedaycode <somedaycode@gmail.com>
2021-06-16 20:47:45 +0900

    Chore: 에러처리, hostAPI 문자열 추가

--- FE/issue-tracker/src/const/var.ts
+++ FE/issue-tracker/src/const/var.ts
@@ -1,2 +1,5 @@
 export const test = 'url';
 export const issueAPI = process.env.REACT_APP_API_ISSUE;
+export const hostAPI = process.env.REACT_APP_API;
+export const urlErrorMsg = '요청하신 주소에 문제가 있습니다.';
+export const issueListErrorMsg = '이슈 목록을 불러오는 데 실패했습니다.';
