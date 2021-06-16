438bfbb4653cd39caab2b3df0386206628439e98
eve712 <62237639+eve712@users.noreply.github.com>
2021-06-17 05:06:33 +0900

    Style: #18 작성취소 버튼에 X표시 svg 컴포넌트로 변경

--- FE/issue-tracker/src/components/newIssue/ButtonBox.tsx
+++ FE/issue-tracker/src/components/newIssue/ButtonBox.tsx
@@ -1,13 +1,18 @@
 import styled from 'styled-components';
 
+import { Link } from 'react-router-dom';
 import { Button } from '@chakra-ui/button';
+import { ReactComponent as CancelIcon } from '@assets/cancel.svg';
 
 function ButtonBox() {
   return (
     <ButtonBoxWrap>
-      <CancelBtn>
-        <span>x 작성취소</span>
-      </CancelBtn>
+      <Link to="issues">
+        <CancelBtn>
+          <CancelIcon className="cancel_icon" />
+          <span>작성취소</span>
+        </CancelBtn>
+      </Link>
       <Button
         width="240px"
         height="56px"
@@ -34,7 +39,14 @@ const CancelBtn = styled.button`
   margin-right: 32px;
   width: 83px;
   height: 32px;
-
+  display: flex;
+  align-items: center;
+  .cancel_icon {
+    margin-right: 5px;
+    path {
+      stroke: ${({ theme }) => theme.colors.gr_label};
+    }
+  }
   span {
     color: ${({ theme }) => theme.colors.gr_label};
     font-weight: ${({ theme }) => theme.fontWeights.bold};
