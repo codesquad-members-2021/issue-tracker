a5362148cc1052635e69c8abca1f90d244750b08
jeong-inho <derosatam76@gmail.com>
2021-06-17 18:59:42 +0900

    fix: (#60) 배포 스크립트 수정

--- BE/deploy.sh
+++ BE/deploy.sh
@@ -3,7 +3,7 @@
 REPOSITORY=/home/ubuntu/issue-tracker/
 cd $REPOSITORY
 APP_NAME=issue-tracker
-JAR_NAME=$(ls $REPOSITORY/ | grep '.jar' | tail -n 1)
+JAR_NAME=$(ls $REPOSITORY/build/libs | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/$JAR_NAME
 CURRENT_PID=$(pgrep -f $APP_NAME)
 
@@ -31,3 +31,21 @@ nohup java -jar $JAR_PATH &
 #aws s3 cp s3://issue-tracker-jane-eno/issue-tracker.jar ~/
 #nohup java -jar issue-tracker.jar &
 #aws s3 rm s3://issue-tracker-jane-eno/issue-tracker.jar
+
+#cd $REPOSITORY
+#APP_NAME=issue-tracker
+#JAR_NAME=$(ls $REPOSITORY/ | grep '.jar' | tail -n 1)
+#JAR_PATH=$REPOSITORY/$JAR_NAME
+#CURRENT_PID=$(pgrep -f $APP_NAME)
+#
+#if [ -z $CURRENT_PID ]
+#then
+#  echo "> Nothing to end."
+#else
+#  echo "> kill -9 $CURRENT_PID"
+#  kill -9 $CURRENT_PID
+#  sleep 5
+#fi
+#
+#echo "> $JAR_PATH deploy"
+#nohup java -jar $JAR_PATH &
