483c58df024b33a77802c6a04da88defe940f956
woody <woojihye2339@gmail.com>
2021-06-17 16:08:25 +0900

    feat : UserResponse 정적 팩토리 메소드 생성 #3
    > UserResponse::create 팩토리 메소드 생성

--- BE/src/main/java/com/codesquad/issuetracker/response/UserResponse.java
+++ BE/src/main/java/com/codesquad/issuetracker/response/UserResponse.java
@@ -1,7 +1,12 @@
 package com.codesquad.issuetracker.response;
 
+import com.codesquad.issuetracker.domain.User;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
+@AllArgsConstructor
 public class UserResponse {
 
     private String name;
@@ -9,16 +14,8 @@ public class UserResponse {
     @JsonProperty("login_id")
     private String loginId;
 
-    public UserResponse(String name, String loginId) {
-        this.name = name;
-        this.loginId = loginId;
+    public static UserResponse create(User user) {
+        return new UserResponse(user.getName(), user.getLoginId());
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getloginId() {
-        return loginId;
-    }
 }
