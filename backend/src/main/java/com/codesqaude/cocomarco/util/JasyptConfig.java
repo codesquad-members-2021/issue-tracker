c960fd0a19edeb85a5a0d76773cd4e2472c2b766
95degree <95degree@naver.com>
2021-06-17 17:15:28 +0900

    feat : 암호화

--- backend/src/main/java/com/codesqaude/cocomarco/util/JasyptConfig.java
+++ backend/src/main/java/com/codesqaude/cocomarco/util/JasyptConfig.java
@@ -3,15 +3,19 @@ package com.codesqaude.cocomarco.util;
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JasyptConfig {
 
+    @Value("${key}")
+    private String key;
+
     public StringEncryptor stringEncryptor() {
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-        config.setPassword("cocomarco");
+        config.setPassword(key);
         encryptor.setConfig(config);
         return encryptor;
     }
