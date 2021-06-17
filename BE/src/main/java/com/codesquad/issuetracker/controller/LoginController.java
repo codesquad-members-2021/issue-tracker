807857073a45ae096735b7723f8e25709a32c6cd
bibi <bibi6666667@kakao.com>
2021-06-17 15:26:35 +0900

    feat : Github OAuth Client 정보들을 환경변수로 치환

--- BE/src/main/java/com/codesquad/issuetracker/controller/LoginController.java
+++ BE/src/main/java/com/codesquad/issuetracker/controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.codesquad.issuetracker.controller;
 
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +10,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class LoginController {
 
     private final String GITHUB_URL = "https://github.com/login/oauth/authorize";
-    private final String CLIENT_ID = "7bd8b036c3471804563e";
+    private final String CLIENT_ID;
+
+    public LoginController(Environment environment) {
+        this.CLIENT_ID = environment.getProperty("github.client.id");
+    }
 
     @GetMapping
     public String login() {
