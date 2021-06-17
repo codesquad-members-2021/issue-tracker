c723f7f7c4ede7283a00f316e0db1ae3df524118
Dami Kim <dami.k.dev@gmail.com>
2021-06-18 02:56:54 +0900

    feat: Label Card style 추가 (#63)

---
+++ frontend/src/styles/StyledLayout .jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+//컴포넌트 명 수정 예정
+
+export const CenterJcAi = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+
+export const CenterAi = styled.div`
+	display: flex;
+	align-items: center;
+`;
