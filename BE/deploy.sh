3e27bf4d50637b322f36a93660e68fca777c4c35
jeong-inho <derosatam76@gmail.com>
2021-06-17 19:27:25 +0900

    fix: (#60) 배포 스크립트 수정

--- BE/deploy.sh
+++ BE/deploy.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
-REPOSITORY=/home/ubuntu/issue-tracker/
+REPOSITORY=/home/ubuntu/issue-tracker
 cd $REPOSITORY
-APP_NAME=issue-tracker
 JAR_NAME=$(ls $REPOSITORY/build/libs | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
-CURRENT_PID=$(pgrep -f $APP_NAME)
 fuser -k 8080/tcp
 nohup java -jar $JAR_PATH &
