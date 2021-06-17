543c96b9328a6422a39b9dfd2db6e68cf416a1b5
Dami Kim <dami.k.dev@gmail.com>
2021-06-18 02:53:24 +0900

    style: 컴포넌트 이름 svg stroke 값 수정

--- frontend/src/components/common/Navigator.jsx
+++ frontend/src/components/common/Navigator.jsx
@@ -20,7 +20,7 @@ const Navigator = () => {
 	};
 
 	return (
-		<Wrapper>
+		<NavigatorLayout>
 			<ButtonGroup
 				milestoneCount={"N"}
 				milestoneClickEvent={handleMilestoneClick}
@@ -29,13 +29,13 @@ const Navigator = () => {
 				isMainPage={false}
 			/>
 			<AddButton text="추가" clickEvent={() => {}} />
-		</Wrapper>
+		</NavigatorLayout>
 	);
 };
 
-export default Navigator;
-
-const Wrapper = styled.div`
+const NavigatorLayout = styled.div`
 	display: flex;
 	justify-content: space-between;
 `;
+
+export default Navigator;
