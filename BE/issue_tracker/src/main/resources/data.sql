a40a8ed50ef18f8df8f4141a1a66389a7920cc34
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-06-17 12:11:11 +0900

    chore : 이모지 데이터 추가

--- BE/issue_tracker/src/main/resources/data.sql
+++ BE/issue_tracker/src/main/resources/data.sql
@@ -1,45 +1,52 @@
--- user
-insert into `user`(id, email, oauth_resource, profile_image, username)
-values (1, null, "GITHUB", "https://avatars.githubusercontent.com/u/65011131?v=4", "kimnayeon0108");
-insert into `user`(id, email, oauth_resource, profile_image, username)
-values (2, "ehdrhelr@gmail.com", "GITHUB", "프로필 이미지 url", "shion");
-
--- milestone
-insert into milestone(id, content, created_date, due_date, `open`, title, deleted)
-values (1, "백엔드 1주차 마일스톤 내용", "2021-06-01", "2021-06-15", false, "BE 1주차", false),
-       (2, "백엔드 2주차 마일스톤 내용", "2021-06-14", "2021-06-30", true, "BE 2주차", false),
-       (3, "백엔드 3주차 마일스톤 내용", "2021-06-15", "2021-06-30", true, "BE 3주차", false);
-
--- issue
-insert into issue(id, created_time, `open`, title, writer_id, milestone_id, deleted)
-values (1, CURRENT_TIMESTAMP, false, "이슈 제목1", 1, 1, false),
-       (2, CURRENT_TIMESTAMP, true, "이슈 제목2", 2, 1, false);
-
--- label
-insert into label(id, title, content, color, deleted)
-values (1, "BE", "백엔드 라벨", "#3DDCFF", false),
-       (2, "FE", "프론트엔드 라벨", "#3DDCFF", true),
-       (3, "iOS", "iOS 라벨", "#3DDCFF", false);
-
--- issue_label
-insert into issue_label(issue_id, label_id)
-values (1, 1);
-
--- issue_assignee
-insert into issue_assignee(issue_id, user_id)
-values (1, 1);
-
--- comment
-insert into comment(id, content, file, created_time, issue_id, writer_id, deleted)
-values (1, "good", "file url", CURRENT_TIMESTAMP, 1, 1, false),
-       (2, "nice", "file url", CURRENT_TIMESTAMP, 1, 2, true);
+-- -- user
+-- insert into `user`(id, email, oauth_resource, profile_image, username)
+-- values (1, null, "GITHUB", "https://avatars.githubusercontent.com/u/65011131?v=4", "kimnayeon0108");
+-- insert into `user`(id, email, oauth_resource, profile_image, username)
+-- values (2, "ehdrhelr@gmail.com", "GITHUB", "프로필 이미지 url", "shion");
+--
+-- -- milestone
+-- insert into milestone(id, content, created_date, due_date, `open`, title, deleted)
+-- values (1, "백엔드 1주차 마일스톤 내용", "2021-06-01", "2021-06-15", false, "BE 1주차", false),
+--        (2, "백엔드 2주차 마일스톤 내용", "2021-06-14", "2021-06-30", true, "BE 2주차", false),
+--        (3, "백엔드 3주차 마일스톤 내용", "2021-06-15", "2021-06-30", true, "BE 3주차", false);
+--
+-- -- issue
+-- insert into issue(id, created_time, `open`, title, writer_id, milestone_id, deleted)
+-- values (1, CURRENT_TIMESTAMP, false, "이슈 제목1", 1, 1, false),
+--        (2, CURRENT_TIMESTAMP, true, "이슈 제목2", 2, 1, false);
+--
+-- -- label
+-- insert into label(id, title, content, color, deleted)
+-- values (1, "BE", "백엔드 라벨", "#3DDCFF", false),
+--        (2, "FE", "프론트엔드 라벨", "#3DDCFF", true),
+--        (3, "iOS", "iOS 라벨", "#3DDCFF", false);
+--
+-- -- issue_label
+-- insert into issue_label(issue_id, label_id)
+-- values (1, 1);
+--
+-- -- issue_assignee
+-- insert into issue_assignee(issue_id, user_id)
+-- values (1, 1);
+--
+-- -- comment
+-- insert into comment(id, content, file, created_time, issue_id, writer_id, deleted)
+-- values (1, "good", "file url", CURRENT_TIMESTAMP, 1, 1, false),
+--        (2, "nice", "file url", CURRENT_TIMESTAMP, 1, 2, true);
 
 -- emoji
-insert
-into emoji(id, `name`, deleted)
-values (1, ":happy face", false),
-       (2, ":surprise face", false);
+insert into emoji(id, `name`, deleted)
+values (1, ":happy_face", false),
+       (2, ":surprise_face", false),
+       (3, ":heart", false),
+       (4, ":sun", false),
+       (5, ":cloud", false),
+       (6, ":gold_medal", false),
+       (7, ":honux", false),
+       (8, ":tears", false),
+       (9, ":thumb_up", false),
+       (10, ":fire", false);
 
--- comment_emoji
-insert into comment_emoji(comment_id, emoji_id)
-values (1, 1);
+-- -- comment_emoji
+-- insert into comment_emoji(comment_id, emoji_id)
+-- values (1, 1);
