ff17e8e669d0fdb6d4ca3cf0758d02c2dff15a68
Tree <gmldbs1109@naver.com>
2021-06-17 16:05:52 +0900

    [#18] chore : :hammer: 이슈 전체목록 조회와 이슈조회에서 차이있는 점이 있어 DTO 분리

--- BE/server/src/main/java/com/team11/issue/dto/issue/IssueDetailResponseDTO.java
+++ BE/server/src/main/java/com/team11/issue/dto/issue/IssueDetailResponseDTO.java
@@ -3,37 +3,46 @@ package com.team11.issue.dto.issue;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.team11.issue.domain.History;
 import com.team11.issue.domain.Issue;
+import com.team11.issue.dto.comment.CommentsResponseDTO;
 import com.team11.issue.dto.history.HistoryResponseDTO;
 import com.team11.issue.dto.label.LabelsResponseDTO;
 import com.team11.issue.dto.milestone.IssueCountResponseDTO;
 import com.team11.issue.dto.milestone.MilestoneResponseDTO;
+import com.team11.issue.dto.user.UserResponseDTO;
+import com.team11.issue.dto.user.UsersResponseDTO;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-@JsonPropertyOrder({"issueId", "history", "isOpen", "title", "contents", "milestone", "labels"})
+@JsonPropertyOrder({"issueId", "history", "isOpen", "title", "contents", "author", "assignee", "milestone", "labels", "comments"})
 @RequiredArgsConstructor
 @Builder
 @Getter
-public class IssueResponseDTO {
+public class IssueDetailResponseDTO {
 
     private final Long issueId;
     private final HistoryResponseDTO history;
     private final boolean isOpen;
     private final String title;
     private final String contents;
+    private final UserResponseDTO author;
+    private final UsersResponseDTO assignees;
     private final MilestoneResponseDTO milestone;
     private final LabelsResponseDTO labels;
+    private final CommentsResponseDTO comments;
 
-    public static IssueResponseDTO from(Issue issue, History history, IssueCountResponseDTO issueCountResponseDTO, LabelsResponseDTO labelsResponseDTO) {
-        return IssueResponseDTO.builder()
+    public static IssueDetailResponseDTO from(Issue issue, History history, UsersResponseDTO usersResponseDTO, IssueCountResponseDTO issueCountResponseDTO, LabelsResponseDTO labelsResponseDTO, CommentsResponseDTO commentsResponseDTO) {
+        return IssueDetailResponseDTO.builder()
                 .issueId(issue.getId())
                 .history(HistoryResponseDTO.from(history))
                 .isOpen(issue.isOpen())
                 .title(issue.getTitle())
                 .contents(issue.getContents())
+                .author(UserResponseDTO.from(issue.getUser()))
+                .assignees(usersResponseDTO)
                 .milestone(MilestoneResponseDTO.from(issue.getMilestone(), issueCountResponseDTO))
                 .labels(labelsResponseDTO)
+                .comments(commentsResponseDTO)
                 .build();
 
     }
