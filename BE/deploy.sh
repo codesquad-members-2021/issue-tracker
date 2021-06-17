b4e63cdc00106b884a498041f340fa1a86485769
jeong-inho <derosatam76@gmail.com>
2021-06-17 19:46:52 +0900

    fix: (#60) 배포 스크립트 수정

--- BE/deploy.sh
+++ BE/deploy.sh
@@ -4,4 +4,4 @@ cd $REPOSITORY
 JAR_NAME=$(ls $REPOSITORY/build/libs | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 fuser -k 8080/tcp
-nohup java -jar $JAR_PATH &
+nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
