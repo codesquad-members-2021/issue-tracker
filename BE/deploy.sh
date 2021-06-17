2ef37b82768ff688c64a4952dc03ccd5ca5e6bbc
jeong-inho <derosatam76@gmail.com>
2021-06-17 19:07:14 +0900

    fix: (#60) 배포 스크립트 수정

--- BE/deploy.sh
+++ BE/deploy.sh
@@ -1,51 +1,9 @@
-# deploy.sh
 #!/usr/bin/env bash
 REPOSITORY=/home/ubuntu/issue-tracker/
 cd $REPOSITORY
 APP_NAME=issue-tracker
 JAR_NAME=$(ls $REPOSITORY/build/libs | grep '.jar' | tail -n 1)
-JAR_PATH=$REPOSITORY/$JAR_NAME
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 CURRENT_PID=$(pgrep -f $APP_NAME)
-
-if [ -z $CURRENT_PID ]
-then
-  echo "> Nothing to end."
-else
-  echo "> kill -9 $CURRENT_PID"
-  kill -9 $CURRENT_PID
-  sleep 5
-fi
-
-echo "> $JAR_PATH deploy"
+fuser -k 8080/tcp
 nohup java -jar $JAR_PATH &
-
-
-#CK=`aws s3 ls s3://issue-tracker-jane-eno/issue-tracker.jar`
-#echo $CK
-#if [[ ! $CK ]]; then
-#        echo "no need to build"
-#        exit 0
-#fi
-#
-#fuser -k 8080/tcp
-#aws s3 cp s3://issue-tracker-jane-eno/issue-tracker.jar ~/
-#nohup java -jar issue-tracker.jar &
-#aws s3 rm s3://issue-tracker-jane-eno/issue-tracker.jar
-
-#cd $REPOSITORY
-#APP_NAME=issue-tracker
-#JAR_NAME=$(ls $REPOSITORY/ | grep '.jar' | tail -n 1)
-#JAR_PATH=$REPOSITORY/$JAR_NAME
-#CURRENT_PID=$(pgrep -f $APP_NAME)
-#
-#if [ -z $CURRENT_PID ]
-#then
-#  echo "> Nothing to end."
-#else
-#  echo "> kill -9 $CURRENT_PID"
-#  kill -9 $CURRENT_PID
-#  sleep 5
-#fi
-#
-#echo "> $JAR_PATH deploy"
-#nohup java -jar $JAR_PATH &
