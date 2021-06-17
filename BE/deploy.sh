71b00ee93885b5794189d5e3105fe79d83eec1a2
jeong-inho <derosatam76@gmail.com>
2021-06-17 17:08:54 +0900

    fix: (#60) 배포 스크립트 수정

--- BE/deploy.sh
+++ BE/deploy.sh
@@ -1,21 +1,23 @@
 # deploy.sh
 #!/usr/bin/env bash
-REPOSITORY=/deploy
+REPOSITORY=/home/ubuntu/issue-tracker/
 cd $REPOSITORY
-APP_NAME=action_codedeploy
-JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
-JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+APP_NAME=issue-tracker
+JAR_NAME=$(ls $REPOSITORY/ | grep '.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/$JAR_NAME
 CURRENT_PID=$(pgrep -f $APP_NAME)
+
 if [ -z $CURRENT_PID ]
 then
   echo "> Nothing to end."
 else
   echo "> kill -9 $CURRENT_PID"
-  kill -15 $CURRENT_PID
+  kill -9 $CURRENT_PID
   sleep 5
 fi
+
 echo "> $JAR_PATH deploy"
-nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar $JAR_PATH &
 
 
 #CK=`aws s3 ls s3://issue-tracker-jane-eno/issue-tracker.jar`
