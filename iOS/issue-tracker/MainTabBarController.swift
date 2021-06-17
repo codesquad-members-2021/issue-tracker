fc9281b7702f93f5959e93667f8fea64c0f09cc9
dahun-lee-daji <ssss6596@gmail.com>
2021-06-17 15:19:07 +0900

    Refactor: MainTabBar의 Coordinator를 외부에서 주입 (#61)

--- iOS/issue-tracker/MainTabBarController.swift
+++ iOS/issue-tracker/MainTabBarController.swift
@@ -9,14 +9,31 @@ import UIKit
 
 class MainTabBarController: UITabBarController {
     
-    var issueCoordinator = IssueCoordinator()
-      
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setChildViewControllers()
+    private var coordinators: [Coordinator]
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        self.coordinators = []
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
     }
     
-    func setChildViewControllers() {
-        self.viewControllers = [issueCoordinator.navigationController]
+    required init?(coder: NSCoder) {
+        self.coordinators = []
+        super.init(coder: coder)
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func appendCoordinators(_ coordinators : Coordinator...) {
+        self.coordinators.append(contentsOf: coordinators)
+        var viewControllers = [UIViewController]()
+        
+        self.coordinators.enumerated().makeIterator().forEach({
+            (index, coordinator) in
+            viewControllers.append(coordinator.navigationController)
+        })
+        self.viewControllers = viewControllers
+    }
+    
 }
--- iOS/issue-tracker/Supporting/SceneDelegate.swift
+++ iOS/issue-tracker/Supporting/SceneDelegate.swift
@@ -15,6 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let tempVC = MainTabBarController()
+        tempVC.appendCoordinators(IssueCoordinator())
         
         let appWindow = UIWindow(frame: windowScene.coordinateSpace.bounds)
         appWindow.windowScene = windowScene
