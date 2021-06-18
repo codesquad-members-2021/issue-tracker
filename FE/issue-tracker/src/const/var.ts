31a05189d8932512497784709e252409d8db9c49
eve712 <62237639+eve712@users.noreply.github.com>
2021-06-19 05:51:22 +0900

    Fix: 배포된 baseURL로 수정

--- FE/issue-tracker/src/const/var.ts
+++ FE/issue-tracker/src/const/var.ts
@@ -1,10 +1,9 @@
 export const issueAPI = process.env.REACT_APP_API_ISSUE;
 
-export const baseURL =
-  'https://dc48e9f0-781a-4770-ac5b-997aadcc475a.mock.pstmn.io/api';
+export const baseURL = 'http://15.164.68.136/api';
 
 const clientID = process.env.REACT_APP_CLIENT_ID;
 const localURL = 'http://localhost:3000/api';
-export const LOGIN_URL = `https://github.com/login/oauth/authorize?client_id=${clientID}&redirect_uri=${localURL}/login/callback&scope=user`;
+export const LOGIN_URL = `http://github.com/login/oauth/authorize?client_id=${clientID}&redirect_uri=${localURL}/login/callback&scope=user`;
 
 export const TOKEN_URL = `${baseURL}/login/auth?client=web&code=`;
