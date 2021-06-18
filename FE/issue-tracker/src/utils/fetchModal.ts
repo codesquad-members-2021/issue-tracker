cd2205d61498ddbaa1ccfc436d4b47f21a6cbdbf
eve712 <62237639+eve712@users.noreply.github.com>
2021-06-19 05:53:34 +0900

    Feat: #39 이슈 사이드바(SelectBox) fetch요청 에러처리

--- FE/issue-tracker/src/utils/fetchModal.ts
+++ FE/issue-tracker/src/utils/fetchModal.ts
@@ -5,8 +5,21 @@ type fetchModalType = {
   setState: (state: any) => void;
 };
 
+const handleError = (status: number) => {
+  if (status >= 400 && status < 500) throw `💢 에러!!!(${status})`;
+  else if (status >= 500) throw '🚨 서버 확인!!!';
+};
+
 export const fetchModal = async ({ path, setState }: fetchModalType) => {
-  const response = await fetch(`${baseURL}/${path}`);
-  const json = await response.json();
-  setState(json);
+  try {
+    const res = await fetch(`${baseURL}/${path}`);
+    handleError(res.status);
+    const json = await res.json();
+    setState(json);
+  } catch (error) {
+    if (typeof error === 'string') throw error;
+    else {
+      throw '🔺 요청 주소 확인!!';
+    }
+  }
 };
