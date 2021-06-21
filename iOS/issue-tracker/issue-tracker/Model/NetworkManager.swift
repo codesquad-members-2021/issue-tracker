e25ee6664ebfecf5bf04abf0c8d40fc7329945a9
zeke <yangsin1031@naver.com>
2021-06-21 23:37:04 +0900

    feat: ✨ deleteRequest구현 (#79)

--- iOS/issue-tracker/issue-tracker/Model/NetworkManager.swift
+++ iOS/issue-tracker/issue-tracker/Model/NetworkManager.swift
@@ -11,6 +11,7 @@ import Alamofire
 protocol Networkable {
     func request<T: Decodable>(url: URL, decodableType: T.Type, completion: @escaping (T) -> Void)
     func postRequest<T: Encodable>(url: URL, encodable: T, completion: @escaping () -> Void)
+    func deleteRequest(url: URL, completion: @escaping () -> Void)
 }
 
 class NetworkManager: Networkable {
@@ -35,6 +36,19 @@ class NetworkManager: Networkable {
             .responseData { response in
                 switch response.result {
                 case .success :
+                    completion()
+                case .failure(let error):
+                    print(error)
+                }
+            }
+    }
+
+    func deleteRequest(url: URL, completion: @escaping () -> Void) {
+        AF.request(url, method: .delete)
+            .validate(statusCode: 200..<300)
+            .response { response in
+                switch response.result {
+                case .success:
                     completion()
                 case .failure(let error):
                     print(error)
