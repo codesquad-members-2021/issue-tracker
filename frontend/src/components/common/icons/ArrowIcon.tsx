6fc73f5b377d1fb7d71be56b79ea397091d4468d
Jaeuk-YOO <ryou9305@naver.com>
2021-06-21 18:52:17 +0900

    fix: 아이콘 컴포넌트 컨벤션 변경

--- frontend/src/components/common/icons/ArrowIcon.tsx
+++ frontend/src/components/common/icons/ArrowIcon.tsx
@@ -2,7 +2,7 @@ const ArrowIcon = () => {
 
   return (
     <svg width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M1 1L5 5L9 1" stroke="#6E7191" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M1 1L5 5L9 1" stroke="#6E7191" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round"/>
     </svg>
   )
 }
