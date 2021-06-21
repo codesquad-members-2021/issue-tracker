c08700bc4770c8a19c0837f7fa9e3f5d7a0718f6
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-06-21 18:35:41 +0900

    refactor: AllArgsConstructor 어노테이션 사용 대신 직접 생성자 작성하도록 변경

--- BE/issue_tracker/src/main/java/team02/issue_tracker/dto/issue/IssueMilestoneResponse.java
+++ BE/issue_tracker/src/main/java/team02/issue_tracker/dto/issue/IssueMilestoneResponse.java
@@ -1,10 +1,8 @@
 package team02.issue_tracker.dto.issue;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import team02.issue_tracker.domain.Milestone;
 
-@AllArgsConstructor
 @Getter
 public class IssueMilestoneResponse {
 
@@ -12,6 +10,7 @@ public class IssueMilestoneResponse {
     private String title;
 
     public IssueMilestoneResponse(Milestone milestone) {
-        this(milestone.getId(), milestone.getTitle());
+        this.id = milestone.getId();
+        this.title = milestone.getTitle();
     }
 }
