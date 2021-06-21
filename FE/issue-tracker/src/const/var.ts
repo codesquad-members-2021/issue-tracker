2f67c42de8e8ea305807ef88ccdc89476fb758be
somedaycode <somedaycode@gmail.com>
2021-06-21 22:16:28 +0900

    Chore: 메뉴 옵션을 위한 변수 추가

--- FE/issue-tracker/src/const/var.ts
+++ FE/issue-tracker/src/const/var.ts
@@ -7,9 +7,37 @@ export const issueListErrorMsg = '이슈 목록을 불러오는 데 실패했습
 
 const clientID = process.env.REACT_APP_CLIENT_ID;
 
-const baseURL = 'http://localhost:3000/api';
+const baseURL = 'http://15.164.68.136/api';
+const localURL = 'http://localhost:3000/api';
 
-export const LOGIN_URL = `https://github.com/login/oauth/authorize?client_id=${clientID}&redirect_uri=${baseURL}/login/callback&scope=user`;
+export const LOGIN_URL = `https://github.com/login/oauth/authorize?client_id=${clientID}&redirect_uri=${localURL}/login/callback&scope=user`;
 
-export const TOKEN_URL =
-  'https://f88e009a-3e2b-4862-838e-1f2cde9b95ed.mock.pstmn.io/api/login/auth?client=web&code=';
+export const TOKEN_URL = `${baseURL}/login/auth?client=web&code=`;
+
+export const menuOptions = [
+  {
+    id: 1,
+    title: '열린 이슈',
+    value: 'open',
+  },
+  {
+    id: 2,
+    title: '내가 작성한 이슈',
+    value: 'author',
+  },
+  {
+    id: 3,
+    title: '나에게 할당된 이슈',
+    value: 'assignee',
+  },
+  {
+    id: 4,
+    title: '내가 댓글을 남긴 이슈',
+    value: 'comment',
+  },
+  {
+    id: 5,
+    title: '닫힌 이슈',
+    value: 'close',
+  },
+];
