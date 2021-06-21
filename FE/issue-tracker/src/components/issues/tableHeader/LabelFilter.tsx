874f7ece0f3ed50591118d7dc89a2329d624f405
somedaycode <somedaycode@gmail.com>
2021-06-21 22:25:43 +0900

    Feat: #35 필터 체크박스에 따른 이슈 목록 출력

--- FE/issue-tracker/src/components/issues/tableHeader/LabelFilter.tsx
+++ FE/issue-tracker/src/components/issues/tableHeader/LabelFilter.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useRecoilState } from 'recoil';
 
 import {
@@ -12,14 +13,40 @@ import { checkBoxStyle, menuItemStyle } from '@styles/chakraStyle';
 import { ReactComponent as DropDownIcon } from '@assets/dropDown.svg';
 import { menuBtnStyle } from './style';
 
+import { querySet } from '@store/atoms/issueList';
 import { labelFilterList } from '@store/atoms/issueFilter';
 import { fetchOnMouseEnter } from '@utils/fetchOnEnter';
 
 import MenuTitle from '@components/common/MenuTitle';
+import type { CheckBoxs } from './MilestoneFilter';
 
 function LabelFilter() {
   const [labelList, setLabelList] = useRecoilState(labelFilterList);
+  const [query, setQuery] = useRecoilState(querySet);
+
   const { data } = labelList;
+  const [isChecked, setChecked] = useState<CheckBoxs>({});
+
+  const handleClickCheckBox = (e: React.MouseEvent) => {
+    const target = e.target as HTMLInputElement;
+    const checkNumber = target.id;
+    if (checkNumber === '') return;
+
+    const toggleBoolean = isChecked[checkNumber] === true ? false : true;
+    if (toggleBoolean)
+      setQuery({
+        closed: null,
+        author: null,
+        assignee: null,
+        label: Number(checkNumber),
+        milestone: null,
+      });
+
+    setChecked({
+      ...isChecked,
+      [checkNumber]: toggleBoolean,
+    });
+  };
 
   return (
     <div onMouseEnter={() => fetchOnMouseEnter(labelList, setLabelList)}>
@@ -36,9 +63,13 @@ function LabelFilter() {
           <MenuTitle>레이블 필터</MenuTitle>
           {data.map(({ id, title }) => {
             return (
-              <MenuItem key={id} {...menuItemStyle}>
+              <MenuItem
+                key={id}
+                onClick={handleClickCheckBox}
+                {...menuItemStyle}
+              >
                 {title}
-                <Checkbox {...checkBoxStyle} />
+                <Checkbox id={id} checked={isChecked[id]} {...checkBoxStyle} />
               </MenuItem>
             );
           })}
