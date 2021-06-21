68e4c5acbd9f79726683ecdccce3424749165000
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-06-21 21:22:42 +0900

    feat: User 엔티티의 id 생성 전략 설정

--- BE/issue_tracker/src/main/java/team02/issue_tracker/domain/User.java
+++ BE/issue_tracker/src/main/java/team02/issue_tracker/domain/User.java
@@ -13,8 +13,9 @@ import java.util.List;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
+
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Enumerated(EnumType.STRING)
