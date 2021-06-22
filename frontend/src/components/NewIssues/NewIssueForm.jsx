becb5bca92a69ce2fa93381f66d778150534a0fd
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-06-23 08:50:23 +0900

    feat: 새 이슈 페이지 스켈레톤 UI 완료

--- frontend/src/components/NewIssues/NewIssueForm.jsx
+++ frontend/src/components/NewIssues/NewIssueForm.jsx
@@ -3,8 +3,11 @@ import styled from "styled-components";
 import IssueCategoryList from "components/common/IssueCategoryList";
 import CommentInput from "components/common/CommentInput";
 import SubmitBtn from "components/common/Button/BlueButtons";
+import { ReactComponent as Xsquare } from "images/x-square.svg";
+import getUserInfo from "util/getUserInfo";
 
 const NewIssueForm = () => {
+	const userInfo = getUserInfo();
 	const handleSubmit = () => {
 		console.log("작성완료");
 	};
@@ -12,6 +15,9 @@ const NewIssueForm = () => {
 	return (
 		<>
 			<Wrapper>
+				<ImgWrapper>
+					<img src={userInfo.imageUrl} alt={userInfo.gitHubId} />
+				</ImgWrapper>
 				<ContentsWrapper>
 					<InputTitleWrapper>
 						<Input type="text" placeholder="제목" />
@@ -21,10 +27,14 @@ const NewIssueForm = () => {
 				<IssueCategoryList />
 			</Wrapper>
 			<ButtonWrapper>
-				{/* 스타일 코드 작성시 이부분 button 컴포넌트 적용 필요 */}
-				<Link to="/main">
-					<CancelBtn>x 작성 취소</CancelBtn>
-				</Link>
+				<CancelBtn>
+					<Link to="/main">
+						<CancelBtn>
+							<Xsquare stroke="#000000" />
+							작성 취소
+						</CancelBtn>
+					</Link>
+				</CancelBtn>
 				<SubmitBtn text="완료" size="l" clickHandler={handleSubmit} />
 			</ButtonWrapper>
 		</>
@@ -35,9 +45,21 @@ export default NewIssueForm;
 
 const Wrapper = styled.div`
 	display: grid;
-	grid-template-columns: 3fr 1fr;
+	grid-template-columns: 0.15fr 3fr 1fr;
 	padding: ${({ theme }) => `${theme.fontSizes.xxxl} 0`};
 	border-bottom: 1px solid ${({ theme }) => theme.grayScale.line};
+`;
+
+const ImgWrapper = styled.div`
+	width: 44px;
+	height: 44px;
+	border: 1px solid ${({ theme }) => theme.grayScale.line};
+	border-radius: 50%;
+	overflow: hidden;
+	img {
+		width: 100%;
+		height: 100%;
+	}
 `;
 
 const ContentsWrapper = styled.div`
@@ -65,4 +87,10 @@ const Input = styled.input`
 	background-color: ${({ theme }) => theme.grayScale.input_background};
 `;
 
-const CancelBtn = styled.button``;
+const CancelBtn = styled.div`
+	/* display: flex;
+	align-items: center;
+	justify-content: space-between; */
+	align-self: center;
+	margin-right: 1rem;
+`;
