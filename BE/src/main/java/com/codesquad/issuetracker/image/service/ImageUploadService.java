41802b604c0ff1572d49c3c4ca9b64517d7e43b7
PizzaCola-K <PizzaCola.K@gmail.com>
2021-06-22 15:35:45 +0900

    feat: ImageUploadService (#78)
    - 이미지 업로드 서비스
    - application-aws.properties 추가해야합니다.
    - AmazonS3Config 파일에 빈을 등록해야 합니다.

--- BE/src/main/java/com/codesquad/issuetracker/image/service/ImageUploadService.java
+++ BE/src/main/java/com/codesquad/issuetracker/image/service/ImageUploadService.java
@@ -1,5 +1,6 @@
 package com.codesquad.issuetracker.image.service;
 
+import com.codesquad.issuetracker.image.component.S3Uploader;
 import com.codesquad.issuetracker.image.dto.Image;
 import com.codesquad.issuetracker.image.dto.ImageWrapper;
 import org.springframework.stereotype.Service;
@@ -7,7 +8,14 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class ImageUploadService {
+
+    private final S3Uploader s3Uploader;
+
+    public ImageUploadService(S3Uploader s3Uploader) {
+        this.s3Uploader = s3Uploader;
+    }
+
     public ImageWrapper uploadImage(MultipartFile image) {
-        return ImageWrapper.wrap(Image.of(image.getName()));
+        return ImageWrapper.wrap(Image.of(s3Uploader.upload(image)));
     }
 }
