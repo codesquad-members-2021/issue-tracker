3c13ad97b445f8c892f8f91fc9393dcdb912e933
somedaycode <somedaycode@gmail.com>
2021-06-22 18:44:09 +0900

    Feat: #72 이슈 상세페이지를 위한 Selector 함수 추가
    * 데이터 요청 후 데이터를 받아온다.
    * 이슈 상세 리스트와 댓글, 작성자 이름을 가져온다.

---
+++ FE/issue-tracker/src/store/atoms/issueDetail.ts
@@ -0,0 +1,46 @@
+import { selector, selectorFamily } from 'recoil';
+
+import { hostAPI, issueAPI } from '@const/var';
+import { fetchWithAuth } from '@utils/fetchWithAuth';
+
+const issueDetailList = selectorFamily({
+  key: 'issueDetailList',
+  get: (id) => async () => {
+    try {
+      const path = `/${String(id)}`;
+      const url = issueAPI + path;
+      const res = await fetchWithAuth(url, '이슈 상세 에러', {});
+      const data = await res.json();
+      return data;
+    } catch (err) {
+      throw err;
+    }
+  },
+});
+
+const issueDetailComment = selectorFamily({
+  key: 'issueDetailComment',
+  get: (id) => async () => {
+    try {
+      const path = `/${String(id)}/comments`;
+      const url = issueAPI + path;
+      const res = await fetchWithAuth(url, '이슈 상세 에러', {});
+      const data = await res.json();
+      return data;
+    } catch (err) {
+      throw err;
+    }
+  },
+});
+
+const authorName = selectorFamily({
+  key: 'authorName',
+  get: (userID) => async () => {
+    const ID = String(userID);
+    const res = await fetchWithAuth(`${hostAPI}/${ID}`, '작성자 에러', {});
+    const { name } = await res.json();
+    return name;
+  },
+});
+
+export { issueDetailList, issueDetailComment, authorName };
