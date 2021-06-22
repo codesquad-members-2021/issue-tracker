5c2a3245c16413018b451d7779490627912b6da7
zeke <yangsin1031@naver.com>
2021-06-22 21:31:11 +0900

    chore: 🔧 서버 배포하여 Endpoint 수정 (#79)

--- iOS/issue-tracker/issue-tracker/Model/Endpoint.swift
+++ iOS/issue-tracker/issue-tracker/Model/Endpoint.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 struct Endpoint {
-    let scheme: String = "https"
-    let host: String = "77b8f295-a324-4645-9ff3-3d93eaf7b630.mock.pstmn.io"
+    let scheme: String = "http"
+    let host: String = "3.37.161.3"
     let port: Int = 8080
     var path: Path
 
@@ -17,6 +17,7 @@ struct Endpoint {
         var urlComponents = URLComponents()
         urlComponents.scheme = scheme
         urlComponents.host = host
+        urlComponents.port = port
         urlComponents.path = path.pathString
         if let id = id {
             urlComponents.path = path.pathString + "/" + String(id)
