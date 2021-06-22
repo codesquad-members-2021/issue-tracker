b4fd4b8aa4aa4e4b675c865398c106ceb1b36b48
woody <woojihye2339@gmail.com>
2021-06-22 10:27:03 +0900

    feat : S3Client 빈 생성 #15
    > S3Client 클래스 구현
       - AmazonS3을 사용한 S3 이미지 업로드 로직 구현
       - 업로드가 끝나면 생성된 image url을 반환
    
    > AwsConfig에서 S3Client 빈 생성
       - application-aws.properties의 aws 정보들을 통해
        AmazonS3 객체를 생성하고 S3Client 객체에 주입

---
+++ BE/src/main/java/com/codesquad/issuetracker/config/AwsConfig.java
@@ -0,0 +1,40 @@
+package com.codesquad.issuetracker.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.codesquad.issuetracker.domain.S3Client;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AwsConfig {
+
+    @Value("${cloud.aws.s3.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.s3.secret-key}")
+    private String secretKey;
+
+    @Value("${cloud.aws.s3.region}")
+    private String region;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @Bean
+    public S3Client s3Client() {
+        final AWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+        final AmazonS3 amazonS3 = AmazonS3ClientBuilder
+                .standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
+
+        return S3Client.create(amazonS3, region, bucket);
+
+    }
+}
---
+++ BE/src/main/java/com/codesquad/issuetracker/domain/S3Client.java
@@ -0,0 +1,61 @@
+package com.codesquad.issuetracker.domain;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.InputStream;
+import java.util.UUID;
+
+@Slf4j
+@AllArgsConstructor(staticName = "create")
+public class S3Client {
+
+    private static final String FILE_EXTENSION = ".jpeg";
+    private static final String CONTENT_TYPE = "image/jpeg";
+
+    private final AmazonS3 amazonS3;
+    private final String region;
+    private final String bucketName;
+
+    public String uploadJPGFile(InputStream inputStream) {
+        String name = randomNameGenerator();
+        return uploadFile(name, FILE_EXTENSION, inputStream, getMetadata(CONTENT_TYPE));
+    }
+
+    public String uploadFile(String filename, String extension, InputStream inputStream, ObjectMetadata metadata) {
+        String key = filename + extension;
+
+        PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, key, inputStream, metadata);
+        putObjectRequest.setCannedAcl(CannedAccessControlList.PublicRead);
+        amazonS3.putObject(putObjectRequest);
+
+        String url = amazonS3.getUrl(bucketName, key).getPath();
+        log.debug("Image url : {}", url);
+
+        return getBaseS3Url() + url;
+    }
+
+    private String randomNameGenerator() {
+        return UUID.randomUUID().toString();
+    }
+
+    private ObjectMetadata getMetadata(String contentType) {
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentType(contentType);
+        return metadata;
+    }
+
+    private String getBaseS3Url() {
+        return new StringBuilder()
+                .append("https://")
+                .append(bucketName)
+                .append(".s3.")
+                .append(region)
+                .append(".amazonaws.com")
+                .toString();
+    }
+}
