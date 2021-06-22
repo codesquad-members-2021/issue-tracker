dec18d9de3dbe80123328e0f91c29fc851ef98bb
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2021-06-22 14:21:39 +0900

    feat:(#73) 로그인 리다이렉트 설정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { Suspense, lazy, useEffect } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Header from './components/header/Header';
 import LoginPage from 'page/loginPage/LoginPage';
 
@@ -15,9 +15,9 @@ function App() {
   const MilestonePage = lazy(() => import('./page/milestonePage/MilestonePage'));
   const setLoginData = useSetRecoilState(controlLoginState);
   const isLogin = () => !!localStorage.getItem('token');
-  useEffect(()=>{
-    if (isLogin()) setLoginData({isLogin: true, loginData: null})
-  },[])
+  useEffect(() => {
+    if (isLogin()) setLoginData({ isLogin: true, loginData: null });
+  }, []);
   /*
   token유무로 로그인상태 확인=> jwt api 요청=> setLoginData => redirect(/main)
   : loginPage에서 token값확인하는 부분 삭제하기.
@@ -26,10 +26,12 @@ function App() {
   return (
     <div className='App'>
       <Router>
-        <Header/>
+        <Header />
         <Suspense fallback={<h1>Loading...</h1>}>
           <Switch>
-            <Route path='/' component={LoginPage} exact />
+            <Route path='/' exact>
+              {isLogin() ? <Redirect to='/main' /> : <LoginPage />}
+            </Route>
             <Route path='/main' component={MainPage} />
             <Route path='/create' component={CreateIssuePage} />
             <Route path='/detail' component={DetailIssuePage} />
