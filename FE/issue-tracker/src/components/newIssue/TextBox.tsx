5499d3f3e45852f6080c4ba82c077e6dd859ef4a
eve712 <62237639+eve712@users.noreply.github.com>
2021-06-23 22:51:32 +0900

    Feat: #39 newIssue의 TextBox 상태관리 추가
    * 완료 버튼이 클릭되면 title, contents 내용을 상태에 저장
    * 파일 첨부 구현은 미완(아직 백엔드 api 미완)
    * textarea 글자수 실시간 표기

--- FE/issue-tracker/src/components/newIssue/TextBox.tsx
+++ FE/issue-tracker/src/components/newIssue/TextBox.tsx
@@ -1,16 +1,40 @@
+import { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
-import { useRecoilState } from 'recoil';
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 
 import { Input } from '@chakra-ui/input';
 import { Textarea } from '@chakra-ui/textarea';
 
 import { ReactComponent as FileIcon } from '@assets/file.svg';
-import { contentsInput, titleInput } from './style';
-import { isInputtedTitleState } from '@store/atoms/newIssue';
+import { contentsInputStyle, titleInputStyle } from './style';
+import {
+  isInputtedTitleAtom,
+  isClickedCompleteBtnAtom,
+  newIssueContentsAtom,
+} from '@store/atoms/newIssue';
+import React from 'react';
 
 function TextBox() {
   const [isInputtedTitle, setIsInputtedTitle] =
-    useRecoilState(isInputtedTitleState);
+    useRecoilState(isInputtedTitleAtom);
+  const isClickedCompleteBtn = useRecoilValue(isClickedCompleteBtnAtom);
+  const setNewIssueContents = useSetRecoilState(newIssueContentsAtom);
+  const [numOfChars, setNumOfChars] = useState(0);
+
+  const titleInput = useRef<HTMLInputElement>(null);
+  const contentsInput = useRef<HTMLTextAreaElement>(null);
+
+  useEffect(() => {
+    if (
+      isClickedCompleteBtn &&
+      titleInput.current != null &&
+      contentsInput.current != null
+    )
+      setNewIssueContents({
+        title: titleInput.current.value,
+        description: contentsInput.current.value,
+      });
+  }, [isClickedCompleteBtn]);
 
   const handleChangeTitle = (e: React.ChangeEvent) => {
     const target = e.target as HTMLInputElement;
@@ -19,15 +43,47 @@ function TextBox() {
       setIsInputtedTitle(false);
   };
 
+  const handleChangeTextArea = (e: React.ChangeEvent) => {
+    const target = e.target as HTMLInputElement;
+    const length = target.value.length;
+    if (numOfChars === length) return;
+    setNumOfChars(length);
+  };
+
+  // 아직 서버 구현이 안 돼있어 추후에 구현 예정
+  const handleChangeFile = (e: React.ChangeEvent) => {
+    const target = e.target as HTMLInputElement;
+    const fileList = target.files;
+    if (fileList == null) return;
+    const formData = new FormData();
+    formData.append('file', fileList[0]);
+  };
+
   return (
     <TextBoxWrap>
-      <Input {...titleInput} onChange={handleChangeTitle} />
+      <Input
+        {...titleInputStyle}
+        ref={titleInput}
+        onChange={handleChangeTitle}
+      />
       <Description>
-        <Textarea {...contentsInput} />
-        <Span>띄어쓰기 포함 \d\d 자</Span>
+        <Textarea
+          {...contentsInputStyle}
+          ref={contentsInput}
+          onChange={handleChangeTextArea}
+        />
+        <Span>띄어쓰기 포함 {numOfChars} 자</Span>
         <ImageUploadWrap>
-          <FileIcon />
-          <span>파일 첨부하기</span>
+          <label htmlFor="input_file">
+            <FileIcon />
+            <span>파일 첨부하기</span>
+          </label>
+          <input
+            type="file"
+            accept="image/*"
+            id="input_file"
+            onChange={handleChangeFile}
+          />
         </ImageUploadWrap>
       </Description>
     </TextBoxWrap>
@@ -59,14 +115,30 @@ const Span = styled.div`
 const ImageUploadWrap = styled.div`
   display: flex;
   align-items: center;
-  padding: 16px 24px;
   height: 52px;
   color: ${({ theme }) => theme.colors.label};
   font-weight: ${({ theme }) => theme.fontWeights.bold};
   border-top: 1px dotted ${({ theme }) => theme.colors.gr_line};
-  cursor: pointer;
 
-  span {
-    padding-left: 10px;
+  label {
+    padding: 16px 24px;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+
+    span {
+      padding-left: 10px;
+    }
+  }
+
+  input {
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    padding: 0;
+    border: 0;
+    margin: -1px;
+    overflow: hidden;
+    clip: rect(0, 0, 0, 0);
   }
 `;
