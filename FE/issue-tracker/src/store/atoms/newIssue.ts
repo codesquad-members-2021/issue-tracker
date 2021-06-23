7ce00e92ea4667e2bc1ce96556e9c39d804a704f
eve712 <62237639+eve712@users.noreply.github.com>
2021-06-23 23:17:12 +0900

    Add: #41 이슈 생성과 관련된 atom 추가

--- FE/issue-tracker/src/store/atoms/newIssue.ts
+++ FE/issue-tracker/src/store/atoms/newIssue.ts
@@ -1,6 +1,19 @@
 import { atom } from 'recoil';
 
-export const isInputtedTitleState = atom({
+export const isInputtedTitleAtom = atom({
   key: 'isInputtedTitleState',
   default: false,
+});
+
+export const isClickedCompleteBtnAtom = atom({
+  key: 'isClickedCompleteBtnState',
+  default: false,
+});
+
+export const newIssueContentsAtom = atom({
+  key: 'newIssueContentsState',
+  default: {
+    title: '',
+    description: '',
+  },
 });
