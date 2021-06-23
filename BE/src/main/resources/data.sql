b72e5110c8c9ccf0a69464fe51f022841eae8f0a
bibi <bibi6666667@kakao.com>
2021-06-23 19:03:23 +0900

    feat : Issue 필터링 기능 구현을 위한 테스트 데이터 추가 #14

--- BE/src/main/resources/data.sql
+++ BE/src/main/resources/data.sql
@@ -6,11 +6,15 @@ INSERT INTO `milestone` (title, content, due_date, opened_issue, closed_issue)
 VALUES ("milestone title 1", "milestone content 1", NOW(), 0, 0);
 
 INSERT INTO `issue` (title, status, created_at, content, milestone_id, user_id)
-VALUES ("title1", true, NOW(), "content1", NULL, 1);
+VALUES ("title1", true, NOW(), "content1", NULL, 1),
+       ("title2", false, NOW(), "content2", NULL, 2),
+       ("title3 blah", true, NOW(), "content3 blah", NULL, 1);
 
 INSERT INTO `comment` (content, created_at, issue_id, user_id)
 VALUES ("comment content 1", NOW(), 1, 1),
-       ("comment content 2", NOW(), 1, 2);
+       ("comment content 2", NOW(), 1, 2),
+       ("comment content 3", NOW(), 2, 1),
+       ("comment content 4", NOW(), 3, 2);
 
 INSERT INTO `label` (title, content, color)
 VALUES ("label title 1", "label content 1", "FFFFFF"),
@@ -20,7 +24,9 @@ VALUES ("label title 1", "label content 1", "FFFFFF"),
 
 INSERT INTO `assignee` (issue_id, user_id)
 VALUES (1, 1),
-       (1, 2);
+       (1, 2),
+       (2, 1),
+       (3, 2);
 
 INSERT INTO `issue_label` (issue_id, label_id)
 VALUES (1, 1),
