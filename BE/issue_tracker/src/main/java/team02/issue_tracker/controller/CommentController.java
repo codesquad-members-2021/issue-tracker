d8e7b71305adca5aecfd49e81bd3334acc43d875
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-06-24 00:38:22 +0900

    chore : swagger 문서에 제목과 설명 추가

--- BE/issue_tracker/src/main/java/team02/issue_tracker/controller/CommentController.java
+++ BE/issue_tracker/src/main/java/team02/issue_tracker/controller/CommentController.java
@@ -1,5 +1,7 @@
 package team02.issue_tracker.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 import team02.issue_tracker.dto.CommentEmojiRequest;
 import team02.issue_tracker.dto.CommentRequest;
@@ -9,6 +11,7 @@ import team02.issue_tracker.service.CommentService;
 
 import java.util.List;
 
+@Api(tags = {"코멘트 관련 API"}, description = "코멘트 수정, 이모지 조회, 이모지 추가 가능합니다.")
 @RestController
 @RequestMapping("/api/comments")
 public class CommentController {
@@ -19,18 +22,21 @@ public class CommentController {
         this.commentService = commentService;
     }
 
+    @ApiOperation(value = "코멘트에 이모지 추가", notes = "코멘트에 이모지를 추가합니다.")
     @PostMapping("/{commentId}/emojis")
     public ApiResult<String> addEmojiOnComment(@PathVariable Long commentId, @RequestBody CommentEmojiRequest commentEmojiRequest) {
         commentService.addEmoji(commentId, commentEmojiRequest);
         return ApiResult.ok();
     }
 
+    @ApiOperation(value = "코멘트 수정", notes = "코멘트를 수정합니다.")
     @PatchMapping("/{commentId}")
     public ApiResult<String> modifyComment(@PathVariable Long commentId, @RequestBody CommentRequest commentRequest) {
         commentService.modifyComment(commentId, commentRequest);
         return ApiResult.ok();
     }
 
+    @ApiOperation(value = "이모지 조회", notes = "이모지를 전체 조회합니다.")
     @GetMapping("/emojis")
     public ApiResult<List<EmojiResponse>> showAllEmojis() {
         return ApiResult.success(commentService.getAllEmojiResponses());
--- BE/issue_tracker/src/main/java/team02/issue_tracker/controller/LabelController.java
+++ BE/issue_tracker/src/main/java/team02/issue_tracker/controller/LabelController.java
@@ -1,5 +1,7 @@
 package team02.issue_tracker.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 import team02.issue_tracker.dto.LabelCountResponse;
 import team02.issue_tracker.dto.LabelRequest;
@@ -9,6 +11,7 @@ import team02.issue_tracker.service.LabelService;
 
 import java.util.List;
 
+@Api(tags = {"레이블 관련 API"}, description = "레이블 등록, 조회, 수정, 삭제 가능합니다.")
 @RestController
 @RequestMapping("/api/labels")
 public class LabelController {
@@ -19,28 +22,33 @@ public class LabelController {
         this.labelService = labelService;
     }
 
+    @ApiOperation(value = "전체 레이블 조회", notes = "등록된 모든 레이블을 조회합니다.")
     @GetMapping
     public ApiResult<List<LabelResponse>> showAllLabels() {
         return ApiResult.success(labelService.getAllLabelResponses());
     }
 
+    @ApiOperation(value = "레이블 개수 조회", notes = "레이블 개수를 조회합니다.")
     @GetMapping("/count")
     public ApiResult<LabelCountResponse> showLabelCount() {
         return ApiResult.success(labelService.getLabelCountResponse());
     }
 
+    @ApiOperation(value = "레이블 수정", notes = "레이블을 수정합니다.")
     @PutMapping("/{labelId}")
     public ApiResult<String> modifyLabel(@PathVariable Long labelId, @RequestBody LabelRequest labelRequest) {
         labelService.modifyLabel(labelId, labelRequest);
         return ApiResult.ok();
     }
 
+    @ApiOperation(value = "레이블 등록", notes = "새로운 레이블을 등록합니다.")
     @PostMapping
     public ApiResult<String> createLabel(@RequestBody LabelRequest labelRequest) {
         labelService.addLabel(labelRequest);
         return ApiResult.ok();
     }
 
+    @ApiOperation(value = "레이블 삭제", notes = "레이블을 삭제합니다.")
     @DeleteMapping("/{labelId}")
     public ApiResult<String> deleteLabel(@PathVariable Long labelId) {
         labelService.deleteLabel(labelId);
