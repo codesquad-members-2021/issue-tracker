3ff3ec9933ecff697c43c91b06fba8a92999e863
somedaycode <somedaycode@gmail.com>
2021-06-23 18:30:15 +0900

    Fix: #59 쿼리에 따라 atom의 formerDataKey값을 바꾸어주며 리렌더링

--- FE/issue-tracker/src/components/issues/tableMain/IssueList.tsx
+++ FE/issue-tracker/src/components/issues/tableMain/IssueList.tsx
@@ -1,7 +1,7 @@
 import { useRecoilValue, useRecoilValueLoadable } from 'recoil';
 
 import { queryString, wholeIssueLists } from '@store/atoms/issueList';
-import { pushState } from '@utils/query';
+import { useReRender } from '@utils/query';
 
 import { IssueSkeleton } from '@components/common/Skeleton';
 import Issue from './Issue';
@@ -11,13 +11,13 @@ import NoIssue from './NoIssue';
 function IssueList() {
   const query = useRecoilValue(queryString);
   const { state, contents } = useRecoilValueLoadable(wholeIssueLists);
-
   const renderNoIssue = (contents: any): JSX.Element | undefined => {
     if (typeof contents === 'string' || contents.length === 0) {
       return <NoIssue isSearched={false} />;
     }
   };
-  pushState(query);
+  useReRender(query);
+
   return (
     <>
       {state === 'hasError' && <ErrorIssueList>{contents}</ErrorIssueList>}
@@ -37,7 +37,8 @@ export type IssueInfo = {
   id: number;
   title: string;
   description: string;
-  author: Author;
+  author: Person;
+  assignees: Person[];
   label_list: Label[];
   issue_number: number;
   created_time: string;
@@ -50,7 +51,7 @@ type Label = {
   color_code: string;
 };
 
-type Author = {
+type Person = {
   user_id: number;
   name: string;
   avatar_url: string;
