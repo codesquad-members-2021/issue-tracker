f286a9764ffdebb8b9f4bc84af9ff812cf93ac7e
lena <sue822330@gmail.com>
2021-06-23 11:18:56 +0900

    feat: add badge, author to issue detail view

--- iOS/issue-tracker/issue-tracker/Controller/IssueDetailViewController.swift
+++ iOS/issue-tracker/issue-tracker/Controller/IssueDetailViewController.swift
@@ -11,11 +11,38 @@ import RxSwift
 import RxCocoa
 
 final class IssueDetailViewController: UIViewController {
+
+    var viewModel: IssueDetailViewModel = IssueDetailViewModel()
+
     private let cellReuseIdentifier = "IssueDetailCell"
-    var viewModel: IssueDetailViewModel!
     private let disposeBag = DisposeBag()
     private var comment: [Comment] = []
     private var textFieldHeightConstraint: NSLayoutConstraint?
+
+    private let headerStackView: UIStackView = {
+        let stackView = UIStackView(frame: CGRect(origin: .zero, size: CGSize(width: 1, height: 44)))
+        stackView.axis = .vertical
+        stackView.alignment = .leading
+        stackView.spacing = 10
+        return stackView
+    }()
+
+    private let isOpened: PaddingLabel = {
+        let label = PaddingLabel(withInsets: 0, 0, 10, 10)
+        label.textAlignment = .center
+        label.backgroundColor = .systemPink
+        label.textColor = .white
+        label.layer.masksToBounds = true
+        label.layer.cornerRadius = 10
+        label.snp.makeConstraints { $0.width.greaterThanOrEqualTo(50) }
+        return label
+    }()
+
+    private let authorLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        return label
+    }()
 
     private let tableView: UITableView = {
         let tableView = UITableView()
@@ -37,10 +64,16 @@ final class IssueDetailViewController: UIViewController {
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "ellipsis"), style: .plain, target: self, action: #selector(showIssueDetailInfo))
 
+        textField.textFieldDelegate = self
         tableView.dataSource = self
         tableView.delegate = self
         tableView.frame = view.bounds
         tableView.selectRow(at: IndexPath(row: 0, section: 0), animated: false, scrollPosition: .none)
+
+        headerStackView.addArrangedSubview(isOpened)
+        headerStackView.addArrangedSubview(authorLabel)
+
+        tableView.tableHeaderView = headerStackView
 
         view.addSubview(tableView)
         view.addSubview(textField)
@@ -74,6 +107,7 @@ final class IssueDetailViewController: UIViewController {
     }
 
     private func setupAutolayout() {
+        headerStackView.snp.makeConstraints { $0.leading.trailing.top.bottom.equalToSuperview().inset(20) }
         NSLayoutConstraint.activate([
             textField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
             textField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
@@ -103,11 +137,14 @@ private extension IssueDetailViewController {
     }
 
     func bind() {
-        viewModel.subject.bind { detail in
-            self.navigationItem.title = detail?.data.title
-            guard let comment = detail?.data.comment else { return }
-            self.comment = comment
-            self.tableView.reloadData()
+        viewModel.subject.bind { [weak self] detail in
+            guard let detail = detail?.data else { return }
+            self?.navigationItem.title = detail.title
+            self?.isOpened.text = detail.isOpen ? "열림" : "닫힘"
+            self?.authorLabel.text = "\(detail.author.name)님이 작성했습니다."
+            guard let comment = detail.comment else { return }
+            self?.comment = comment
+            self?.tableView.reloadData()
         }
         .disposed(by: disposeBag)
     }
@@ -130,5 +167,14 @@ extension IssueDetailViewController: UITableViewDataSource {
 extension IssueDetailViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
         UIView()
+    }
+}
+
+extension IssueDetailViewController: ToolBarTextFieldDelegate {
+    func register() {
+        guard let comment = textField.text else {
+            return
+        }
+        viewModel.post(comment: comment)
     }
 }
