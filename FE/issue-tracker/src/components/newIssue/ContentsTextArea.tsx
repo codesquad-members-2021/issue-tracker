1b94d6d496f553b36c916783898210071f3f8261
eve712 <62237639+eve712@users.noreply.github.com>
2021-06-24 13:28:37 +0900

    Fix: #41 contents 객체 하나로 상태에 담았던 것을 title 상태 따로 분리

--- FE/issue-tracker/src/components/newIssue/ContentsTextArea.tsx
+++ FE/issue-tracker/src/components/newIssue/ContentsTextArea.tsx
@@ -1,10 +1,11 @@
 import { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
-import { useRecoilState, useRecoilValue } from 'recoil';
+import { useSetRecoilState, useRecoilValue } from 'recoil';
 import { Textarea } from '@chakra-ui/textarea';
 
 import {
   isClickedCompleteBtnAtom,
+  newIssueTitleAtom,
   newIssueContentsAtom,
 } from '@store/atoms/newIssue';
 import { contentsInputStyle } from './style';
@@ -12,17 +13,14 @@ import { contentsInputStyle } from './style';
 function ContentsTextArea() {
   const [numOfChars, setNumOfChars] = useState(0);
   const isClickedCompleteBtn = useRecoilValue(isClickedCompleteBtnAtom);
-  const [newIssueContents, setNewIssueContents] =
-    useRecoilState(newIssueContentsAtom);
+  const newIssueTitle = useRecoilValue(newIssueTitleAtom);
+  const setNewIssueContents = useSetRecoilState(newIssueContentsAtom);
   const contentsInput = useRef<HTMLTextAreaElement>(null);
 
   useEffect(() => {
     if (isClickedCompleteBtn && contentsInput.current != null)
-      setNewIssueContents({
-        ...newIssueContents,
-        description: contentsInput.current.value,
-      });
-  }, [isClickedCompleteBtn]);
+      setNewIssueContents(contentsInput.current.value);
+  }, [newIssueTitle]);
 
   const handleChangeTextArea = (e: React.ChangeEvent) => {
     const target = e.target as HTMLInputElement;
