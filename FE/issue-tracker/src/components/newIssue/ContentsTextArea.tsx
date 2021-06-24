142e1482d12aca22487038a2eea2b50837705a20
eve712 <62237639+eve712@users.noreply.github.com>
2021-06-24 15:02:26 +0900

    Fix: onchange마다 상태 저장하는 것으로 변경
    * 글자 수를 실시간 표시하기 위해 length 상태 사용을 하기 때문에 contents내용만 담은 상태는 삭제

--- FE/issue-tracker/src/components/newIssue/ContentsTextArea.tsx
+++ FE/issue-tracker/src/components/newIssue/ContentsTextArea.tsx
@@ -1,42 +1,24 @@
-import { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
-import { useSetRecoilState, useRecoilValue } from 'recoil';
+import { useRecoilState } from 'recoil';
 import { Textarea } from '@chakra-ui/textarea';
 
-import {
-  isClickedCompleteBtnAtom,
-  newIssueTitleAtom,
-  newIssueContentsAtom,
-} from '@store/atoms/newIssue';
+import { newIssueContentsAtom } from '@store/atoms/newIssue';
 import { contentsInputStyle } from './style';
 
 function ContentsTextArea() {
-  const [numOfChars, setNumOfChars] = useState(0);
-  const isClickedCompleteBtn = useRecoilValue(isClickedCompleteBtnAtom);
-  const newIssueTitle = useRecoilValue(newIssueTitleAtom);
-  const setNewIssueContents = useSetRecoilState(newIssueContentsAtom);
-  const contentsInput = useRef<HTMLTextAreaElement>(null);
-
-  useEffect(() => {
-    if (isClickedCompleteBtn && contentsInput.current != null)
-      setNewIssueContents(contentsInput.current.value);
-  }, [newIssueTitle]);
+  const [newIssueContents, setNewIssueContents] =
+    useRecoilState(newIssueContentsAtom);
 
   const handleChangeTextArea = (e: React.ChangeEvent) => {
     const target = e.target as HTMLInputElement;
-    const length = target.value.length;
-    if (numOfChars === length) return;
-    setNumOfChars(length);
+    const contents = target.value;
+    setNewIssueContents(contents);
   };
 
   return (
     <>
-      <Textarea
-        {...contentsInputStyle}
-        ref={contentsInput}
-        onChange={handleChangeTextArea}
-      />
-      <Span>띄어쓰기 포함 {numOfChars} 자</Span>
+      <Textarea {...contentsInputStyle} onChange={handleChangeTextArea} />
+      <Span>띄어쓰기 포함 {newIssueContents.length} 자</Span>
     </>
   );
 }
