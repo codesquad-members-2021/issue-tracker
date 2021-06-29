17505c30561958e1fe58a9ba46e20b4e676f9976
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2021-06-29 12:20:09 +0900

    Fix/Add: 테스트 삭제, 필요한 테스트 추가

---
+++ be/src/test/java/com/issuetracker/repository/MilestoneRepositoryTest.java
@@ -0,0 +1,20 @@
+package com.issuetracker.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MilestoneRepositoryTest {
+
+    @Test
+    void findMilestoneByMilestoneId() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findAllByClosed() {
+    }
+}
