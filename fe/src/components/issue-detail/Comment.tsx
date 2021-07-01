6072f2d4cc2e9d5f30774e292c91bb96fd4998c6
Autumn (조홍비) <60209518+dyongdi@users.noreply.github.com>
2021-07-01 15:17:37 +0900

    [#126] 코멘트 편집 기능 (#134)
    * [#126] Feat: 코멘트 편집 버튼 클릭 시 input창 렌더링
    
    * [#126] Feat: 코멘트 수정 기능 추가 (다시 get요청 받아 화면 업데이트 필요)
    
    * [#126] Feat: 코멘트 편집 후 새로 get 요청 보내서 화면 업데이트

--- fe/src/components/issue-detail/Comment.tsx
+++ fe/src/components/issue-detail/Comment.tsx
@@ -1,8 +1,10 @@
+import { useState } from 'react';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { Box, Button } from '@material-ui/core';
 import styled from 'styled-components';
 
 import AuthorAvatar from 'components/common/AuthorAvatar';
+import CommentEdit from 'components/issue-detail/CommentEdit';
 import { ReactComponent as EditSvg } from 'icons/edit.svg';
 import { ReactComponent as DeleteSvg } from 'icons/delete.svg';
 import { ReactComponent as EmojiSvg } from 'icons/emoji.svg';
@@ -22,56 +24,70 @@ const Comment = ({ commentData }: { commentData: CommentType }) => {
   const loginUser = useRecoilValue(decodedUserDataAtom);
   const clickedIssueId = useRecoilValue(clickedIssueIdAtom);
   const setCommentUpdate = useSetRecoilState(commentUpdateAtom);
+  const [isEditing, setIsEditing] = useState(false);
+  const commentRequestUrl = `${process.env.REACT_APP_API_URL}/api/issues/${clickedIssueId}/comments/${id}`;
 
   const clickDeleteHandler = () => {
     (async () => {
-      await instanceWithAuth.delete(
-        `${process.env.REACT_APP_API_URL}/api/issues/${clickedIssueId}/comments/${id}`
-      );
+      await instanceWithAuth.delete(commentRequestUrl);
       setCommentUpdate((cur) => ++cur);
     })();
+  };
+
+  const clickEditHandler = () => {
+    setIsEditing(true);
   };
 
   return (
     <Box display="flex">
       <AuthorAvatar size="L" profileImg={author.profileImg} />
-      <CommentWrapper>
-        <CommentHeader display="flex">
-          <Box display="flex">
-            <div className="comment-author">{author.name}</div>
-            <div className="comment-created-time">{createdTime}분 전</div>
-          </Box>
-          <Box display="flex" alignItems="center">
-            {issueAuthorId === author.id && (
-              <IssueAuthorLabel
-                display="flex"
-                alignItems="center"
-                justifyContent="center"
-              >
-                <span>작성자</span>
-              </IssueAuthorLabel>
-            )}
-            {loginUser && loginUser.id === author.id && (
-              <Button startIcon={<EditIcon />}>편집</Button>
-            )}
-            {loginUser && loginUser.id === author.id && (
-              <Button
-                onClick={clickDeleteHandler}
-                startIcon={<DeleteIcon />}
-                color="secondary"
-              >
-                삭제
-              </Button>
-            )}
-            <EmojiButton>
-              <EmojiSvg />
-            </EmojiButton>
-          </Box>
-        </CommentHeader>
-        <CommentBody>
-          <div className="comment-content">{description}</div>
-        </CommentBody>
-      </CommentWrapper>
+      {isEditing ? (
+        <CommentEdit
+          defaultValue={description}
+          setIsEditing={setIsEditing}
+          commentRequestUrl={commentRequestUrl}
+        />
+      ) : (
+        <CommentWrapper>
+          <CommentHeader display="flex">
+            <Box display="flex">
+              <div className="comment-author">{author.name}</div>
+              <div className="comment-created-time">{createdTime}분 전</div>
+            </Box>
+            <Box display="flex" alignItems="center">
+              {issueAuthorId === author.id && (
+                <IssueAuthorLabel
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
+                >
+                  <span>작성자</span>
+                </IssueAuthorLabel>
+              )}
+              {loginUser && loginUser.id === author.id && (
+                <Button startIcon={<EditIcon />} onClick={clickEditHandler}>
+                  편집
+                </Button>
+              )}
+              {loginUser && loginUser.id === author.id && (
+                <Button
+                  onClick={clickDeleteHandler}
+                  startIcon={<DeleteIcon />}
+                  color="secondary"
+                >
+                  삭제
+                </Button>
+              )}
+              <EmojiButton>
+                <EmojiSvg />
+              </EmojiButton>
+            </Box>
+          </CommentHeader>
+          <CommentBody>
+            <div className="comment-content">{description}</div>
+          </CommentBody>
+        </CommentWrapper>
+      )}
     </Box>
   );
 };
