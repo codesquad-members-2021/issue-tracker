7b9d9087ea085fb35bc0e9776f61b4e62612ba4a
woody <woojihye2339@gmail.com>
2021-07-26 18:03:42 +0900

    refactor : LoginController 생성자 주입 대상 변경 #19
    > 생성자를 통해 Environment 빈을 주입받는 대신, @Value 어노테이션을 통해 직접 특정 값을 주입받아서 사용

--- BE/src/main/java/com/codesquad/issuetracker/auth/controller/GitHubLoginController.java
+++ BE/src/main/java/com/codesquad/issuetracker/auth/controller/GitHubLoginController.java
@@ -5,10 +5,7 @@ import com.codesquad.issuetracker.auth.response.GitHubUserResponse;
 import com.codesquad.issuetracker.auth.service.GitHubLoginService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/login")
--- BE/src/main/java/com/codesquad/issuetracker/auth/controller/LoginController.java
+++ BE/src/main/java/com/codesquad/issuetracker/auth/controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.codesquad.issuetracker.auth.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,17 +11,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class LoginController {
 
     private final String GITHUB_URL = "https://github.com/login/oauth/authorize";
-    private final String CLIENT_ID;
+    private final String CLIENT_ID_WEB;
     private final String CLIENT_ID_IOS;
 
-    public LoginController(Environment environment) {
-        this.CLIENT_ID = environment.getProperty("github.client.id");
-        this.CLIENT_ID_IOS = environment.getProperty("github.client.id.ios");
+    public LoginController(@Value("github.client.id.web") String webClientId,
+                           @Value("github.client.id.ios") String iOSClientId) {
+        CLIENT_ID_WEB = webClientId;
+        CLIENT_ID_IOS = iOSClientId;
     }
 
     @GetMapping
     public String login() {
-        return "redirect:" + GITHUB_URL + "?client_id=" + CLIENT_ID;
+        return "redirect:" + GITHUB_URL + "?client_id=" + CLIENT_ID_WEB;
     }
 
     @GetMapping("/iOS")
