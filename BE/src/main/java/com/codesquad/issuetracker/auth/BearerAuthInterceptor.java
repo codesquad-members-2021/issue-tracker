565604fdb9f62c1b2a268c627d502ec2fa71f3e6
woody <woojihye2339@gmail.com>
2021-07-26 17:10:10 +0900

    refactor : "user" 문자열 상수처리 #19
    > "user"문자열을 USER 상수 처리해서 user http header로 사용

--- BE/src/main/java/com/codesquad/issuetracker/auth/BearerAuthInterceptor.java
+++ BE/src/main/java/com/codesquad/issuetracker/auth/BearerAuthInterceptor.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 public class BearerAuthInterceptor implements HandlerInterceptor {
 
     private final String TOKEN_TYPE = "Bearer";
+    private final String USER = "user";
 
     private AuthorizationExtractor authorizationExtractor;
     private JwtProvider jwtProvider;
@@ -25,7 +26,7 @@ public class BearerAuthInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        request.setAttribute("user", getUserFromJWT(request));
+        request.setAttribute(USER, getUserFromJWT(request));
         return true;
     }
 
